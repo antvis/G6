b6f7b52d40ff6a27bb8d2f35bc6af89deadb267b
TomHuangCN <418466938@qq.com>
2018-06-11 17:19:09 +0800

    feat: add quadraticCurve config cpd

--- plugins/edge.quadraticCurve/index.js
+++ plugins/edge.quadraticCurve/index.js
@@ -1,24 +1,26 @@
 /**
- * @fileOverview 拓展二阶贝塞尔曲线
+ * @fileOverview extend quadraticCurve
+ * model configurable:
+ * cpd control point distance detail {number} could be 0 ~ 1
  * @author huangtonger@aliyun.com
  */
 const G6 = require('@antv/g6');
-
+const Util = G6.Util;
 G6.registerEdge('quadraticCurve', {
   getPath(item) {
     const points = item.getPoints();
+    const model = item.getModel();
     const start = points[0];
     const source = item.getSource();
     const target = item.getTarget();
     const end = points[points.length - 1];
-    const q = 0.5; // 向量方向长度系数
-    const v1 = [ start.x + (end.x - start.x) * q, start.y + (end.y - start.y) * q ];
-    const v2 = [ v1[0] - start.x, v1[1] - start.y ];
-    const v3 = [ -v2[1], v2[0] ];
+    const cpd = !Util.isNil(model.cpd) ? model.cpd : 0.5; // 向量方向长度系数
+    const v1 = [ (end.x + start.x) * 0.5 - start.x, (end.y + start.y) * 0.5 - start.y ];
+    const v2 = [ -v1[1] * cpd, v1[0] * cpd ];
     // 控制点
     const cp = {
-      x: v3[0] + v2[0] + start.x,
-      y: v3[1] + v2[1] + start.y
+      x: v1[0] + v2[0] + start.x,
+      y: v2[1] + v2[1] + start.y
     };
     const sp = source.getLinkPoints(cp)[0];
     const tp = target.getLinkPoints(cp)[0];
