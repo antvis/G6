e1c756164092fd2a77d2e464435cd1d73a849768
TomHuangCN <418466938@qq.com>
2018-07-12 21:38:24 +0800

    Release 2.0.4

--- test/unit/plugins/tool.tooltip-spec.js
+++ test/unit/plugins/tool.tooltip-spec.js
@@ -1,19 +1,18 @@
 const G6 = require('../../../src/index');
 const Tooltip = require('../../../plugins/tool.tooltip/');
+const Simulate = require('event-simulate');
 const expect = require('chai').expect;
 const Util = G6.Util;
 
 document.body.appendChild(Util.createDOM(`
-<div>
   <div id='mountNode'></div>
-</div>
 `));
 
 describe('tooltip test', () => {
   const tooltip = new Tooltip({
     getTooltip() {
       return `
-        <div>dddd</div>
+        <div id="tooltip">dddd</div>
       `;
     }
   });
@@ -38,8 +37,55 @@ describe('tooltip test', () => {
     height: 500,
     plugins: [ tooltip ]
   });
+  const mouseEventWrapper = graph.getMouseEventWrapper();
+  let top;
+  let left;
   graph.read(data);
-  it('tooltip destroy', () => {
-    graph.destroy();
+  it('mouseenter show tooltip', () => {
+    const node1Model = graph.find('node1').getModel();
+    const clientPoint = graph.getClientPoint(node1Model);
+    Simulate.simulate(mouseEventWrapper, 'mousedown', {
+      clientX: clientPoint.x - 50,
+      clientY: clientPoint.y
+    });
+    Simulate.simulate(mouseEventWrapper, 'mousemove', {
+      clientX: clientPoint.x - 50,
+      clientY: clientPoint.y
+    });
+    Simulate.simulate(mouseEventWrapper, 'mousemove', {
+      clientX: clientPoint.x,
+      clientY: clientPoint.y
+    });
+    expect(document.getElementById('tooltip')).not.eql(null);
   });
+  it('mousemove move tooltip', () => {
+    const node1Model = graph.find('node1').getModel();
+    const clientPoint = graph.getClientPoint(node1Model);
+    Simulate.simulate(mouseEventWrapper, 'mousemove', {
+      clientX: clientPoint.x + 5,
+      clientY: clientPoint.y
+    });
+  });
+  // it('mouseleave hide tooltip', () => {
+  //   const node1Model = graph.find('node1').getModel();
+  //   const clientPoint = graph.getClientPoint(node1Model);
+  //   Simulate.simulate(mouseEventWrapper, 'mousedown', {
+  //     clientX: clientPoint.x - 50,
+  //     clientY: clientPoint.y
+  //   });
+  //   Simulate.simulate(mouseEventWrapper, 'mousemove', {
+  //     clientX: clientPoint.x - 50,
+  //     clientY: clientPoint.y
+  //   });
+  //   Simulate.simulate(mouseEventWrapper, 'mousemove', {
+  //     clientX: clientPoint.x,
+  //     clientY: clientPoint.y
+  //   });
+  // });
+  // it('mouseleave', () => {
+
+  // });
+  // it('tooltip destroy', () => {
+  //   graph.destroy();
+  // });
 });
