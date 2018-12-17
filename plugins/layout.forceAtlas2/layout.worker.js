6869feb330b421b226eb33190cebf84370be60e1
yilin.qyl <yilin.qyl@alipay.com>
2018-12-17 14:55:33 +0800

    chore: sort out 3.0.0 dirs

--- plugins/layout.forceAtlas2/layout.worker.js
+++
@@ -1,7 +0,0 @@
-
-const ForceCal = require('./forceCalculator');
-onmessage = function(event) {
-  const forceCal = new ForceCal();
-  const nodes = forceCal.updateNodesByForces(event.data);
-  self.postMessage(nodes);
-};
