da4316189da332d4bd6ac9428c2afe2d00372bb9
yilin.qyl <yilin.qyl@alipay.com>
2018-12-17 16:34:59 +0800

    chore: sort out graph base class

---
+++ test/unit/base-spec.js
@@ -0,0 +1,27 @@
+const expect = require('chai').expect;
+const Base = require('../../src/base');
+
+describe('base test', () => {
+  const base = new Base();
+  it('new base', () => {
+    const baseInst = new Base({ test: 'aaaa' });
+    expect(baseInst).not.to.be.undefined;
+    expect(baseInst._cfg).not.to.be.undefined;
+    expect(baseInst._cfg.test).to.equal('aaaa');
+  });
+  it('base getter & setter', ()=> {
+    base.set('a', 'a');
+    expect(base.get('a')).to.equal('a');
+  });
+  it('base events', () => {
+    let triggered = false;
+    base.on('event', () => { triggered = true; });
+    base.emit('event');
+    expect(triggered).to.be.true;
+
+    triggered = false;
+    base.removeEvent('event');
+    base.emit('event');
+    expect(triggered).to.be.false;
+  });
+});
