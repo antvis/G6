cc80e4a8bedeeb766ab6d8aa39838f0765901f6e
dxq613 <dxq613@gmail.com>
2018-12-21 18:08:18 +0800

    feat(shape):add shape and register node

--- src/global.js
+++ src/global.js
@@ -4,16 +4,23 @@
  */
 
 module.exports = {
-  defaultNodeSize: 40,
-  labelStyle: {
-    fill: '#595959',
-    textAlign: 'center',
-    textBaseline: 'middle'
+  nodeSize: 40,
+  edgeSize: 2,
+  nodeLabel: {
+    style: {
+      fill: '#595959',
+      textAlign: 'center',
+      textBaseline: 'middle'
+    },
+    offset: 5 // 节点的默认文本不居中时的偏移量
   },
-  groupStyle: {
-    stroke: '#CED4D9',
-    fill: '#F2F4F5',
-    radius: 2
-  },
-  groupBackgroundPadding: [ 40, 10, 10, 10 ]
+  // 节点应用状态后的样式，默认仅提供 active 和 selected 用户可以自己扩展
+  nodeStateStyle: {
+    active: {
+      fillOpacity: 0.8
+    },
+    selected: {
+      lineWidth: 2
+    }
+  }
 };
