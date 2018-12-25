9ac89ef507d7e426443b6665c49f68d27fe85abc
dxq613 <dxq613@gmail.com>
2018-12-25 19:50:49 +0800

    feat(edge): add edge

--- src/global.js
+++ src/global.js
@@ -9,6 +9,8 @@ module.exports = {
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
   edgeSize: 2,
+  nodeColor: '#333',
+  edgeColor: '#333',
   nodeLabel: {
     style: {
       fill: '#595959',
@@ -17,8 +19,26 @@ module.exports = {
     },
     offset: 5 // 节点的默认文本不居中时的偏移量
   },
+  edgeLabel: {
+    style: {
+      fill: '#595959',
+      textAlign: 'center',
+      textBaseline: 'middle'
+    },
+    refX: 0, // 沿着边的切线方向的偏移
+    refY: 0,  // 垂直于边的方向的偏移
+    autoRotate: false // 是否自动旋转，保证文本沿着边的切线方向
+  },
   // 节点应用状态后的样式，默认仅提供 active 和 selected 用户可以自己扩展
   nodeStateStyle: {
+    active: {
+      fillOpacity: 0.8
+    },
+    selected: {
+      lineWidth: 2
+    }
+  },
+  labelStateStyle: {
     active: {
       fillOpacity: 0.8
     },
--- src/shape/node.js
+++ src/shape/node.js
@@ -6,41 +6,30 @@
 const Shape = require('./shape');
 const Util = require('../util/index');
 const Global = require('../global');
-
+const SingleShapeMixin = require('./single-shape-mixin');
 const CLS_SHAPE = 'node-shape';
-const CLS_LABEL = 'node-label';
-const SUFFIX_STATE = '-state-style';
-
-// 根据指定的属性获取图形当前的属性
-function getCurrentStyle(shape, style) {
-  const rst = {};
-  Util.each(style, (v, k) => {
-    rst[k] = shape.attr(k);
-  });
-  return rst;
-}
 
 // 注册 Node 的工厂方法
 Shape.registerFactory('node', {
   defaultShapeType: 'circle'
 });
 
-// 单个图形的基础，可以有 label，默认 label 居中
-Shape.registerNode('single-shape', {
-	// 单个图形的类型
+const singleNodeDefinition = Util.mix({}, SingleShapeMixin, {
+  itemType: 'node',
+  // 单个图形的类型
   shapeType: '',
-	/**
-	 * 文本相对图形的位置，默认以中心点
-	 * 位置包括： top, bottom, left, right, center
-	 * @type {String}
-	 */
+  /**
+   * 文本相对图形的位置，默认以中心点
+   * 位置包括： top, bottom, left, right, center
+   * @type {String}
+   */
   labelPosition: 'center',
-	/**
-	 * 获取节点宽高
-	 * @internal 返回节点的大小，以 [width, height] 的方式维护
-	 * @param  {Object} cfg 节点的配置项
-	 * @return {Array} 宽高
-	 */
+  /**
+   * 获取节点宽高
+   * @internal 返回节点的大小，以 [width, height] 的方式维护
+   * @param  {Object} cfg 节点的配置项
+   * @return {Array} 宽高
+   */
   getSize(cfg) {
     let size = cfg.size || Global.nodeSize;
     if (!Util.isArray(size)) {
@@ -48,19 +37,10 @@ Shape.registerNode('single-shape', {
     }
     return size;
   },
-	/**
-	 * 获取图形的配置项
-	 * @internal 仅在定义这一类节点使用，用户创建和更新节点
-	 * @param  {Object} cfg 节点的配置项
-	 * @return {Object} 图形的配置项
-	 */
-  getShapeStyle(cfg) {
-    return cfg.style;
-  },
-	// 私有方法，不希望扩展的节点复写这个方法
-  _getLabelStyleByPosition(cfg) {
+  // 私有方法，不希望扩展的节点复写这个方法
+  getLabelStyleByPosition(cfg) {
     const labelPosition = cfg.labelPosition || this.labelPosition;
-		// 默认的位置（最可能的情形），所以放在最上面
+    // 默认的位置（最可能的情形），所以放在最上面
     if (labelPosition === 'center') {
       return { x: 0, y: 0 };
     }
@@ -101,104 +81,18 @@ Shape.registerNode('single-shape', {
     }
     return style;
   },
-	/**
-	 * 获取文本的配置项
-	 * @internal 仅在定义这一类节点使用，用户创建和更新节点
-	 * @param  {Object} cfg 节点的配置项
-	 * @return {Object} 图形的配置项
-	 */
-  getLabelStyle(cfg) {
-    const calculateStyle = this._getLabelStyleByPosition(cfg);
-    calculateStyle.text = cfg.label;
-    const labelStyle = Util.mix({}, Global.nodeLabel.style, calculateStyle, cfg.labelStyle);
-    return labelStyle;
-  },
-  drawLabel(cfg, group) {
-    const labelStyle = this.getLabelStyle(cfg);
-    const label = group.addShape('text', {
-      className: CLS_LABEL,
-      attrs: labelStyle
-    });
-    return label;
-  },
-	/**
-	 * 绘制节点，包含文本
-	 * @override
-	 * @param  {Object} cfg 节点的配置项
-	 * @param  {G.Group} group 节点的容器
-	 * @return {G.Shape} 绘制的图形
-	 */
-  draw(cfg, group) {
+  drawShape(cfg, group) {
     const shapeType = this.shapeType || this.type;
     const style = this.getShapeStyle(cfg);
     const shape = group.addShape(shapeType, {
       className: CLS_SHAPE,
       attrs: style
     });
-    if (cfg.label) {
-      this.drawLabel(cfg, group);
-    }
     return shape;
-  },
-	/**
-	 * 更新节点，包含文本
-	 * @override
-	 * @param  {Object} cfg 节点的配置项
-	 * @param  {G6.Node} node 节点
-	 */
-  update(cfg, node) {
-    const group = node.getContainer();
-    const shape = group.findByClassName(CLS_SHAPE);
-    const shapeStyle = this.getShapeStyle(cfg);
-    shape.attr(shapeStyle);
-    const label = group.findByClassName(CLS_LABEL);
-		// 此时需要考虑之前是否绘制了 label 的场景存在三种情况
-		// 1. 更新时不需要 label，但是原先存在 label，此时需要删除
-		// 2. 更新时需要 label, 但是原先不存在，创建节点
-		// 3. 如果两者都存在，更新
-    if (!cfg.label) {
-      label && label.remove();
-    } else {
-      if (!label) {
-        this.drawLabel(cfg, group);
-      } else {
-        const labelStyle = this.getLabelStyle(cfg);
-        label.attr(labelStyle);
-      }
-    }
-  },
-  getStateStyle(name, node) {
-    const defaultStyle = Global.nodeStateStyle[name];
-    const fieldName = name + 'Style'; // 状态名 + style（activeStyle) 存储在 node 中，如果 item 中不存在这些信息，则使用默认的样式
-    const style = Util.mix({}, defaultStyle, node.get(fieldName));
-    return style;
-  },
-	/**
-	 * 设置节点的状态，主要是交互状态，业务状态请在 draw 方法中实现
-	 * 单图形的节点仅考虑 selected、active 状态，有其他状态需求的用户自己复写这个方法
-	 * @override
-	 * @param  {String} name 状态名称
-	 * @param  {Object} value 状态值
-	 * @param  {G6.Node} node 节点
-	 */
-  setState(name, value, node) {
-    const group = node.getContainer();
-    const shape = group.findByClassName(CLS_SHAPE);
-    const cacheName = name + SUFFIX_STATE;
-
-    if (value) { // 如果设置状态
-      const style = this.getStateStyle(name, node);
-      const cacheStyle = getCurrentStyle(shape, style);
-      shape.attr(style);
-      shape.set(cacheName, cacheStyle);
-    } else { // 取消状态
-      const originStyle = shape.get(cacheName);
-      if (originStyle) {
-        shape.attr(originStyle);
-      }
-    }
   }
 });
+// 单个图形的基础，可以有 label，默认 label 居中
+Shape.registerNode('single-shape', singleNodeDefinition);
 
 /**
  * 基本的圆，可以添加文本，默认文本居中
