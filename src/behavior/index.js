7e9baa83d5e73e7ec23257cfbc7164295bbc5b61
yilin.qyl <yilin.qyl@alipay.com>
2018-12-28 20:27:28 +0800

    feat(behavior): add behavior: zoom canvas

--- src/behavior/index.js
+++ src/behavior/index.js
@@ -1,7 +1,8 @@
 const Util = require('../util');
 const Behavior = require('./behavior');
 const behaviors = {
-  'drag-canvas': require('./drag-canvas')
+  'drag-canvas': require('./drag-canvas'),
+  'zoom-canvas': require('./zoom-canvas')
 };
 Util.each(behaviors, (behavior, type) => {
   Behavior.registerBehavior(type, behavior);
