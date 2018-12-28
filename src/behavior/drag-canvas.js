74f95a14e107a25dfbb8db0aada8b6933637d0f0
yilin.qyl <yilin.qyl@alipay.com>
2018-12-28 18:14:39 +0800

    feat(behavior): add behavior: drag canvas

---
+++ src/behavior/drag-canvas.js
@@ -0,0 +1,80 @@
+const Util = require('../util');
+const defaultCfg = {
+  direction: 'both',
+  shouldUpdate() { return true; }
+};
+
+module.exports = {
+  bind(graph) {
+    this._cfg = Util.mix({}, defaultCfg, this._cfg);
+    this.group = graph.get('group');
+    this.graph = graph;
+    this.events = {
+      mousedown: Util.wrapBehavior(this, 'onMouseDown'),
+      mousemove: Util.wrapBehavior(this, 'onMouseMove'),
+      mouseup: Util.wrapBehavior(this, 'onMouseUp')
+    };
+    Util.each(this.events, (callback, event) => {
+      graph.on('canvas:' + event, callback);
+    });
+  },
+  unbind(graph) {
+    Util.each(this.events, (callback, event) => {
+      graph.off('canvas:' + event, callback);
+    });
+  },
+  updateViewport(e) {
+    const origin = this.origin;
+    const clientX = +e.clientX;
+    const clientY = +e.clientY;
+    if (isNaN(clientX) || isNaN(clientY)) {
+      return;
+    }
+    let dx = clientX - origin.x;
+    let dy = clientY - origin.y;
+    if (this.get('direction') === 'x') {
+      dy = 0;
+    } else if (this.get('direction') === 'y') {
+      dx = 0;
+    }
+    this.origin = {
+      x: clientX,
+      y: clientY
+    };
+    this.graph.translate(dx, dy);
+    this.graph.paint();
+  },
+  onMouseDown(e) {
+    this.origin = { x: e.clientX, y: e.clientY };
+    this.dragging = false;
+  },
+  onMouseMove(e) {
+    if (!this.origin) { return; }
+    if (this.origin && !this.dragging) {
+      e.type = 'dragstart';
+      this.graph.emit('canvas:dragstart', e);
+      this.dragging = true;
+      return;
+    }
+    if (this.dragging) {
+      e.type = 'drag';
+      this.graph.emit('canvas:drag', e);
+    }
+    if (this.get('shouldUpdate').call(this, e)) {
+      this.updateViewport(e);
+    }
+  },
+  onMouseUp(e) {
+    if (!this.dragging) {
+      return;
+    }
+    const graph = this.graph;
+    if (this.get('shouldUpdate').call(this, e)) {
+      this.updateViewport(e);
+    }
+    e.type = 'dragend';
+    graph.emit('canvas:dragend', e);
+    this.origin = null;
+    this.dragging = false;
+  }
+};
--- src/behavior/index.js
+++ src/behavior/index.js
@@ -1,2 +1,9 @@
+const Util = require('../util');
 const Behavior = require('./behavior');
+const behaviors = {
+  'drag-canvas': require('./drag-canvas')
+};
+Util.each(behaviors, (behavior, type) => {
+  Behavior.registerBehavior(type, behavior);
+});
 module.exports = Behavior;
