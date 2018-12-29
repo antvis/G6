ee9d5be33d630c31f47bbb91c15254eec3c4bded
yilin.qyl <yilin.qyl@alipay.com>
2018-12-29 18:10:21 +0800

    chore(behavior): add default interface shouldXxx for behavior

--- src/behavior/index.js
+++ src/behavior/index.js
@@ -2,7 +2,8 @@ const Util = require('../util');
 const Behavior = require('./behavior');
 const behaviors = {
   'drag-canvas': require('./drag-canvas'),
-  'zoom-canvas': require('./zoom-canvas')
+  'zoom-canvas': require('./zoom-canvas'),
+  'drag-node': require('./drag-node')
 };
 Util.each(behaviors, (behavior, type) => {
   Behavior.registerBehavior(type, behavior);
