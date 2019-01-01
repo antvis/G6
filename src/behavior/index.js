9846e77c01a8560fdd662a3c942cacf6229f3d01
yilin.qyl <yilin.qyl@alipay.com>
2019-01-01 22:02:54 +0800

    feat(behavior): add behavior click to select

--- src/behavior/index.js
+++ src/behavior/index.js
@@ -3,7 +3,8 @@ const Behavior = require('./behavior');
 const behaviors = {
   'drag-canvas': require('./drag-canvas'),
   'zoom-canvas': require('./zoom-canvas'),
-  'drag-node': require('./drag-node')
+  'drag-node': require('./drag-node'),
+  'click-select': require('./click-select')
 };
 Util.each(behaviors, (behavior, type) => {
   Behavior.registerBehavior(type, behavior);
