54b0f6625c86ce312e5020a0ff45508230d24763
Elaine1234 <yilin.qyl@antfin.com>
2019-01-24 15:22:06 +0800

    feat(tree-graph): add collapse & expand

--- src/graph/graph.js
+++ src/graph/graph.js
@@ -3,8 +3,8 @@
  * @author huangtonger@aliyun.com
  */
 
-const EventEmitter = require('@antv/g/lib/').EventEmitter;
 const G = require('@antv/g/lib');
+const EventEmitter = G.EventEmitter;
 const Util = require('../util');
 const Global = require('../global');
 
@@ -629,6 +629,27 @@ class Graph extends EventEmitter {
     this.get('eventController').destroy();
     this.get('canvas').destroy();
     this.destroyed = true;
+  }
+  stopAnimate() {
+    const self = this;
+    const canvas = self.get('canvas');
+    canvas.stopAnimate();
+    self.set('animating', false);
+  }
+  positionAnimate(fn, duration, ease, onFinish, delay = 0) {
+    const self = this;
+    const canvas = self.get('canvas');
+    if (self.get('animating')) {
+      canvas.stopAnimate();
+    }
+    self.set('animating', true);
+    canvas.animate({ onFrame(ratio) {
+      fn(ratio);
+      self.refreshPositions();
+    } }, duration, ease, () => {
+      onFinish();
+      self.set('animating', false);
+    }, delay);
   }
 }
 
