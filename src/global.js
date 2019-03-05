7c5c360c38635e7198614f5a53642d71c3f16ed6
Elaine1234 <yilin.qyl@antfin.com>
2019-03-05 18:05:54 +0800

    chore(item): add test case of update position

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.0.0-beta.14',
+  version: '3.0.0-beta.15',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
