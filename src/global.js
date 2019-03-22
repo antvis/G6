8eb8b200890c0f0f8669b75906597866d6732cd1
Elaine1234 <yilin.qyl@antfin.com>
2019-03-22 10:55:32 +0800

    chore(package): update package version

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.0.0-rc.4',
+  version: '3.0.0',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
