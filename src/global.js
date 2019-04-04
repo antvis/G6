2e9f25cfd1b960633df4b89b8411ff7ef1e1a787
yilin.qyl <yilin.qyl@alipay.com>
2019-04-04 14:53:24 +0800

    chore(package): update package version

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.0.0',
+  version: '3.0.1',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
