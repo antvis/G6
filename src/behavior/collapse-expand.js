00f7aa7f76f4ecb15814710ae65e3f34120dbf81
elaine <yilin.qyl@alipay.com>
2019-04-26 17:38:32 +0800

    chore(tree-graph): modify behavior

--- src/behavior/collapse-expand.js
+++ src/behavior/collapse-expand.js
@@ -14,17 +14,17 @@ module.exports = {
   },
   onNodeClick(e) {
     const item = e.item;
-    const rawData = this.graph.findDataById(item.getModel().id);
-    const children = rawData.children;
+    const model = item.getModel();
+    const children = model.children;
     // 叶子节点的收缩和展开没有意义
     if (!children || children.length === 0) {
       return;
     }
-    const collapsed = !rawData.collapsed;
+    const collapsed = !model.collapsed;
     if (!this.shouldBegin(e, collapsed)) {
       return;
     }
-    rawData.collapsed = collapsed;
+    model.collapsed = collapsed;
     this.graph.emit('itemcollapsed', { item: e.item, collapsed });
     if (!this.shouldUpdate(e, collapsed)) {
       return;
