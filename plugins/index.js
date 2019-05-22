25e7e01811e1269d3e59842432b44d059a3f4411
elaine <yilin.qyl@alipay.com>
2019-05-22 11:46:31 +0800

    chore: export newly added plugins

--- plugins/index.js
+++ plugins/index.js
@@ -1,6 +1,9 @@
 const G6Plugins = {
   Minimap: require('./minimap'),
-  Grid: require('./grid')
+  Grid: require('./grid'),
+  Force: require('./force'),
+  Dagre: require('./dagre'),
+  Menu: require('./menu')
 };
 
 module.exports = G6Plugins;
