17b3d1984ac58cee7bf54723cb17a6bc874b03bb
elaine <yilin.qyl@alipay.com>
2019-05-24 11:40:54 +0800

    chore: update package version

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.0.5-beta.2',
+  version: '3.0.5-beta.3',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
