b16b3407537fa17f2588e152a7da48f872ebf231
shiwu.wyy <shiwu.wyy@antfin.com>
2019-07-18 17:57:31 +0800

    radial layout

--- plugins/index.js
+++ plugins/index.js
@@ -2,6 +2,7 @@ const G6Plugins = {
   Minimap: require('./minimap'),
   Grid: require('./grid'),
   Force: require('./force'),
+  Radial: require('./radial'),
   Dagre: require('./dagre'),
   Menu: require('./menu')
 };
