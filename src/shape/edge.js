d14f6ce6bf61a5ad14d846cfa322d1cc74d7baac
shiwu.wyy <shiwu.wyy@antfin.com>
2019-08-07 14:08:59 +0800

    begin adding arc edge

--- src/shape/edge.js
+++ src/shape/edge.js
@@ -177,6 +177,15 @@ Shape.registerEdge('spline', {
   }
 }, 'single-line');
 
+Shape.registerEdge('arc', {
+  getPath(points) {
+    const path = [];
+    path.push([ 'M', points[0].x, points[0].y ]);
+    path.push([ 'A', radius, radius, 0, 0, flag, points[1].x, points[1].y ]);
+    return path;
+  }
+}, 'single-line');
+
 Shape.registerEdge('quadratic', {
   curvePosition: 0.5, // 弯曲的默认位置
   curveOffset: -20, // 弯曲度，沿着startPoint, endPoint 的垂直向量（顺时针）方向，距离线的距离，距离越大越弯曲
