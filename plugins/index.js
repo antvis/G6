5b5edab299026f7dc0fc1987af7e7d524051e2eb
shiwu.wyy <shiwu.wyy@antfin.com>
2019-09-29 18:02:42 +0800

    add: polyline test; delete: layouts in plugins; fix: new format of edge shape.

--- plugins/index.js
+++ plugins/index.js
@@ -1,14 +1,8 @@
 const G6Plugins = {
   Minimap: require('./minimap'),
   Grid: require('./grid'),
-  Force: require('./force'),
-  Radial: require('./radial'),
-  Fruchterman: require('./fruchterman'),
-  Mds: require('./mds'),
-  Dagre: require('./dagre'),
   Menu: require('./menu'),
-  Circular: require('./circular'),
-  Bundling: require('./Bundling')
+  Bundling: require('./bundling')
 };
 
 module.exports = G6Plugins;
