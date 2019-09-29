f87d425c5e72b28edc3c55e1af5161371a4604e0
shiwu.wyy <shiwu.wyy@antfin.com>
2019-09-29 19:15:13 +0800

    fix: test of bundling with circular layout plugin problem

--- test/unit/plugins/bundling-spec.js
+++ test/unit/plugins/bundling-spec.js
@@ -1,6 +1,5 @@
 const expect = require('chai').expect;
 const G6 = require('../../../src');
-const Circular = require('../../../plugins/circular');
 const Bundling = require('../../../plugins/bundling');
 const data = require('./data.json');
 
@@ -8,22 +7,19 @@ const div = document.createElement('div');
 div.id = 'force-layout';
 document.body.appendChild(div);
 
-// function mathEqual(a, b) {
-//   return Math.abs(a - b) < 1;
-// }
-
 describe('edge bundling', () => {
   const graph = new G6.Graph({
     container: div,
     width: 500,
     height: 500,
+    layout: {
+      type: 'circular'
+    },
     defaultNode: { size: 10 }
   });
+  graph.data(data);
+  graph.render();
   it('edge bundling on circular layout with default configs', () => {
-    const circularLayout = new Circular({ center: [ 250, 250 ] });
-    circularLayout.initPlugin(graph);
-    circularLayout.layout(data);
-
     const bundle = new Bundling();
     bundle.initPlugin(graph);
     bundle.bundling(data);
@@ -33,10 +29,6 @@ describe('edge bundling', () => {
   });
 
   it('bundling on circular with fixed bundleThreshold and iterations', () => {
-    const circularLayout = new Circular({ center: [ 250, 250 ] });
-    circularLayout.initPlugin(graph);
-    circularLayout.layout(data);
-
     const bundle = new Bundling({
       bundleThreshold: 0.1,
       iterations: 120
@@ -51,27 +43,25 @@ describe('edge bundling', () => {
   it('bundling update', () => {
     const data2 = {
       nodes: [
-        { id: '0' }, { id: '1' }
+        { id: 'n0' }, { id: 'n1' }
       ],
       edges: [
-        { source: '0', target: '1' }
+        { source: 'n0', target: 'n1' }
       ]
     };
-    const circularLayout = new Circular({ center: [ 250, 250 ] });
-    circularLayout.initPlugin(graph);
-    circularLayout.layout(data2);
+    graph.changeData(data2);
 
     const bundle = new Bundling();
     bundle.initPlugin(graph);
     bundle.bundling(data2);
 
     data2.nodes = [
-      { id: '0', x: 10, y: 100 }, { id: '1', x: 100, y: 100 }, { id: '2', x: 10, y: 10 }
+      { id: 'n0', x: 10, y: 100 }, { id: 'n1', x: 100, y: 100 }, { id: 'n2', x: 10, y: 10 }
     ];
     data2.edges = [
-      { source: '0', target: '1' },
-      { source: '1', target: '2' },
-      { source: '0', target: '2' }
+      { source: 'n0', target: 'n1' },
+      { source: 'n1', target: 'n2' },
+      { source: 'n0', target: 'n2' }
     ];
 
     bundle.updateBundling({
@@ -92,6 +82,6 @@ describe('edge bundling', () => {
       bundle.bundling(data);
     }
     expect(fn).to.throw;
-
+    graph.destroy();
   });
 });
