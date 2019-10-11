a0120e8d59f44df7785d97e0bd76b81e4d41df1f
zhanning.bzn <zhanning.bzn@antfin.com>
2019-10-11 09:42:12 +0800

    update package version

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.1.0',
+  version: '3.1.1',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
