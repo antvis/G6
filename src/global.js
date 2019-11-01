7aa2450be56706581dc7d8d0c6aa509675950857
baizn <576375879@qq.com>
2019-11-01 10:44:03 +0800

    fix: update package version

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.1.4',
+  version: '3.1.5',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
