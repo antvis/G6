a9a6b622c54e86735c902d8880ef0b42c05f66ab
zhanning.bzn <zhanning.bzn@antfin.com>
2019-11-15 15:40:13 +0800

    WIP: G6 迁移新官网，添加css样式

--- examples/case/simplifyCluster/demo/index.js
+++ examples/case/simplifyCluster/demo/index.js
@@ -1,4 +1,18 @@
 import G6 from '@antv/g6';
+import insertCss from 'insert-css';
+
+insertCss(`
+  .g6-tooltip {
+    border: 1px solid #e2e2e2;
+    border-radius: 4px;
+    font-size: 12px;
+    color: #545454;
+    background-color: rgba(255, 255, 255, 0.9);
+    padding: 10px 8px;
+    box-shadow: rgb(174, 174, 174) 0px 0px 10px;
+  }
+`);
+
 /**
    * 该示例演示 Fruchterman 布局算法的聚类效果，以及点击交互进行聚类的聚合和扩散效果
    * by 十吾
