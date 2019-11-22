914213231164f17fabbe157a0f993f988f2d2454
Yanyan-Wang <yanyanwang93@gmail.com>
2019-11-22 19:29:03 +0800

    fix: webworker for case simplify

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
-window.g6 = require('./src/index.js');
+window.g6 = require('./build/g6.js');
 window.insertCss = require('insert-css');
 window.sankey = require('d3-sankey');
 window.d3 = require('d3');
