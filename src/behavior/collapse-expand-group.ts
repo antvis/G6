ede6b9fba846e78563b4f48e8000bc8fcb0d940e
Yanyan-Wang <yanyanwang93@gmail.com>
2019-12-17 14:31:14 +0800

    add: two more behavior ts files

---
+++ src/behavior/collapse-expand-group.ts
@@ -0,0 +1,45 @@
+/*
+ * @Author: moyee
+ * @Date: 2019-07-31 14:36:15
+ * @LastEditors: moyee
+ * @LastEditTime: 2019-08-22 18:43:24
+ * @Description: 收起和展开群组
+ */
+
+import { G6Event, IG6GraphEvent } from "@g6/types";
+
+const DEFAULT_TRIGGER = 'dblclick';
+const ALLOW_EVENTS = [ 'click', 'dblclick' ];
+export default {
+  getDefaultCfg(): object {
+    return {
+      trigger: DEFAULT_TRIGGER
+    };
+  },
+  getEvents(): { [key in G6Event]?: string } {
+    let trigger;
+    // 检测输入是否合法
+    if (ALLOW_EVENTS.includes(this.trigger)) {
+      trigger = this.trigger;
+    } else {
+      trigger = DEFAULT_TRIGGER;
+      console.warn('Behavior collapse-expand-group的trigger参数不合法，请输入click或dblclick');
+    }
+    return {
+      [`${trigger}`]: 'onGroupClick'
+    };
+  },
+  onGroupClick(evt: IG6GraphEvent) {
+    const { target } = evt;
+
+    const groupId = target.get('groupId');
+    if (!groupId) {
+      return false;
+    }
+
+    const graph = this.graph;
+
+    const customGroupControll = graph.get('customGroupControll');
+    customGroupControll.collapseExpandGroup(groupId);
+  }
+};
