f9ef6c5e6658d4a2cd00b7ab2aa11dec99046f24
zhanning.bzn <zhanning.bzn@antfin.com>
2019-12-18 11:29:59 +0800

    fix: refactor behavior

--- src/behavior/behavior.ts
+++ src/behavior/behavior.ts
@@ -1,19 +1,41 @@
-import clone from '@antv/util/lib/clone'
+import { clone, each, wrapBehavior } from '@antv/util/lib';
 import { BehaviorOpation } from '@g6/types';
-import BehaviorOption from './behaviorOption'
+import behaviorOption from './behaviorOption'
 
 export default class Behavior {
+  // 所有自定义的 Behavior 的实例
   private static types = {}
+  /**
+   * 自定义 Behavior
+   * @param type Behavior 名称
+   * @param behavior Behavior 定义的方法集合
+   */
   public static registerBehavior<T, U>(type: string, behavior: BehaviorOpation<U>) {
     if(!behavior) {
       throw new Error(`please specify handler for this behavior: ${type}`)
     }
 
-    const _proptype = clone(BehaviorOption.prototype)
+    const _proptype = clone(behaviorOption)
    
     Object.assign(_proptype, behavior)
 
-    const base = function() {}
+    const base = function(cfg: object) {
+      const self = this
+      const events = self.getEvents()
+      self._events = null
+
+      Object.assign(self, self.getDefaultCfg(), cfg)
+      
+      const eventsToBind = {}
+
+      if(events) {
+        each(events, (handle, event) => {
+          eventsToBind[event] = wrapBehavior(self, handle)
+        })
+        self._events = eventsToBind
+      }
+    }
+
     base.prototype = _proptype
     
     this.types[type] = base
