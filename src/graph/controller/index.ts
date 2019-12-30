4f5fb6ed1e36aa2a63ae2fe0d4487fe0c0b46685
Deturium <fanyangliang@gmail.com>
2019-12-30 10:30:38 +0800

    faet: complete layout controller

--- src/graph/controller/index.ts
+++ src/graph/controller/index.ts
@@ -1,6 +1,7 @@
-export { default as ModeController } from './mode'
-export { default as ViewController } from './view'
-export { default as EventController } from './event'
-export { default as ItemController } from './item'
-export { default as StateController } from './state'
-export { default as CustomGroup } from './customGroup'
+export { default as ModeController } from './mode';
+export { default as ViewController } from './view';
+export { default as EventController } from './event';
+export { default as ItemController } from './item';
+export { default as LayoutController } from './layout';
+export { default as StateController } from './state';
+export { default as CustomGroup } from './customGroup';
