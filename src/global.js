b20d2234392ce57283995a093df5f408391a6d04
baizn <576375879@qq.com>
2020-01-16 10:47:42 +0800

    feat: update package version number

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-  version: '3.2.8',
+  version: '3.2.9',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
