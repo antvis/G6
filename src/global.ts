87a149a7ca1063ad68fb6f850dbffce1dc4151d2
baizn <576375879@qq.com>
2020-02-05 20:34:59 +0800

    fix: 修复发布后lib目录不是最新的问题

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,5 @@
 export default {
-  version: '3.3.0-beta.4',
+  version: '3.3.0-beta.5',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
