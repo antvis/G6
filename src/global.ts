aecd3b284af18b54f57f7fdf0582d96bd87ab225
Yanyan-Wang <yanyanwang93@gmail.com>
2020-02-05 11:30:31 +0800

    update: version

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,5 @@
 export default {
-  version: '3.3.0-beta.2',
+  version: '3.3.0-beta.3',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
