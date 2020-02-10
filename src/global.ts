79c9ec97aa0d9d486a39ebd2f5d53f6707316f04
baizn <576375879@qq.com>
2020-02-10 22:09:16 +0800

    feat: update package version number

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,5 @@
 export default {
-  version: '3.3.0-beta.7',
+  version: '3.3.0',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
