d54897ac49de1ece2ccdf623f55ed523371034ab
Yanyan-Wang <yanyanwang93@gmail.com>
2020-03-20 17:38:16 +0800

    fix: dagre with controlPoints and loop edges. closes: #1328.

--- stories/Shape/component/polyline.tsx
+++ stories/Shape/component/polyline.tsx
@@ -3,31 +3,201 @@ import G6 from '../../../src';
 import { IGraph } from '../../../src/interface/graph';
 
 let graph: IGraph = null;
+
 const data = {
   nodes: [
-  {
-  id: "2",
-  label: "2",
-  },
-  {
-  id: "3",
-  label: "3",
-  },
-  {
-  id: "4",
-  label: "4",
-  }
+    {
+      id: "1",
+      dataType: "alps",
+      name: "alps_file1",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "2",
+      dataType: "alps",
+      name: "alps_file2",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "3",
+      dataType: "alps",
+      name: "alps_file3",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "4",
+      dataType: "sql",
+      name: "sql_file1",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "5",
+      dataType: "sql",
+      name: "sql_file2",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "6",
+      dataType: "feature_etl",
+      name: "feature_etl_1",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "7",
+      dataType: "feature_etl",
+      name: "feature_etl_1",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    },
+    {
+      id: "8",
+      dataType: "feature_extractor",
+      name: "feature_extractor",
+      conf: [
+        {
+          label: "conf",
+          value: "pai_graph.conf"
+        },
+        {
+          label: "dot",
+          value: "pai_graph.dot"
+        },
+        {
+          label: "init",
+          value: "init.rc"
+        }
+      ]
+    }
   ],
   edges: [
-  // 配置内置折线的弯折弧度、端点最小距离
-  {
-  source: "2",
-  target: "3"
-  },
-  {
-  source: "2",
-  target: "4"
-  }
+    {
+      source: "1",
+      target: "1",
+      type: "loop",
+      // controlPoints: [{x: 50, y: 100}]
+    },
+    {
+      source: "1",
+      target: "2"
+    },
+    {
+      source: "1",
+      target: "3"
+    },
+    {
+      source: "2",
+      target: "4"
+    },
+    {
+      source: "3",
+      target: "4"
+    },
+    {
+      source: "4",
+      target: "5"
+    },
+    {
+      source: "5",
+      target: "6"
+    },
+    {
+      source: "6",
+      target: "7"
+    },
+    {
+      source: "6",
+      target: "8"
+    }
   ]
 };
 const Polyline = () => {
@@ -38,19 +208,28 @@ const Polyline = () => {
 
       const graph = new G6.Graph({
         container: container.current as string | HTMLElement,
-        width: 500,
-        height: 500,
-        linkCenter: true,
+        width: 1000,
+        height: 800,
         layout: {
-          type: "fruchterman"
+          type: "dagre",
+          rankdir: "LR",
+          align: "UL",
+          nodesep: 25,
+          ranksep: 50,
+          controlPoints: true
         },
         defaultEdge: {
-          shape: "polyline",
+          type: "polyline",
           style: {
-            radius: 10,
-            offset: 15,
+            radius: 20,
             endArrow: true,
-            stroke: "#F6BD16"
+            lineWidth: 2,
+            stroke: "#C2C8D5"
+          },
+          loopCfg: {
+            position: "left",
+            dist: 75,
+            clockwise: true
           }
         },
         modes: {
