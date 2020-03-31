152b4dedb387c1788d2e90e55c18399e179649fd
Yanyan-Wang <yanyanwang93@gmail.com>
2020-03-31 13:02:50 +0800

    fix: cubic with only one controlPoint error. fix: polyline without L attribute error.

--- stories/Layout/component/dagre-layout.tsx
+++ stories/Layout/component/dagre-layout.tsx
@@ -42,154 +42,154 @@ const data = {
         },
       ],
     },
-    {
-      id: '3',
-      type: 'alps',
-      name: 'alps_file3',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '4',
-      type: 'sql',
-      name: 'sql_file1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '5',
-      type: 'sql',
-      name: 'sql_file2',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '6',
-      type: 'feature_etl',
-      name: 'feature_etl_1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '7',
-      type: 'feature_etl',
-      name: 'feature_etl_1',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
-    {
-      id: '8',
-      type: 'feature_extractor',
-      name: 'feature_extractor',
-      conf: [
-        {
-          label: 'conf',
-          value: 'pai_graph.conf',
-        },
-        {
-          label: 'dot',
-          value: 'pai_graph.dot',
-        },
-        {
-          label: 'init',
-          value: 'init.rc',
-        },
-      ],
-    },
+    // {
+    //   id: '3',
+    //   type: 'alps',
+    //   name: 'alps_file3',
+    //   conf: [
+    //     {
+    //       label: 'conf',
+    //       value: 'pai_graph.conf',
+    //     },
+    //     {
+    //       label: 'dot',
+    //       value: 'pai_graph.dot',
+    //     },
+    //     {
+    //       label: 'init',
+    //       value: 'init.rc',
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: '4',
+    //   type: 'sql',
+    //   name: 'sql_file1',
+    //   conf: [
+    //     {
+    //       label: 'conf',
+    //       value: 'pai_graph.conf',
+    //     },
+    //     {
+    //       label: 'dot',
+    //       value: 'pai_graph.dot',
+    //     },
+    //     {
+    //       label: 'init',
+    //       value: 'init.rc',
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: '5',
+    //   type: 'sql',
+    //   name: 'sql_file2',
+    //   conf: [
+    //     {
+    //       label: 'conf',
+    //       value: 'pai_graph.conf',
+    //     },
+    //     {
+    //       label: 'dot',
+    //       value: 'pai_graph.dot',
+    //     },
+    //     {
+    //       label: 'init',
+    //       value: 'init.rc',
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: '6',
+    //   type: 'feature_etl',
+    //   name: 'feature_etl_1',
+    //   conf: [
+    //     {
+    //       label: 'conf',
+    //       value: 'pai_graph.conf',
+    //     },
+    //     {
+    //       label: 'dot',
+    //       value: 'pai_graph.dot',
+    //     },
+    //     {
+    //       label: 'init',
+    //       value: 'init.rc',
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: '7',
+    //   type: 'feature_etl',
+    //   name: 'feature_etl_1',
+    //   conf: [
+    //     {
+    //       label: 'conf',
+    //       value: 'pai_graph.conf',
+    //     },
+    //     {
+    //       label: 'dot',
+    //       value: 'pai_graph.dot',
+    //     },
+    //     {
+    //       label: 'init',
+    //       value: 'init.rc',
+    //     },
+    //   ],
+    // },
+    // {
+    //   id: '8',
+    //   type: 'feature_extractor',
+    //   name: 'feature_extractor',
+    //   conf: [
+    //     {
+    //       label: 'conf',
+    //       value: 'pai_graph.conf',
+    //     },
+    //     {
+    //       label: 'dot',
+    //       value: 'pai_graph.dot',
+    //     },
+    //     {
+    //       label: 'init',
+    //       value: 'init.rc',
+    //     },
+    //   ],
+    // },
   ],
   edges: [
     {
       source: '1',
       target: '2',
     },
-    {
-      source: '1',
-      target: '3',
-    },
-    {
-      source: '2',
-      target: '4',
-    },
-    {
-      source: '3',
-      target: '4',
-    },
-    {
-      source: '4',
-      target: '5',
-    },
-    {
-      source: '5',
-      target: '6',
-    },
-    {
-      source: '6',
-      target: '7',
-    },
-    {
-      source: '6',
-      target: '8',
-    },
+    // {
+    //   source: '1',
+    //   target: '3',
+    // },
+    // {
+    //   source: '2',
+    //   target: '4',
+    // },
+    // {
+    //   source: '3',
+    //   target: '4',
+    // },
+    // {
+    //   source: '4',
+    //   target: '5',
+    // },
+    // {
+    //   source: '5',
+    //   target: '6',
+    // },
+    // {
+    //   source: '6',
+    //   target: '7',
+    // },
+    // {
+    //   source: '6',
+    //   target: '8',
+    // },
   ],
 };
 
@@ -254,7 +254,7 @@ const DagreLayout = () => {
           type: 'sql',
         },
         defaultEdge: {
-          type: 'polyline',
+          type: 'cubic',
           style: {
             radius: 20,
             offset: 45,
