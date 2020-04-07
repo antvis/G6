8e46b54fdd192914637645d9112fa24b9489912c
Yanyan-Wang <yanyanwang93@gmail.com>
2020-04-07 20:21:07 +0800

    feat: layout for combo. feat: nonoverlap for combo layout in first stage.

--- stories/Layout/layout.stories.tsx
+++ stories/Layout/layout.stories.tsx
@@ -6,6 +6,7 @@ import DagreLayout from './component/dagre-layout';
 import FruchtermanWorker from './component/fruchterman-worker-layout';
 import AddNodeLayout from './component/addNodeLayout'
 import ChangeData from './component/changeData'
+import G6ForceLayout from './component/g6force-layout';
 
 export default { title: 'Layout' };
 
@@ -15,3 +16,5 @@ storiesOf('Layout', module)
   .add('Fruchterman worker layout', () => <FruchtermanWorker />)
   .add('add node and layout', () => <AddNodeLayout />)
   .add('change data', () => <ChangeData />)
+  .add('G6 force layout', () => <G6ForceLayout />)
+  .add('Fruchterman worker layout', () => <FruchtermanWorker />);
