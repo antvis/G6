697cd9ec2866da8c3d4894cffc90db20cbb03731
Yanyan-Wang <yanyanwang93@gmail.com>
2020-04-08 13:51:16 +0800

    feat: combo force layout with preventOverlap.

--- src/layout/comboForce.ts
+++ src/layout/comboForce.ts
@@ -156,6 +156,7 @@ export default class ComboForce extends BaseLayout {
     self.initVals();
 
     // init the positions to make the nodes with same combo gather
+    self.initPos(comboMap);
 
     // iterate
     for (let i = 0; i < maxIteration; i++) {
@@ -481,6 +482,7 @@ export default class ComboForce extends BaseLayout {
     self.updateComboSizes(comboMap);
     self.calRepulsive(displacements, vecMap, comboMap);
     self.calAttractive(displacements, vecMap);
+    
     const preventComboOverlap = self.preventComboOverlap;
     if (preventComboOverlap) self.comboNonOverlapping(displacements, comboMap);
   }
