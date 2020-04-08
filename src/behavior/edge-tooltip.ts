4de519685559bf8e7063d86db010f1e225fb8102
Small <xiaozhu@v6plus.com>
2020-04-08 19:43:06 +0800

    feat: set offset as a option in tooltip

--- src/behavior/edge-tooltip.ts
+++ src/behavior/edge-tooltip.ts
@@ -6,6 +6,7 @@ export default Object.assign(
     getDefaultCfg(): object {
       return {
         item: 'edge',
+        offset: 12,
         formatText(model: EdgeConfig) {
           return `source: ${model.source} target: ${model.target}`;
         },
