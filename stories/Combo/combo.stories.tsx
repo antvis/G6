f7d39ee85e0a1ae9464c9fae58d216b39f83fded
Yanyan-Wang <yanyanwang93@gmail.com>
2020-04-14 18:29:22 +0800

    feat: registerCombo in first stage

--- stories/Combo/combo.stories.tsx
+++ stories/Combo/combo.stories.tsx
@@ -2,10 +2,14 @@
 import { storiesOf } from '@storybook/react';
 import React from 'react';
 import DefaultCombo from './component/default-combo';
+import RegisterCombo from './component/register-combo';
 
 export default { title: 'Combo' };
 
 storiesOf('Combo', module)
   .add('default combo', () => (
     <DefaultCombo />
+  ))
+  .add('register combo', () => (
+    <RegisterCombo />
   ));
