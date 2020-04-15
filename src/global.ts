8580eb02b362fe44d779d814d688e4e5c191f3f0
Yanyan-Wang <yanyanwang93@gmail.com>
2020-04-15 14:47:05 +0800

    fix: moveTo wrong logic problem. docs: update version number and CHANGELOG.

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,5 @@
 export default {
-  version: '3.4.5',
+  version: '3.4.6',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
