818b15338461d51d48b82582cff6521086675b52
Yanyan-Wang <yanyanwang93@gmail.com>
2020-04-27 20:28:33 +0800

    fix: curveOffset for arc, quadratic, cubic edge.

--- stories/Shape/shape.stories.tsx
+++ stories/Shape/shape.stories.tsx
@@ -7,28 +7,32 @@ import CustomNode from './component/custom-node';
 import Polyline from './component/polyline';
 import Image2 from './component/image2';
 import Quadratic from './component/quadratic';
+import HideItem from './component/hide-item';
 
 export default { title: 'Shape' };
 
 storiesOf('Shape', module)
-.add('default node', () => (
-  // 一个 add 表示添加一个 story
-  <DefaultShape />
-))
-.add('image node', () => (
-  // 一个 add 表示添加一个 story
-  <Image />
-))
-.add('image node 2', () => (
-  // 一个 add 表示添加一个 story
-  <Image2 />
-))
-.add('custom node', () => (
-  <CustomNode />
-))
-.add('polyline', () => (
-  <Polyline />
-))
-.add('quadratic', () => (
-  <Quadratic />
-));
+  .add('default node', () => (
+    // 一个 add 表示添加一个 story
+    <DefaultShape />
+  ))
+  .add('image node', () => (
+    // 一个 add 表示添加一个 story
+    <Image />
+  ))
+  .add('image node 2', () => (
+    // 一个 add 表示添加一个 story
+    <Image2 />
+  ))
+  .add('custom node', () => (
+    <CustomNode />
+  ))
+  .add('polyline', () => (
+    <Polyline />
+  ))
+  .add('quadratic', () => (
+    <Quadratic />
+  ))
+  .add('hide item', () => (
+    <HideItem />
+  ));
