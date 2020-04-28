1ec3c386c20b2615dd6c4604baa021885e514438
Yanyan-Wang <yanyanwang93@gmail.com>
2020-04-28 19:09:07 +0800

    fix: reconstructTree without parentId.

--- stories/Combo/combo.stories.tsx
+++ stories/Combo/combo.stories.tsx
@@ -8,6 +8,7 @@ import ComboLayoutCollapseExpand from './component/combo-layout-collapse-expand'
 import CollapseExpandVEdge from './component/collapse-expand-vedge';
 import ComboCollapseExpandTree from './component/combo-collapse-expand-tree';
 import ComboExample from './component/combo-example'
+import ComboDragCollapseExpand from './component/combo-drag-collapse-expand';
 
 export default { title: 'Combo' };
 
@@ -31,3 +32,6 @@ storiesOf('Combo', module)
     <ComboCollapseExpandTree />
   ))
   .add('combo example', () => <ComboExample />)
+  .add('drag collapse expand', () => (
+    <ComboDragCollapseExpand />
+  ));
