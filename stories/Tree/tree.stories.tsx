2ce92d628762e7a128348944678d7f307cf9bf5c
Yanyan-Wang <yanyanwang93@gmail.com>
2020-05-11 12:04:37 +0800

    fix: TreeGraphData type with style and stateStyles.

--- stories/Tree/tree.stories.tsx
+++ stories/Tree/tree.stories.tsx
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/react';
 import React from 'react';
 import FileSystem from './component/file-system';
 import FlowTree from './component/flow-tree'
+import TreeData from './component/tree-data';
 
 export default { title: 'Tree' };
 
 storiesOf('Tree', module)
   .add('indented tree file system', () => <FileSystem />)
   .add('flow-tree', () => <FlowTree />)
+  .add('tree-data', () => <TreeData />)
