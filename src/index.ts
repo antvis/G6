973337c9f836a58be94aba49b8083edad329d90b
多牧 <xuzhi.mxz@antfin.com>
2020-06-03 11:34:37 +0800

    feat: add register xml node to interface and stroy book

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import * as Algorithm from './algorithm';
 const registerNode = Shape.registerNode;
 const registerEdge = Shape.registerEdge;
 const registerCombo = Shape.registerCombo;
+const registerNodeByXML = Shape.registerNodeByXML;
 const registerBehavior = Behaviors.registerBehavior;
 const registerLayout = Layout.registerLayout;
 const Minimap = Plugins.Minimap;
@@ -57,6 +58,7 @@ export default {
   registerCombo: Shape.registerCombo,
   registerBehavior: Behaviors.registerBehavior,
   registerLayout: Layout.registerLayout,
+  registerNodeByXML: Shape.registerNodeByXML,
   Layout,
   Global,
   Minimap: Plugins.Minimap,
