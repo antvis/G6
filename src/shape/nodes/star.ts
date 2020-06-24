616faaff33adb6c49161e44c636923d6cb8a6ae0
huangz <zhangyuhang@souche.com>
2020-06-24 17:24:51 +0800

    fix: ellipse and star conflict

--- src/shape/nodes/star.ts
+++ src/shape/nodes/star.ts
@@ -78,11 +78,7 @@ Shape.registerNode(
           },
           className: `${this.type}-icon`,
           name: `${this.type}-icon`,
-<<<<<<< HEAD
           draggable: true
-=======
-          draggable: true,
->>>>>>> test: add test case for all node
         });
       }
 
