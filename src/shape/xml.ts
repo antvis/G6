ad2bf83bbb41ba1df4ce0c1d2504114e9b64048c
多牧 <xuzhi.mxz@antfin.com>
2020-06-24 11:15:38 +0800

    fix: xml node update state style error

--- src/shape/xml.ts
+++ src/shape/xml.ts
@@ -475,13 +475,10 @@ export function createNodeFromXML(gen: string | ((node: any) => string)) {
       return keyshape;
     },
     setState(name, value, node) {
-      const cfg = node.get('model') || {};
+      const cfg = Object.assign({}, node.get('model') || {});
       // 根据状态构造style
       if (value && cfg.style && cfg.style[name]) {
-        cfg.originStyle = cfg.style;
         cfg.style = { ...cfg.style, ...cfg.style[name] }
-      } else if (cfg.originStyle) {
-        cfg.style = cfg.originStyle;
       }
       // 根据上下文更新
       this.update(cfg, node);
