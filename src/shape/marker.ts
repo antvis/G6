f5eafe7f17df8f1c231aaf98529e533add13cd1c
Yanyan-Wang <yanyanwang93@gmail.com>
2020-06-24 23:27:06 +0800

    feat: 6 built-in arrows and 4 built-in  markers.

--- src/shape/marker.ts
+++ src/shape/marker.ts
@@ -18,5 +18,27 @@ export default {
             ['M', x - r + r, y - r + 4],
             ['L', x, y + r - 4],
         ];
-    }
+    },
+    upTriangle: (x, y, r) => {
+        const l1 = r * Math.cos(Math.PI / 6);
+        const l2 = r * Math.sin(Math.PI / 6);
+        return [
+            ['M', x - l1, y + l2],
+            ['L', x + l1, y + l2],
+            ['L', x, y - r],
+            ['Z']
+        ];
+
+    },
+    downTriangle: (x, y, r) => {
+        const l1 = r * Math.cos(Math.PI / 6);
+        const l2 = r * Math.sin(Math.PI / 6);
+        return [
+            ['M', x - l1, y - l2],
+            ['L', x + l1, y - l2],
+            ['L', x, y + r],
+            ['Z']
+        ];
+
+    },
 }
