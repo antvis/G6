07533bd1ed6101603bb33e1700737fa57c36d66d
baizn <576375879@qq.com>
2020-07-15 14:47:48 +0800

    feat: slider timebar component

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -4,6 +4,7 @@ import Minimap from './minimap';
 import Bundling from './bundling';
 import ToolBar from './toolBar'
 import Tooltip from './tooltip'
+import TimeBar from './timeBar'
 
 export default {
   Menu,
@@ -11,5 +12,6 @@ export default {
   Minimap,
   Bundling,
   ToolBar,
-  Tooltip
+  Tooltip,
+  TimeBar
 };
