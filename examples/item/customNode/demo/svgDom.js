29733d1bd0921b0b3a38eefe9abf0244b355e54d
Yanyan-Wang <yanyanwang93@gmail.com>
2020-07-22 09:54:18 +0800

    fix: zoom-canvas with fixItem problems. docs: add zoom-canvas with fixItem demo.

--- examples/item/customNode/demo/svgDom.js
+++ examples/item/customNode/demo/svgDom.js
@@ -9,105 +9,105 @@ import G6 from '@antv/g6';
 /**
  * Register a node type with DOM
  */
-G6.registerNode('dom-node', {
-  draw: (cfg, group) => {
-    const stroke = cfg.style ? cfg.style.stroke || '#5B8FF9' : '#5B8FF9';
-    const shape = group.addShape('dom', {
-      attrs: {
-        width: cfg.size[0],
-        height: cfg.size[1],
-        html: `
-        <div id=${cfg.id} class='dom-node' style="background-color: #fff; border: 2px solid ${stroke}; border-radius: 5px; width: ${cfg.size[0] - 5}px; height: ${cfg.size[1] - 5}px; display: flex;">
-          <div style="height: 100%; width: 33%; background-color: #CDDDFD">
-            <img alt="" style="line-height: 100%; margin-left: 7px;" src="https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*Q_FQT6nwEC8AAAAAAAAAAABkARQnAQ" width="20" height="20" />  
-          </div>
-          <span style="margin:auto; padding:auto; color: #5B8FF9">${cfg.label}</span>
-        </div>
-          `
-      },
-      draggable: true
-    });
-    return shape;
-  }
-});
+// G6.registerNode('dom-node', {
+//   draw: (cfg, group) => {
+//     const stroke = cfg.style ? cfg.style.stroke || '#5B8FF9' : '#5B8FF9';
+//     const shape = group.addShape('dom', {
+//       attrs: {
+//         width: cfg.size[0],
+//         height: cfg.size[1],
+//         html: `
+//         <div id=${cfg.id} class='dom-node' style="background-color: #fff; border: 2px solid ${stroke}; border-radius: 5px; width: ${cfg.size[0] - 5}px; height: ${cfg.size[1] - 5}px; display: flex;">
+//           <div style="height: 100%; width: 33%; background-color: #CDDDFD">
+//             <img alt="" style="line-height: 100%; margin-left: 7px;" src="https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*Q_FQT6nwEC8AAAAAAAAAAABkARQnAQ" width="20" height="20" />  
+//           </div>
+//           <span style="margin:auto; padding:auto; color: #5B8FF9">${cfg.label}</span>
+//         </div>
+//           `
+//       },
+//       draggable: true
+//     });
+//     return shape;
+//   }
+// });
 
-/** 数据 */
-const data = {
-  nodes: [
-    {
-      id: 'node1',
-      x: 10,
-      y: 100,
-      label: 'Homepage',
-    },
-    {
-      id: 'node2',
-      x: 200,
-      y: 100,
-      label: 'Subpage',
-    },
-  ],
-  edges: [
-    {
-      source: 'node1',
-      target: 'node2',
-    },
-  ],
-};
+// /** 数据 */
+// const data = {
+//   nodes: [
+//     {
+//       id: 'node1',
+//       x: 10,
+//       y: 100,
+//       label: 'Homepage',
+//     },
+//     {
+//       id: 'node2',
+//       x: 200,
+//       y: 100,
+//       label: 'Subpage',
+//     },
+//   ],
+//   edges: [
+//     {
+//       source: 'node1',
+//       target: 'node2',
+//     },
+//   ],
+// };
 
-const width = document.getElementById('container').scrollWidth;
-const height = document.getElementById('container').scrollHeight || 500;
-const graph = new G6.Graph({
-  container: 'container',
-  width,
-  height,
-  // translate the graph to align the canvas's center, support by v3.5.1
-  fitCenter: true,
-  renderer: 'svg',
-  linkCenter: true,
-  defaultNode: {
-    type: 'dom-node',
-    size: [120, 40]
-  }
-});
+// const width = document.getElementById('container').scrollWidth;
+// const height = document.getElementById('container').scrollHeight || 500;
+// const graph = new G6.Graph({
+//   container: 'container',
+//   width,
+//   height,
+//   // translate the graph to align the canvas's center, support by v3.5.1
+//   fitCenter: true,
+//   renderer: 'svg',
+//   linkCenter: true,
+//   defaultNode: {
+//     type: 'dom-node',
+//     size: [120, 40]
+//   }
+// });
 
-graph.data(data);
-graph.render();
+// graph.data(data);
+// graph.render();
 
-// click listener for dom nodes to response the click by changing stroke color
-const listener = (dom) => {
-  const nodeId = dom.id;
-  const node = graph.findById(nodeId);
-  let stroke = '';
-  if (!node.hasState('selected')) {
-    stroke = '#f00';
-    graph.setItemState(node, 'selected', true);
-  } else {
-    stroke = '#5B8FF9';
-    graph.setItemState(node, 'selected', false);
-  }
-  graph.updateItem(nodeId, {
-    style: {
-      stroke
-    }
-  });
-}
+// // click listener for dom nodes to response the click by changing stroke color
+// const listener = (dom) => {
+//   const nodeId = dom.id;
+//   const node = graph.findById(nodeId);
+//   let stroke = '';
+//   if (!node.hasState('selected')) {
+//     stroke = '#f00';
+//     graph.setItemState(node, 'selected', true);
+//   } else {
+//     stroke = '#5B8FF9';
+//     graph.setItemState(node, 'selected', false);
+//   }
+//   graph.updateItem(nodeId, {
+//     style: {
+//       stroke
+//     }
+//   });
+// }
 
-const bindClickListener = () => {
-  const domNodes = document.getElementsByClassName('dom-node')
-  for (let i = 0; i < domNodes.length; i++) {
-    const dom = domNodes[i];
-    dom.addEventListener('click', (e) => {
-      listener(dom);
-    });
-  }
-}
+// const bindClickListener = () => {
+//   const domNodes = document.getElementsByClassName('dom-node')
+//   for (let i = 0; i < domNodes.length; i++) {
+//     const dom = domNodes[i];
+//     dom.addEventListener('click', (e) => {
+//       listener(dom);
+//     });
+//   }
+// }
 
 
-bindClickListener();
+// bindClickListener();
 
-// after update the item, all the DOMs will be re-rendered
-// so the listeners should be rebinded to the new DOMs
-graph.on('afterupdateitem', e => {
-  bindClickListener();
-});
+// // after update the item, all the DOMs will be re-rendered
+// // so the listeners should be rebinded to the new DOMs
+// graph.on('afterupdateitem', e => {
+//   bindClickListener();
+// });
