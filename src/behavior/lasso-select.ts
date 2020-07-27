c5758855a3682ee164c70f8f1ea6e31bcd0cdb72
Yanyan-Wang <yanyanwang93@gmail.com>
2020-07-27 18:20:52 +0800

    chore: update the import of isPolygonsIntersect

--- src/behavior/lasso-select.ts
+++ src/behavior/lasso-select.ts
@@ -1,5 +1,5 @@
 import { G6Event, IG6GraphEvent, IPoint, Item } from '../types';
-import isPolygonsIntersect from '@antv/path-util/lib/is-polygons-intersect';
+import { isPolygonsIntersect } from '@antv/path-util';
 import { pathToPoints } from '../util/path'
 
 const DEFAULT_TRIGGER = 'shift';
