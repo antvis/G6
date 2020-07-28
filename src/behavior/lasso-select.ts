5f61cf38d58ca7bd0872da74914cd3c0bc745cdb
chenluli <chenluuli@gmail.com>
2020-07-28 14:51:12 +0800

    fix: add isPolygonsIntersect in math util & remove its dependency of @antv/path-util

--- src/behavior/lasso-select.ts
+++ src/behavior/lasso-select.ts
@@ -1,5 +1,5 @@
 import { G6Event, IG6GraphEvent, IPoint, Item } from '../types';
-import { isPolygonsIntersect } from '@antv/path-util';
+import { isPolygonsIntersect } from '../util/math';
 import { pathToPoints } from '../util/path'
 
 const DEFAULT_TRIGGER = 'shift';
