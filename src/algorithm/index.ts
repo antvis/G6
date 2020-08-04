6001e658e8f9c57e6fe690e4dd37436f0ca05b93
chenluli <chenluuli@gmail.com>
2020-08-04 15:06:32 +0800

    feat: add detect all cycles for undirected and directed graph

--- src/algorithm/index.ts
+++ src/algorithm/index.ts
@@ -4,4 +4,5 @@ export { default as detectDirectedCycle } from './detect-cycle'
 export { default as degree } from './degree'
 export { default as adjMatrix } from './adjacent-matrix'
 export { default as floydWarshall } from './floydWarshall'
-
+export { default as getConnectedComponents } from './connected-component'
+export { detectAllCycles, detectAllDirectedCycle, detectAllUndirectedCycle } from './detect-cycle'
