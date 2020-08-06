20ee4434ad2b2ded13d4a2904fe2b7c0f1b27652
LovelyBuggies <ninomyemail@gmail.com>
2020-08-06 12:14:41 +0800

    style: var->let

--- tests/unit/graph/data/auto-layout-test3.ts
+++ tests/unit/graph/data/auto-layout-test3.ts
@@ -2,18 +2,18 @@
  * dense, low
  */
 
-var nodes = [];
-for (var i = 0; i < 36; i++) {
+let nodes = [];
+for (let i = 0; i < 36; i++) {
   nodes.push({
     id: i.toString(),
     label: i.toString(),
   })
 }
 
-var edges = [];
-for (var i = 0; i < 36; i++) {
+let edges = [];
+for (let i = 0; i < 36; i++) {
   if (i < 5) {
-    for (var j = 1; j < 24; j++) {
+    for (let j = 1; j < 24; j++) {
       edges.push({
         source: i.toString(),
         target: j.toString(),
