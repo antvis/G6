af95d52e313e5b8a8e1b1d0b2207622d61559def
Yanyan-Wang <yanyanwang93@gmail.com>
2020-08-10 20:10:20 +0800

    chore: update bablerc, package.json, and webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -42,9 +42,11 @@ module.exports = {
             // babelrc: true,
             presets: [
               '@babel/preset-env',
-              plugins: [
-                '@babel/plugin-proposal-class-properties'
-              ]
+              {
+                plugins: [
+                  '@babel/plugin-proposal-class-properties'
+                ]
+              }
             ],
           },
         },
