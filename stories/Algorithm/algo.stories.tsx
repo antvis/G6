63cc47a1df8a83d190fb36abc5d7597b8216c083
chenluli <chenluuli@gmail.com>
2020-08-14 10:00:52 +0800

    feat: find all paths and the shortest path between two nodes

---
+++ stories/Algorithm/algo.stories.tsx
@@ -0,0 +1,10 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import FindPath from './component/path';
+
+export default { title: 'Algorithm' };
+
+storiesOf('Algorithm', module)
+  .add('find path', () => (
+    <FindPath />
+  ));
