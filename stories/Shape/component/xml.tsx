efe52896db0d2b8c507e3e6f487a9f4eaa8f26f2
Dominic Ming <mxz96102@qq.com>
2020-08-24 11:16:53 +0800

    fix: xml node surport image node (#1960)
    * feat: using style from cfg into xml node
    
    * fix: node style not rollback with the style
    
    * fix: remove unused var
    
    * feat: xml node using single-node setState methods
    
    * fix: {{}} replace bug and only keyshape inherit origin attrs
    
    * lint: fix line problem in xml
    
    * fix: fix image node problem

--- stories/Shape/component/xml.tsx
+++ stories/Shape/component/xml.tsx
@@ -50,6 +50,28 @@ G6.registerNode('rect-xml', (cfg) => `
     width: 100, height: 20, fill: '#1890ff', stroke: '#1890ff', radius: [6, 6, 0, 0]
   }} keyshape="true" name="test">
     <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }} name="title">${cfg.label || cfg.id}</text>
+    <polygon style={{
+      points:[[ 30, 30 ], [ 40, 20 ], [ 30, 50 ], [ 60, 100 ]],
+          fill: 'red'
+    }} />
+    <path style={{
+          startArrow: {
+            path: 'M 10,0 L -10,-10 L -10,10 Z',
+            d: 10
+          },
+          endArrow: {
+            path: 'M 10,0 L -10,-10 L -10,10 Z',
+            d: 10
+          },
+          path: [
+             [ 'M', 100, 100 ],
+             [ 'L', 200, 200 ]
+          ],
+          stroke: '#000',
+          lineWidth: 8
+        }} />
+        <polyline style={{ points: [[ 30, 30 ], [ 40, 20 ], [ 60, 100 ]] }} />
+        <image style={{ img: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png', width: 48, height: 48, marginTop: 100 }} />
   </rect>
 `)
 
