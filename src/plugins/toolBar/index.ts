a1a638663d02b960cfceba637996e7bfb9965030
Yanyan-Wang <yanyanwang93@gmail.com>
2020-08-31 13:51:29 +0800

    docs: toolbar demo in stories

--- src/plugins/toolBar/index.ts
+++ src/plugins/toolBar/index.ts
@@ -188,6 +188,7 @@ export default class ToolBar extends Base {
     }
 
     const currentData = undoStack.pop();
+    debugger
     if (currentData) {
       let { action, data } = currentData;
       graph.pushStack(action, clone(data), 'redo');
@@ -221,6 +222,7 @@ export default class ToolBar extends Base {
           break;
         }
         case 'add':
+          console.log('redo data', data)
           graph.removeItem(data.id, false);
           break;
         default:
