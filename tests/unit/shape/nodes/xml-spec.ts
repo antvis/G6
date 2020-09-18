fa675b0103d9f8719a5993adbc3f28fbdc4d98a0
Dominic Ming <xuzhi.mxz@antfin.com>
2020-09-18 17:18:11 +0800

    feat: jsx node extend config support and next style (#2092)
    * feat: register node from xml string or xml string genrator
    
    * feat: add register xml node to interface and stroy book
    
    * fix: merge register to node & basic props back to shape options
    
    * fix: delete unused static function on shape
    
    * feat: xml node update when setState
    
    * feat: simple template renderer on xml
    
    * feat: simple template renderer on xml
    
    * fix: xml node update state style error
    
    * chore: remove lodash dependency
    
    * fix: remove xml html node after compile
    
    * feat: support loose json and object wrapped with {{ }}
    
    * feat: add test for new xml parser
    
    * feat: using style from cfg into xml node
    
    * feat: using style from cfg into xml node
    
    * fix: node style not rollback with the style
    
    * fix: remove unused var
    
    * feat: xml node using single-node setState methods
    
    * feat: register node from xml string or xml string genrator
    
    * feat: add register xml node to interface and stroy book
    
    * fix: merge register to node & basic props back to shape options
    
    * fix: delete unused static function on shape
    
    * feat: xml node update when setState
    
    * feat: simple template renderer on xml
    
    * feat: simple template renderer on xml
    
    * fix: xml node update state style error
    
    * chore: remove lodash dependency
    
    * fix: remove xml html node after compile
    
    * feat: support loose json and object wrapped with {{ }}
    
    * feat: add test for new xml parser
    
    * feat: using style from cfg into xml node
    
    * fix: xml node state style not correct (#1916)
    
    * feat: using style from cfg into xml node
    
    * fix: node style not rollback with the style
    
    * fix: remove unused var
    
    * feat: xml node using single-node setState methods
    
    * fix: update & state
    
    * fix: {{}} replace bug and only keyshape inherit origin attrs
    
    * lint: fix line problem in xml
    
    * fix: xml node parse error and origin style only used on keyshape (#1942)
    
    * feat: using style from cfg into xml node
    
    * fix: node style not rollback with the style
    
    * fix: remove unused var
    
    * feat: xml node using single-node setState methods
    
    * fix: {{}} replace bug and only keyshape inherit origin attrs
    
    * lint: fix line problem in xml
    
    * fix: test xml node
    
    * fix: fix image node problem
    
    * doc: add xml node examples
    
    * feat: add jsx key for node extend
    
    * feat: add next key to keep shape display inline
    
    * fix: xml json parse bug cause boolean not supported
    
    * fix: lint problem
    
    * fix: coveage fix
    
    Co-authored-by: Moyee <576375879@qq.com>

--- tests/unit/shape/nodes/xml-spec.ts
+++ tests/unit/shape/nodes/xml-spec.ts
@@ -1,5 +1,11 @@
-import { generateTarget, parseXML, compareTwoTarget, xmlDataRenderer } from "../../../../src/shape/xml";
-import G6 from "../../../../src";
+import {
+  generateTarget,
+  parseXML,
+  compareTwoTarget,
+  xmlDataRenderer,
+  createNodeFromXML,
+} from '../../../../src/shape/xml';
+import G6 from '../../../../src';
 
 const testXML = `
 <group>
@@ -7,19 +13,33 @@ const testXML = `
     width: 100, height: 20, fill: '#1890ff', stroke: '#1890ff', radius: [6, 6, 0, 0]
   }}>
     <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }}>{{id}}</text>
+    <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }}>{{id}}</text>
+    <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }}>{{id}}</text>
   </rect>
 <group>
-`
+`;
 
-const testXMLNode = cfg => `
+const testXMLNode = (cfg) => `
 <group>
   <rect style={{
     width: 100, height: 20, fill: '#1890ff', stroke: '#1890ff', radius: [6, 6, 0, 0]
 }} keyshape="true" name="test">
-    <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }} name="title">${cfg.id}</text>
-  </rect>
+  <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }} name="title">${
+    cfg.id
+  }</text>
+  <text style={{ marginTop: 2, marginTop: 10, textAlign: 'center', fontWeight: 'bold', fill: '#fff', next: inline }} visible="false">{{id}}</text>
+  ${
+    cfg.name
+      ? `<polygon style={{
+    points: [[10, 10], [15, 20], [20, 40], [30, 90]],
+    fill: '#F6BD16',
+    stroke: 'green',
+  }}/>`
+      : ''
+  }
+</rect>
 <group>
-`
+`;
 
 const div = document.createElement('div');
 div.id = 'graph-spec';
@@ -37,15 +57,15 @@ describe('xml node test', () => {
           type: 'test',
           style: {
             lineWidth: 4,
-          }
+          },
         },
         nodeStateStyles: {
-          'test': {
+          test: {
             title: {
-              fill: '#eee'
-            }
-          }
-        }
+              fill: '#eee',
+            },
+          },
+        },
       });
       const data = {
         nodes: [
@@ -63,7 +83,7 @@ describe('xml node test', () => {
       const nodes = graph.getNodes();
       const node = nodes[0];
       const group = node.get('group');
-      expect(group.getCount()).toEqual(2);
+      expect(group.getCount()).toEqual(3);
       const keyShape = node.getKeyShape();
       expect(keyShape.attr('fill')).toEqual('#1890ff');
       expect(keyShape.attr('lineWidth')).toEqual(4);
@@ -72,12 +92,13 @@ describe('xml node test', () => {
       const afterNode = graph.getNodes()[0];
       const afterGroup = afterNode.get('group');
       expect(afterGroup.get('children')[1].attr('fill')).toBe('#eee');
+      graph.updateItem('node', { name: 1 });
+      expect(group.getCount()).toEqual(4);
 
       graph.destroy();
       expect(graph.destroyed).toBe(true);
     });
   });
-
 
   describe('parse test', () => {
     it('xml to object', () => {
@@ -102,9 +123,8 @@ describe('xml node test', () => {
       expect(target.bbox.x).toBe(0);
       expect(target.bbox.y).toBe(0);
       expect(target.children[0].bbox.width).toBe(100);
-      expect(target.children[0].children[0].bbox.x).toBe(50)
-
-    })
+      expect(target.children[0].children[0].bbox.x).toBe(50);
+    });
   });
 
   describe('compare test', () => {
@@ -116,27 +136,90 @@ describe('xml node test', () => {
 
     it('compare same', () => {
       const result = compareTwoTarget(target, target);
-      expect(result.action).toBe('same')
+      expect(result.action).toBe('same');
     });
 
     it('compare add', () => {
       const result = compareTwoTarget(target, null);
-      expect(result.action).toBe('add')
+      expect(result.action).toBe('add');
     });
 
     it('compare delete', () => {
       const result = compareTwoTarget(null, target);
-      expect(result.action).toBe('delete')
+      expect(result.action).toBe('delete');
     });
 
     it('compare restructure', () => {
       const result = compareTwoTarget(target.children[0], target);
-      expect(result.action).toBe('restructure')
+      expect(result.action).toBe('restructure');
     });
-  })
 
-  describe.only('xml node state', () => {
-    G6.registerNode('xml-node', cfg => {
+    it('null compare', () => {
+      const result = compareTwoTarget(null, null);
+      expect(result.action).toBe('same');
+    });
+
+    it('compare children', () => {
+      const result1 = compareTwoTarget(
+        { ...target },
+        { ...target, children: target.children.slice(0, 1) },
+      );
+      expect(result1.action).toBe('same');
+      expect(result1.children[1].action).toBe('add');
+
+      const result2 = compareTwoTarget(
+        { ...target, children: target.children.slice(0, 1) },
+        { ...target },
+      );
+      expect(result2.action).toBe('same');
+      expect(result2.children[1].action).toBe('delete');
+    });
+  });
+
+  describe('node create from xml', () => {
+    const descs = (cfg) => `
+    <rect style={{
+      width: 100, height: 20, fill: '#1890ff', stroke: '#1890ff', radius: [6, 6, 0, 0]
+    }} keyshape="true" name="test">
+      <text style={{ marginTop: 2, marginLeft: 50, textAlign: 'center', fontWeight: 'bold', fill: '#fff' }} name="title">${
+        cfg.label || cfg.id
+      }</text>
+      <polygon style={{
+        points:[[ 30, 30 ], [ 40, 20 ], [ 30, 50 ], [ 60, 100 ]],
+            fill: 'red'
+      }} />
+      <path style={{
+            startArrow: {
+              path: 'M 10,0 L -10,-10 L -10,10 Z',
+              d: 10
+            },
+            endArrow: {
+              path: 'M 10,0 L -10,-10 L -10,10 Z',
+              d: 10
+            },
+            path: [
+               [ 'M', 100, 100 ],
+               [ 'L', 200, 200 ]
+            ],
+            stroke: '#000',
+            lineWidth: 8
+          }} />
+          <polyline style={{ points: [[ 30, 30 ], [ 40, 20 ], [ 60, 100 ]] }} />
+          <image style={{ img: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png', width: 48, height: 48, marginTop: 100 }} />
+          <text style={{ fill: red, next: inline }}>1111</text>
+          <text style={{ fill: yellow, marginLeft: 4, fontWeight: bold }}>2222</text>            
+    </rect>
+  `;
+    it('generate object', () => {
+      const result = createNodeFromXML(descs);
+      expect(result.draw).not.toBeUndefined();
+      expect(result.update).not.toBeUndefined();
+      expect(result.getAnchorPoints).not.toBeUndefined();
+    });
+  });
+
+  describe('xml node state', () => {
+    G6.registerNode('xml-node', (cfg) => {
       return `
         <group>
           <circle keyshape='true' style={{
@@ -160,35 +243,37 @@ describe('xml node test', () => {
             }} name='icon-circle' draggable='true'>
             </circle>
           </circle>
-          <text style={{ marginTop: -140, textAlign: 'center', fontWeight: 'bold', fill: 'green' }}>${cfg.label || cfg.id}</text>
+          <text style={{ marginTop: -140, textAlign: 'center', fontWeight: 'bold', fill: 'green' }}>${
+            cfg.label || cfg.id
+          }</text>
         </group>
-      `
-    })
+      `;
+    });
     const data = {
       nodes: [
         {
           id: 'node1',
           label: 'node1',
           x: 100,
-          y: 100
-        }
-      ]
-    }
+          y: 100,
+        },
+      ],
+    };
     it('node state', () => {
       const graph = new G6.Graph({
         container: 'graph-spec',
         width: 500,
         height: 500,
         modes: {
-          default: ['drag-node', 'zoom-canvas']
+          default: ['drag-node', 'zoom-canvas'],
         },
         defaultNode: {
           type: 'xml-node',
           size: 50,
           style: {
             stroke: 'blue',
-            fill: '#ccc'
-          }
+            fill: '#ccc',
+          },
         },
         nodeStateStyles: {
           hover: {
@@ -196,22 +281,22 @@ describe('xml node test', () => {
             // stroke: 'green',
             // lineWidth: 3,
             'icon-circle': {
-              fill: '#456dc5'
-            }
-          }
-        }
-      })
+              fill: '#456dc5',
+            },
+          },
+        },
+      });
 
-      graph.data(data)
-      graph.render()
+      graph.data(data);
+      graph.render();
 
-      graph.on('icon-circle:mouseenter', evt => {
-        graph.setItemState(evt.item, 'hover', true)
-      })
+      graph.on('icon-circle:mouseenter', (evt) => {
+        graph.setItemState(evt.item, 'hover', true);
+      });
 
-      graph.on('icon-circle:mouseleave', evt => {
-        graph.setItemState(evt.item, 'hover', false)
-      })
-    })
-  })
+      graph.on('icon-circle:mouseleave', (evt) => {
+        graph.setItemState(evt.item, 'hover', false);
+      });
+    });
+  });
 });
