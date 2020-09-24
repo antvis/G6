9981f8dac632fed3bff08aa452115313e09c4c88
baizn <576375879@qq.com>
2020-09-24 10:34:31 +0800

    fix: modelrect & jsx-node

--- examples/item/customNode/demo/jsxNodeWithAnimate.js
+++ examples/item/customNode/demo/jsxNodeWithAnimate.js
@@ -6,55 +6,50 @@ import G6 from '@antv/g6';
  *
  */
 
-
-/**
- * Register a XML Node
- */
-
 G6.registerNode('rect-xml', {
   jsx: (cfg) => `
-<group>
-  <rect style={{
-    width: 200,
-    height: 75,
-  }}>
-    <rect style={{
-      width: 150,
-      height: 20,
-      fill: ${cfg.color},
-      radius: [6, 6, 0, 0],
-      cursor: 'move'，
-      stroke: ${cfg.color}
-    }} draggable="true">
-      <text style={{ 
-        marginTop: 2, 
-        marginLeft: 75, 
-        textAlign: 'center', 
-        fontWeight: 'bold', 
-        fill: '#fff' }}>{{label}}</text>
-    </rect>
-    <rect style={{
-      width: 150,
-      height: 55,
-      stroke: ${cfg.color},
-      fill: #ffffff,
-      radius: [0, 0, 6, 6],
-    }}>
-      <text style={{ marginTop: 5, marginLeft: 3, fill: '#333', marginLeft: 4 }}>描述: {{description}}</text>
-      <text style={{ marginTop: 10, marginLeft: 3, fill: '#333', marginLeft: 4 }}>创建者: {{meta.creatorName}}</text>
-    </rect>
-  </rect>
-  <circle style={{
-    stroke: ${cfg.color},
-    r: 10,
-    fill: '#fff',
-    marginLeft: 75,
-    cursor: 'pointer'
-  }} name="circle">
-    <image name="img" style={{ img: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png', width: 12, height: 12,  marginLeft: 69,  marginTop: -5 }} />
-  </circle>
-</group>
-`,
+    <group>
+      <rect style={{
+        width: 200,
+        height: 75,
+      }}>
+        <rect style={{
+          width: 150,
+          height: 20,
+          fill: ${cfg.color},
+          radius: [6, 6, 0, 0],
+          cursor: 'move'，
+          stroke: ${cfg.color}
+        }} draggable="true">
+          <text style={{ 
+            marginTop: 2, 
+            marginLeft: 75, 
+            textAlign: 'center', 
+            fontWeight: 'bold', 
+            fill: '#fff' }}>{{label}}</text>
+        </rect>
+        <rect style={{
+          width: 150,
+          height: 55,
+          stroke: ${cfg.color},
+          fill: #ffffff,
+          radius: [0, 0, 6, 6],
+        }}>
+          <text style={{ marginTop: 5, marginLeft: 3, fill: '#333', marginLeft: 4 }}>描述: {{description}}</text>
+          <text style={{ marginTop: 10, marginLeft: 3, fill: '#333', marginLeft: 4 }}>创建者: {{meta.creatorName}}</text>
+        </rect>
+      </rect>
+      <circle style={{
+        stroke: ${cfg.color},
+        r: 10,
+        fill: '#fff',
+        marginLeft: 75,
+        cursor: 'pointer'
+      }} name="circle">
+        <image name="img" style={{ img: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png', width: 12, height: 12,  marginLeft: 69,  marginTop: -5 }} />
+      </circle>
+    </group>
+  `,
   afterDraw: (cfg, group) => {
     console.log(group)
     const img = group.findAllByName('img');
