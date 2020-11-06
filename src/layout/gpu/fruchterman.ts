b487f023324c4daaadcb245b7ba21090b0df42bf
Yanyan-Wang <yanyanwang93@gmail.com>
2020-11-06 20:15:56 +0800

    fix: fruchterman and new gpu api

--- src/layout/gpu/fruchterman.ts
+++ src/layout/gpu/fruchterman.ts
@@ -7,65 +7,13 @@ import { EdgeConfig, IPointTuple, NodeConfig, NodeIdxMap } from '../../types';
 import { BaseLayout } from '../layout';
 import { isNumber } from '@antv/util';
 import { World } from '@antv/g-webgpu';
+// compile at runtime in dev mode
+// import { Compiler } from '@antv/g-webgpu-compiler';
+// import { fruchtermanCode, clusterCode } from './fruchtermanShader';
 import { buildTextureData, attributesToTextureData } from '../../util/layout'
-import { cCode, gCode } from './fruchtermanShader';
+// use compiled bundle in prod mode
+import { fruchtermanBundle, clusterBundle } from './fruchtermanShader';
 import { LAYOUT_MESSAGE } from '../worker/layoutConst';
-
-
-const gCode2 = `
-import { globalInvocationID, debug } from 'g-webgpu';
-
-const VERTEX_COUNT;
-const CLUSTER_COUNT;
-
-@numthreads(1, 1, 1)
-class CalcCenter {
-  @in
-  u_Data: vec4[];
-
-  @in
-  u_NodeAttributes: vec4[]; // [[clusterIdx, 0, 0, 0], ...]
-
-  @out
-  u_ClusterCenters: vec4[]; // [[cx, cy, nodeCount, clusterIdx], ...]
-
-  @main
-  compute() {
-    // init the cluster centers
-    for (let i = 0; i < CLUSTER_COUNT; i++) {
-      this.u_ClusterCenters[i] = [ 0, 0, 0, 0 ]
-    }
-
-    for (let j = 0; j < VERTEX_COUNT; j++) {
-      const attributes = this.u_NodeAttributes[j];
-      const clusterIdx: float = attributes[0];
-      const center = this.u_ClusterCenters[int(clusterIdx)];
-      const sumx: float = center.x + this.u_Data[j].x;
-      const sumy: float = center.y + this.u_Data[j].y;
-      const count = center.z + 1;
-      this.u_ClusterCenters[int(clusterIdx)] =
-      [
-        sumx, // cx
-        sumy, // cy
-        count, // nodeCount
-        clusterIdx,
-      ]
-      // !!! sumx sumy count 有值，但 this.u_ClusterCenters[int(clusterIdx)] 未被赋值成功
-      // debug(this.u_ClusterCenters[int(clusterIdx)].x);
-    }
-
-    for (let k = 0; k < CLUSTER_COUNT; k++) {
-      const center = this.u_ClusterCenters[k];
-      this.u_ClusterCenters[k] = [
-        center.x / center.z,
-        center.y / center.z,
-        center.z,
-        center.w
-      ];
-    }
-  }
-}
-`;
 
 type Node = NodeConfig & {
   cluster: string | number;
@@ -76,8 +24,6 @@ type Edge = EdgeConfig;
 type NodeMap = {
   [key: string]: Node;
 };
-
-const SPEED_DIVISOR = 800;
 
 /**
  * fruchterman 布局
@@ -191,12 +137,11 @@ export default class FruchtermanGPULayout extends BaseLayout {
     }
     const center = self.center;
     const area = self.height * self.width;
-    const maxDisplace = Math.sqrt(area) / 10;
+    let maxDisplace = Math.sqrt(area) / 10;
     const k2 = area / (nodes.length + 1);
     const k = Math.sqrt(k2);
     const speed = self.speed;
     const clustering = self.clustering;
-    let curMaxDisplace = maxDisplace;
 
     const { array: attributeArray, count: clusterCount } = attributesToTextureData([self.clusterField], nodes);
 
@@ -208,102 +153,108 @@ export default class FruchtermanGPULayout extends BaseLayout {
     let world;
 
     if (workerEnabled) {
-      world = new World({
+      world = World.create({
         canvas,
         engineOptions: {
           supportCompute: true,
         },
       });
     } else {
-      world = new World({
+      world = World.create({
         engineOptions: {
           supportCompute: true,
         }
       });
     }
 
+    // compile at runtime in dev mode
+    // const compiler = new Compiler();
+    // const fruchtermanBundle = compiler.compileBundle(fruchtermanCode);
+    // const clusterBundle = compiler.compileBundle(clusterCode);
+
+    // use compiled bundle in prod mode
+    // console.log(fruchtermanBundle.toString());
+    // console.log(clusterBundle.toString());
+
     const onLayoutEnd = self.onLayoutEnd;
 
-    const compute = world.createComputePipeline({
-      shader: gCode,
-      // precompiled: true,
-      // shader: cCode,
-      dispatch: [numParticles, 1, 1],
-      maxIteration,
-      onIterationCompleted: async () => {
-        if (clustering) {
-          world.setBinding(compute2, 'u_Data', {
-            entity: compute,
-          });
-        }
-
-        curMaxDisplace *= 0.99;
-        world.setBinding(
-          compute,
-          'u_MaxDisplace',
-          curMaxDisplace,
-        );
-        return;
-      },
-      onCompleted: (finalParticleData) => {
-        if (canvas) {
-          // 传递数据给主线程
-          ctx.postMessage({
-            type: LAYOUT_MESSAGE.GPUEND,
-            vertexEdgeData: finalParticleData,
-            // edgeIndexBufferData,
-          });
-        } else {
-          nodes.forEach((node, i) => {
-            const x = finalParticleData[4 * i];
-            const y = finalParticleData[4 * i + 1];
-            node.x = x;
-            node.y = y;
-          });
-        }
-        onLayoutEnd && onLayoutEnd();
-
-        // 计算完成后销毁相关 GPU 资源
-        world.destroy();
-      }
-    });
-
-    world.setBinding(compute, 'u_Data', nodesEdgesArray);
-    world.setBinding(compute, 'u_K', k);
-    world.setBinding(compute, 'u_K2', k2);
-    world.setBinding(compute, 'u_Gravity', self.gravity);
-    world.setBinding(compute, 'u_ClusterGravity', self.clusterGravity);
-    world.setBinding(compute, 'u_Speed', speed);
-    world.setBinding(compute, 'u_MaxDisplace', maxDisplace);
-    world.setBinding(compute, 'u_Clustering', clustering ? 1 : 0);
-    world.setBinding(compute, 'u_AttributeArray', attributeArray);
-    world.setBinding(compute, 'u_CenterX', center[0]);
-    world.setBinding(compute, 'u_CenterY', center[1]);
-    world.setBinding(compute, 'MAX_EDGE_PER_VERTEX', maxEdgePerVetex);
-    world.setBinding(compute, 'VERTEX_COUNT', numParticles);
-
-
-    let compute2;
-    // 第二个管线用于计算聚类
-    if (clustering) {
-      compute2 = world.createComputePipeline({
-        shader: gCode2,
-        dispatch: [1, 1, 1],
-        maxIteration,
-        onIterationCompleted: async () => {
-          world.setBinding(compute, 'u_ClusterCenters', {
-            entity: compute2,
-          });
-        },
-        onCompleted: (finalParticleData) => {
-          world.destroy();
-        },
-      });
-      world.setBinding(compute2, 'u_Data', nodesEdgesArray);
-      world.setBinding(compute2, 'u_NodeAttributes', attributeArray);
-      world.setBinding(compute2, 'VERTEX_COUNT', numParticles);
-      world.setBinding(compute2, 'CLUSTER_COUNT', clusterCount);
+    const clusterCenters = [];
+    for (let i = 0; i < clusterCount; i++) {
+      clusterCenters.push(0, 0, 0, 0);
     }
 
+    const kernelFruchterman = world
+      .createKernel(fruchtermanBundle)
+      .setDispatch([numParticles, 1, 1])
+      .setBinding({
+        u_Data: nodesEdgesArray,
+        u_K: k,
+        u_K2: k2,
+        u_Gravity: self.gravity,
+        u_ClusterGravity: self.clusterGravity,
+        u_Speed: speed,
+        u_MaxDisplace: maxDisplace,
+        u_Clustering: clustering ? 1 : 0,
+        u_Center: center,
+        u_AttributeArray: attributeArray,
+        u_ClusterCenters: clusterCenters,
+        MAX_EDGE_PER_VERTEX: maxEdgePerVetex,
+        VERTEX_COUNT: numParticles,
+      });
+
+    let kernelCluster;
+    if (clustering) {
+      kernelCluster = world
+        .createKernel(clusterBundle)
+        .setDispatch([clusterCount, 1, 1])
+        .setBinding({
+          u_Data: nodesEdgesArray,
+          u_NodeAttributes: attributeArray,
+          u_ClusterCenters: clusterCenters,
+          VERTEX_COUNT: numParticles,
+          CLUSTER_COUNT: clusterCount,
+        });
+    }
+
+    const execute = async () => {
+      for (let i = 0; i < maxIteration; i++) {
+        await kernelFruchterman.execute();
+
+        if (clustering) {
+          kernelCluster.setBinding({
+            u_Data: kernelFruchterman,
+          });
+          await kernelCluster.execute();
+          kernelFruchterman.setBinding({
+            u_ClusterCenters: kernelCluster,
+          });
+        }
+
+        kernelFruchterman.setBinding({
+          u_MaxDisplace: maxDisplace *= 0.99,
+        });
+      }
+
+      const finalParticleData = await kernelFruchterman.getOutput();
+
+      if (canvas) {
+        // 传递数据给主线程
+        ctx.postMessage({
+          type: LAYOUT_MESSAGE.GPUEND,
+          vertexEdgeData: finalParticleData,
+          // edgeIndexBufferData,
+        });
+      } else {
+        nodes.forEach((node, i) => {
+          const x = finalParticleData[4 * i];
+          const y = finalParticleData[4 * i + 1];
+          node.x = x;
+          node.y = y;
+        });
+      }
+      onLayoutEnd && onLayoutEnd();
+    }
+
+    execute();
   }
 }
