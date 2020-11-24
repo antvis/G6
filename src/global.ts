06da73019d1a6ca8e7481b4229cb85980d966557
baizn <576375879@qq.com>
2020-11-24 11:16:33 +0800

    fix: update package version

--- src/global.ts
+++ src/global.ts
@@ -7,7 +7,7 @@ const textColor = 'rgb(0, 0, 0)';
 const colorSet = getColorsWithSubjectColor(subjectColor, backColor);
 
 export default {
-  version: '4.0.0',
+  version: '4.0.1',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
