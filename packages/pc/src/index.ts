7b719f8553efa888a9cca17b57f0046749bc6446
pomelo-nwu <czynwu@outlook.com>
2020-12-18 13:41:59 +0800

    refactor(pc):import pc-plugins into pc

--- packages/pc/src/index.ts
+++ packages/pc/src/index.ts
@@ -6,20 +6,20 @@ import TreeGraph from './graph/tree-graph';
 import { Layout, registerLayout } from './layout';
 import Global from './global';
 import Util from './util';
-// import Plugins from './plugins';
+import Plugins from './plugins';
 import * as Algorithm from '@antv/algorithm';
 
 // const registerLayout = Layout.registerLayout;
-// const Minimap = Plugins.Minimap;
-// const Grid = Plugins.Grid;
-// const Bundling = Plugins.Bundling;
-// const Menu = Plugins.Menu;
-// const Fisheye = Plugins.Fisheye;
-// const ToolBar = Plugins.ToolBar;
-// const Tooltip = Plugins.Tooltip;
-// const TimeBar = Plugins.TimeBar;
-// const ImageMinimap = Plugins.ImageMinimap;
-// const EdgeFilterLens = Plugins.EdgeFilterLens;
+const Minimap = Plugins.Minimap;
+const Grid = Plugins.Grid;
+const Bundling = Plugins.Bundling;
+const Menu = Plugins.Menu;
+const Fisheye = Plugins.Fisheye;
+const ToolBar = Plugins.ToolBar;
+const Tooltip = Plugins.Tooltip;
+const TimeBar = Plugins.TimeBar;
+const ImageMinimap = Plugins.ImageMinimap;
+const EdgeFilterLens = Plugins.EdgeFilterLens;
 
 export * from '@antv/g6-core';
 export { IGraph } from './interface/graph';
@@ -34,18 +34,18 @@ export {
   Layout,
   Global,
   // registerLayout,
-  // Minimap,
-  // Grid,
-  // Bundling,
-  // Menu,
-  // Fisheye,
+  Minimap,
+  Grid,
+  Bundling,
+  Menu,
+  Fisheye,
   // registerBehavior,
-  // Algorithm,
-  // ToolBar,
-  // Tooltip,
-  // TimeBar,
-  // ImageMinimap,
-  // EdgeFilterLens,
+  Algorithm,
+  ToolBar,
+  Tooltip,
+  TimeBar,
+  ImageMinimap,
+  EdgeFilterLens,
 };
 
 export default {
@@ -60,16 +60,16 @@ export default {
   registerCombo,
   registerEdge,
   registerNode,
-  // Minimap: Plugins.Minimap,
-  // Grid: Plugins.Grid,
-  // Bundling: Plugins.Bundling,
-  // Menu: Plugins.Menu,
-  // ToolBar: Plugins.ToolBar,
-  // Tooltip: Plugins.Tooltip,
-  // TimeBar,
-  // Fisheye,
-  // ImageMinimap,
-  // EdgeFilterLens,
+  Minimap: Plugins.Minimap,
+  Grid: Plugins.Grid,
+  Bundling: Plugins.Bundling,
+  Menu: Plugins.Menu,
+  ToolBar: Plugins.ToolBar,
+  Tooltip: Plugins.Tooltip,
+  TimeBar,
+  Fisheye,
+  ImageMinimap,
+  EdgeFilterLens,
   Algorithm,
   // Arrow,
   // Marker,
---
+++ packages/pc/src/plugins/index.tsx
@@ -0,0 +1,1 @@
+export * as default from '@antv/g6-plugins';
