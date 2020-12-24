84a1e5b9eac8322df8eb754ba27fcf88c092ab9b
Yanyan-Wang <yanyanwang93@gmail.com>
2020-12-24 14:31:59 +0800

    fix: uniqueId

--- packages/core/src/util/index.ts
+++ packages/core/src/util/index.ts
@@ -2,6 +2,7 @@ import * as MathUtil from './math';
 import * as GraphicUtil from './graphic';
 import * as PathUtil from './path';
 import * as BaseUtil from './base';
+import { uniqueId } from '@antv/util';
 
-const Util = { ...BaseUtil, ...GraphicUtil, ...PathUtil, ...MathUtil };
+const Util = { ...BaseUtil, ...GraphicUtil, ...PathUtil, ...MathUtil, uniqueId };
 export default Util;
