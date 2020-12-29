47bd66c1bda8116d0dc53e5ba035f6eb01df65fa
Yanyan-Wang <yanyanwang93@gmail.com>
2020-12-29 19:16:38 +0800

    chore: update version number for g6-pc; chore: update layout package

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -64,7 +64,7 @@ const colorSet = {
 };
 
 export default {
-  version: '4.0.3',
+  version: '0.0.1',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
