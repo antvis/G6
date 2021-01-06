73ad1bb2815c8e14dfa5ecd994f142cf5a42981a
Yanyan-Wang <yanyanwang93@gmail.com>
2021-01-06 20:52:48 +0800

    fix: addItem with array property; chore: remove console

--- packages/core/tests/unit/graph/graph-spec.ts
+++ packages/core/tests/unit/graph/graph-spec.ts
@@ -81,12 +81,7 @@ describe('graph', () => {
     expect(inst.get('group')).not.toBe(undefined);
 
     expect(inst.get('group').get('className')).toEqual('root-container');
-    expect(
-      inst
-        .get('group')
-        .get('id')
-        .endsWith('-root'),
-    ).toBe(true);
+    expect(inst.get('group').get('id').endsWith('-root')).toBe(true);
 
     const children = inst.get('group').get('children');
     expect(children.length).toBe(4);
@@ -568,10 +563,7 @@ describe('graph', () => {
 
   it('client point & model point convert', () => {
     const group = globalGraph.get('group');
-    const bbox = globalGraph
-      .get('canvas')
-      .get('el')
-      .getBoundingClientRect();
+    const bbox = globalGraph.get('canvas').get('el').getBoundingClientRect();
 
     let point = globalGraph.getPointByClient(bbox.left + 100, bbox.top + 100);
 
@@ -654,7 +646,10 @@ describe('all node link center', () => {
     graph.render();
 
     const edge = graph.findById('e1');
-    expect(edge.get('keyShape').attr('path')).toEqual([['M', 10, 10], ['L', 100, 100]]);
+    expect(edge.get('keyShape').attr('path')).toEqual([
+      ['M', 10, 10],
+      ['L', 100, 100],
+    ]);
   });
 
   it('loop', () => {
@@ -665,7 +660,10 @@ describe('all node link center', () => {
       x: 150,
       y: 150,
       style: { fill: 'yellow' },
-      anchorPoints: [[0, 0], [0, 1]],
+      anchorPoints: [
+        [0, 0],
+        [0, 1],
+      ],
     });
 
     const edge1 = graph.addItem('edge', {
@@ -869,7 +867,7 @@ describe('all node link center', () => {
       },
     });
 
-    defaultGraph.on('node:click', e => {
+    defaultGraph.on('node:click', (e) => {
       e.item.setState('selected', true);
       e.item.refresh();
     });
@@ -1000,7 +998,7 @@ describe('all node link center', () => {
     model.size[1] = 50;
 
     expect(model.size[1]).toEqual(50);
-    expect(node.get('model').size[1]).toEqual(50);
+    expect(node.get('model').size[1]).toEqual(40);
     expect(model.labelCfg.position).toEqual('right');
     expect(model.labelCfg.style.fill).toEqual('blue');
 
@@ -1044,7 +1042,7 @@ describe('mapper fn', () => {
   });
 
   it('node & edge mapper', () => {
-    graph.node(node => ({
+    graph.node((node) => ({
       id: `${node.id}Mapped`,
       size: [30, 30],
       label: node.id,
@@ -1055,7 +1053,7 @@ describe('mapper fn', () => {
       },
     }));
 
-    graph.edge(edge => ({
+    graph.edge((edge) => ({
       id: `edge${edge.id}`,
       label: edge.id,
       labelCfg: {
@@ -1077,7 +1075,7 @@ describe('mapper fn', () => {
     expect(keyShape.attr('fill')).toEqual('#666');
 
     const container = node.getContainer();
-    let label = container.find(element => element.get('className') === 'node-label');
+    let label = container.find((element) => element.get('className') === 'node-label');
     expect(label).not.toBe(undefined);
     expect(label.attr('text')).toEqual('node');
     expect(label.attr('fill')).toEqual('#666');
@@ -1091,7 +1089,7 @@ describe('mapper fn', () => {
     expect(keyShape.attr('opacity')).toEqual(0.5);
     expect(keyShape.get('type')).toEqual('path');
 
-    label = edge.getContainer().find(element => element.get('className') === 'edge-label');
+    label = edge.getContainer().find((element) => element.get('className') === 'edge-label');
     expect(label).not.toBe(undefined);
     expect(label.attr('text')).toEqual('edge');
     expect(label.attr('x')).toEqual(115.5);
@@ -1102,7 +1100,7 @@ describe('mapper fn', () => {
   });
 
   it('node & edge mapper with states', () => {
-    graph.node(node => ({
+    graph.node((node) => ({
       type: 'rect',
       label: node.id,
       style: {
@@ -1126,9 +1124,9 @@ describe('mapper fn', () => {
 
     let keyShape = node.getKeyShape();
     expect(keyShape.attr('fill')).toEqual('#666');
-    expect(node.getContainer().find(element => element.get('className') === 'node-label')).not.toBe(
-      undefined,
-    );
+    expect(
+      node.getContainer().find((element) => element.get('className') === 'node-label'),
+    ).not.toBe(undefined);
 
     graph.setItemState(node, 'selected', true);
     expect(keyShape.attr('blue'));
