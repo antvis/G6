6c1690bd354ebb88317ab8391401c7a0d80e1085
Yanyan-Wang <yanyanwang93@gmail.com>
2021-01-18 21:38:53 +0800

    chore: update version

--- packages/pc/src/global.ts
+++ packages/pc/src/global.ts
@@ -7,7 +7,7 @@ const textColor = 'rgb(0, 0, 0)';
 const colorSet = getColorsWithSubjectColor(subjectColor, backColor);
 
 export default {
-  version: '0.0.6',
+  version: '0.0.7',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
