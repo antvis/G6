7d4d44614d4936041c4be27e9b22cabf59e9d670
openwayne <openwayne@gmail.com>
2021-01-18 14:16:55 +0800

    fix: fix mobile event and behavior

--- packages/mobile/src/util/index.ts
+++ packages/mobile/src/util/index.ts
@@ -1,7 +1,6 @@
 import * as ColorUtil from './color';
 import * as LayoutUtil from './layout';
-import * as GpuUtil from './gpu';
 import { Util } from '@antv/g6-core';
-const G6Util = { ...Util, ...ColorUtil, ...LayoutUtil, ...GpuUtil };
+const G6Util: any = { ...Util, ...ColorUtil, ...LayoutUtil };
 
 export default G6Util;
