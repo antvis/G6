f49a8ecc0f2af13c04d12dacfe264311de70212b
baizn <576375879@qq.com>
2021-01-18 11:51:01 +0800

    fix: update package version

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -64,7 +64,7 @@ const colorSet = {
 };
 
 export default {
-  version: '0.0.6',
+  version: '0.0.7',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
