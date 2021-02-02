93593dece5a59101a99a351cf7552b9af5fda317
AlbertAZ1992 <ziyuximing@163.com>
2021-02-02 18:38:05 +0800

    refactor: using layouts on demond

--- packages/mobile/src/extends/mdsLayout/index.ts
+++ packages/mobile/src/extends/mdsLayout/index.ts
@@ -1,4 +1,4 @@
-import { MDSLayout } from '@antv/layout/es/layout/mds';
+import { MDSLayout } from '@antv/layout/lib/layout/mds';
 import { getExtender } from '../../util/extend';
 
 function layoutExtender(option: any, G6: { registerLayout: Function; }) {
