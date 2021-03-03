ba956da1669915d2dab94811bc629afdc345e339
baizn <576375879@qq.com>
2021-03-03 17:29:47 +0800

    fix: add tslib dependencies

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -64,7 +64,7 @@ const colorSet = {
 };
 
 export default {
-  version: '0.0.14',
+  version: '0.0.15',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
