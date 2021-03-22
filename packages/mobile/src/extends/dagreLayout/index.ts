d12f7c94f97b576168f70d7dc6df12fc48b32014
AlbertAZ1992 <ziyuximing@163.com>
2021-03-22 16:26:19 +0800

    refactor: using extends layout with registerlayout

--- packages/mobile/src/extends/dagreLayout/index.ts
+++
@@ -1,9 +0,0 @@
-import { DagreLayout } from '@antv/layout/lib/layout/dagre';
-import { getExtender } from '../../util/extend';
-
-function layoutExtender(option: any, G6: { registerLayout: Function; }) {
-  const { registerLayout } = G6;
-  registerLayout('dagre', DagreLayout);
-}
-
-export default getExtender(layoutExtender);
