d9845a7ff17db122367f1327610fa71a27fa58ba
AlbertAZ1992 <ziyuximing@163.com>
2021-03-22 18:46:08 +0800

    feat: add default random layout for mobile

--- packages/mobile/src/layout/index.ts
+++ packages/mobile/src/layout/index.ts
@@ -1,3 +1,8 @@
 import { Layout } from '@antv/layout/lib/layout/layout';
 import { registerLayout, unRegisterLayout } from '@antv/layout/lib/registy';
+import { RandomLayout } from '@antv/layout/lib/layout/random';
+
+// 默认提供 random 布局
+registerLayout('random', RandomLayout);
+
 export { Layout, registerLayout, unRegisterLayout };
