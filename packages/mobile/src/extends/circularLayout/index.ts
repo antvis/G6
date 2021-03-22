9c9b6f607a73619cfc4b755b7dd8a1b537c417ac
AlbertAZ1992 <ziyuximing@163.com>
2021-03-22 16:26:19 +0800

    refactor: using extends layout with registerlayout

--- packages/mobile/src/extends/circularLayout/index.ts
+++
@@ -1,9 +0,0 @@
-import { CircularLayout } from '@antv/layout/lib/layout/circular';
-import { getExtender } from '../../util/extend';
-
-function layoutExtender(option: any, G6: { registerLayout: Function; }) {
-  const { registerLayout } = G6;
-  registerLayout('circular', CircularLayout);
-}
-
-export default getExtender(layoutExtender);
