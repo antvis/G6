fe57f20d054e9beb053d03dd5f5f8c40bfea76a4
openwayne <openwayne@gmail.com>
2021-03-27 20:43:27 +0800

    fix: resolve miniapp node_module error

--- packages/core/src/element/shapeBase.ts
+++ packages/core/src/element/shapeBase.ts
@@ -71,6 +71,7 @@ export const shapeBase: ShapeOptions = {
     return deepMix(
       {
         // 解决局部渲染导致的文字移动残影问题
+        // 额外判断window.getComputedStyle的原因为，如果nodemodule不安装在src目录下，小程序会有一个错误的window
         labelCfg: {
           style: {
             fontFamily:
