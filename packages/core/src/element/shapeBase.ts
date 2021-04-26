1c1f6a280c5395e330ca2a89defe7786bab014eb
AlbertAZ1992 <ziyuximing@163.com>
2021-04-26 21:05:01 +0800

    chore: sync master

--- packages/core/src/element/shapeBase.ts
+++ packages/core/src/element/shapeBase.ts
@@ -71,11 +71,10 @@ export const shapeBase: ShapeOptions = {
     return deepMix(
       {
         // 解决局部渲染导致的文字移动残影问题
-        // 额外判断window.getComputedStyle的原因为，如果nodemodule不安装在src目录下，小程序会有一个错误的window
         labelCfg: {
           style: {
             fontFamily:
-              typeof window !== 'undefined' && typeof window.getComputedStyle !== 'undefined'
+              typeof window !== 'undefined'
                 ? window.getComputedStyle(document.body, null).getPropertyValue('font-family') ||
                   'Arial, sans-serif'
                 : 'Arial, sans-serif',
@@ -84,7 +83,7 @@ export const shapeBase: ShapeOptions = {
         descriptionCfg: {
           style: {
             fontFamily:
-              typeof window !== 'undefined' && typeof window.getComputedStyle !== 'undefined'
+              typeof window !== 'undefined'
                 ? window.getComputedStyle(document.body, null).getPropertyValue('font-family') ||
                   'Arial, sans-serif'
                 : 'Arial, sans-serif',
