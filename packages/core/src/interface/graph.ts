4e099defab94fbaa888cc9e905c545a6727872e9
dengshilin <dengshilin@gdcattsoft.com>
2021-08-16 17:25:37 +0800

    style:fitView在设置minZoom时,较小分辨率下缩放失败,添加提示告知.

--- packages/core/src/interface/graph.ts
+++ packages/core/src/interface/graph.ts
@@ -31,7 +31,7 @@ export interface IAbstractGraph extends EventEmitter {
   set: <T = any>(key: string | object, value?: T) => Graph;
   findById: (id: string) => Item;
   translate: (dx: number, dy: number) => void;
-  zoom: (ratio: number, center?: Point) => void;
+  zoom: (ratio: number, center?: Point) => boolean;
 
   /**
    * 获取 graph 的根图形分组
@@ -184,8 +184,9 @@ export interface IAbstractGraph extends EventEmitter {
    * 伸缩视口到一固定比例
    * @param {number} toRatio 伸缩比例
    * @param {Point} center 以center的x, y坐标为中心缩放
+   * @return {boolean} 缩放是否成功
    */
-  zoomTo: (toRatio: number, center?: Point) => void;
+  zoomTo: (toRatio: number, center?: Point) => boolean;
 
   /**
    * 删除元素
