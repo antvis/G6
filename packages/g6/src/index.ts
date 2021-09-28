879ba45c03ffbf4d240a6fec6a7edbabe4accd6f
Yanyan Wang <yanyanwang93@gmail.com>
2021-09-28 17:59:13 +0800

    fix: version refine (#3226)
    * fix: version refine
    
    * chore: CHANGELOG
    
    Co-authored-by: shiwu <shiwu.wyy@antgroup.com>

--- packages/g6/src/index.ts
+++ packages/g6/src/index.ts
@@ -1,7 +1,7 @@
 import G6 from '@antv/g6-pc';
 
-G6.version = '4.3.8';
+G6.version = '4.3.9';
 
 export * from '@antv/g6-pc';
 export default G6;
-export const version = '4.3.8';
+export const version = '4.3.9';
