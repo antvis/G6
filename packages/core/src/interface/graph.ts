36a0e67596cb88bc2075da8c887169f24385a875
Blakko <fabio.tacchelli@gmail.com>
2022-01-24 16:02:36 +0100

    Update interface

--- packages/core/src/interface/graph.ts
+++ packages/core/src/interface/graph.ts
@@ -219,7 +219,7 @@ export interface IAbstractGraph extends EventEmitter {
   /**
    * Performs an expansion with the passed items
    */
-  expand: () => void;
+  expand: (items: { type: ITEM_TYPE, model: ModelConfig }[], stack?: boolean, sortCombo?: boolean) => void;
 
   add: (type: ITEM_TYPE, model: ModelConfig, stack?: boolean) => Item;
 
