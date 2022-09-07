5abec3e2e3e09f8cfe494d373a1417590d8edc2f
Yanyan-Wang <yanyanwang93@gmail.com>
2022-09-07 21:37:22 +0800

    chore: refine

--- packages/pc/tests/unit/layout/circular-web-worker-spec.ts
+++ packages/pc/tests/unit/layout/circular-web-worker-spec.ts
@@ -21,13 +21,11 @@ describe('circular layout(web worker)', () => {
       height: 500,
       defaultNode: { size: 10 },
     });
-    graph.data(data);
-    graph.render();
-
     graph.on('afterlayout', () => {
       const width = graph.get('width');
       const height = graph.get('height');
       const radius = height > width ? width / 2 : height / 2;
+      console.log('nodes', data.nodes[0].x, data.nodes[0].y, 250 + radius)
 
       expect(mathEqual(data.nodes[0].x, 250 + radius)).toEqual(true);
       expect(mathEqual(data.nodes[0].y, 250)).toEqual(true);
@@ -35,6 +33,8 @@ describe('circular layout(web worker)', () => {
       graph.destroy();
       done();
     });
+    graph.data(data);
+    graph.render();
   });
 
   it('circular(web worker) counterclockwise, and fixed radius, start angle, end angle', done => {
