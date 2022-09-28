59c40c7909a68ac832e7ffe65e0191cd867bb949
Blakko <fabio.tacchelli@gmail.com>
2022-09-28 10:16:34 +0200

    Don't set beforeDragNodes if stack ops are off

--- packages/pc/src/behavior/drag-node.ts
+++ packages/pc/src/behavior/drag-node.ts
@@ -38,9 +38,9 @@ export default {
       'combo:drop': 'onDropCombo',
       'node:drop': 'onDropNode',
       'canvas:drop': 'onDropCanvas',
-      'touchstart': 'onTouchStart',
-      'touchmove': 'onTouchMove',
-      'touchend': 'onDragEnd',
+      touchstart: 'onTouchStart',
+      touchmove: 'onTouchMove',
+      touchend: 'onDragEnd',
     };
   },
   validationCombo(item: ICombo) {
@@ -108,7 +108,7 @@ export default {
     // 拖动时，设置拖动元素的 capture 为false，则不拾取拖动的元素
     const group = item.getContainer();
     group.set('capture', false);
-    if (!this.cachedCaptureItems) this.cachedCaptureItems = []
+    if (!this.cachedCaptureItems) this.cachedCaptureItems = [];
     this.cachedCaptureItems.push(item);
 
     // 如果拖动的target 是linkPoints / anchorPoints 则不允许拖动
@@ -133,7 +133,7 @@ export default {
     const currentNodeId = item.get('id');
 
     // 当前拖动的节点是否是选中的节点
-    const dragNodes = nodes.filter(node => {
+    const dragNodes = nodes.filter((node) => {
       const nodeId = node.get('id');
       return currentNodeId === nodeId;
     });
@@ -143,7 +143,7 @@ export default {
       this.targets.push(item);
     } else if (nodes.length > 1) {
       // 拖动多个节点
-      nodes.forEach(node => {
+      nodes.forEach((node) => {
         const locked = node.hasLocked();
         if (!locked) {
           this.targets.push(node);
@@ -152,18 +152,21 @@ export default {
     } else {
       this.targets.push(item);
     }
-    const beforeDragNodes = [];
-    this.targets.forEach(t => {
-      const { x, y, id } = t.getModel();
-      beforeDragNodes.push({ x, y, id });
-    });
-    this.set('beforeDragNodes', beforeDragNodes);
+
+    if (this.graph.get('enabledStack') && this.enableStack) {
+      const beforeDragNodes = [];
+      this.targets.forEach((t) => {
+        const { x, y, id } = t.getModel();
+        beforeDragNodes.push({ x, y, id });
+      });
+      this.set('beforeDragNodes', beforeDragNodes);
+    }
 
     this.hidenEdge = {};
     if (this.get('updateEdge') && this.enableOptimize && !this.enableDelegate) {
-      this.targets.forEach(node => {
+      this.targets.forEach((node) => {
         const edges = node.getEdges();
-        edges.forEach(edge => {
+        edges.forEach((edge) => {
           if (!edge.isVisible()) return;
           this.hidenEdge[edge.getID()] = true;
           edge.hide();
@@ -206,7 +209,7 @@ export default {
           updateEdge: this.get('updateEdge'),
         });
       else
-        this.targets.map(target => {
+        this.targets.map((target) => {
           this.update(target, evt);
         });
     }
@@ -221,7 +224,7 @@ export default {
     }
 
     // 拖动结束后，设置拖动元素 group 的 capture 为 true，允许拾取拖动元素
-    this.cachedCaptureItems?.forEach(item => {
+    this.cachedCaptureItems?.forEach((item) => {
       const group = item.getContainer();
       group.set('capture', true);
     });
@@ -233,9 +236,9 @@ export default {
     }
 
     if (this.get('updateEdge') && this.enableOptimize && !this.enableDelegate) {
-      this.targets.forEach(node => {
+      this.targets.forEach((node) => {
         const edges = node.getEdges();
-        edges.forEach(edge => {
+        edges.forEach((edge) => {
           if (this.hidenEdge[edge.getID()]) edge.show();
           edge.refresh();
         });
@@ -252,11 +255,11 @@ export default {
         after: { nodes: [], edges: [], combos: [] },
       };
 
-      this.get('beforeDragNodes').forEach(model => {
+      this.get('beforeDragNodes').forEach((model) => {
         stackData.before.nodes.push(model);
       });
 
-      this.targets.forEach(target => {
+      this.targets.forEach((target) => {
         const { x, y, id } = target.getModel();
         stackData.after.nodes.push({ x, y, id });
       });
@@ -423,8 +426,8 @@ export default {
           updateEdge: this.get('updateEdge'),
           updateFunc: this.update,
         });
-      else if (!restore) this.targets.map(node => this.update(node, evt));
-    } else this.targets.map(node => this.update(node, evt, restore));
+      else if (!restore) this.targets.map((node) => this.update(node, evt));
+    } else this.targets.map((node) => this.update(node, evt, restore));
   },
   /**
    * 更新节点
@@ -465,9 +468,9 @@ export default {
    * @param evt
    */
   debounceUpdate: debounce(
-    event => {
+    (event) => {
       const { targets, graph, point, origin, evt, updateEdge, updateFunc } = event;
-      targets.map(item => {
+      targets.map((item) => {
         const model: NodeConfig = item.get('model');
         const nodeId: string = item.get('id');
         if (!point[nodeId]) {
