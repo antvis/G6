2a01651a68123f47866a958b0611812109f20b63
daniele-pini <daniele.pini@siren.io>
2022-10-19 10:44:28 +0200

    Remove umd build. Also, separate build from publish. (#51)

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -64,7 +64,7 @@ const colorSet = {
 };
 
 export default {
-  version: '0.7.4-siren.2',
+  version: '0.7.4-siren.3',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
