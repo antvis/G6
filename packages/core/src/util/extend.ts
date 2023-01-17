84ef172a68ad2d699a8c631fd6ff496e1cee6246
Yanyan-Wang <yanyanwang93@gmail.com>
2023-01-17 15:59:03 +0800

    chore: update notes

--- packages/core/src/util/extend.ts
+++ packages/core/src/util/extend.ts
@@ -3,11 +3,11 @@ import Graph from '../runtime/graph';
 import registery from '../stdlib';
 
 /**
- * Extend graph class with custom stdlibs.
+ * Extend graph class with custom libs (extendLibrary), and extendLibrary will be merged into useLib.
  * B1 is the Behavior lib from user, B2 is the Behavior lib of the graph to be extended(built-in graph)
- * TODO: more templates, and might be merged to be two templates for the whole extendLibs
+ * TODO: more templates, and might be merged to be two templates for the whole extendLibrary
  * @param GraphClass graph class to be extended
- * @param extendLibs custom libs to extend
+ * @param extendLibrary custom libs to extend
  * @returns extended graph class
  */
 export const extend = <B1 extends BehaviorRegistry, B2 extends BehaviorRegistry>(
@@ -16,6 +16,7 @@ export const extend = <B1 extends BehaviorRegistry, B2 extends BehaviorRegistry>
     behaviors?: B1,
   }
 ): typeof Graph<B1 & B2> => {
+  // merged the extendLibrary to useLib for global usage
   Object.keys(extendLibrary).forEach(cat => {
     registery.useLib[cat] = Object.assign({}, registery.useLib[cat], extendLibrary[cat] || {});
   });
