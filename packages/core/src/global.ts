7f0471391a29d943a0d72171d2f5fa9e59540c8d
Gary McGovern <gary.mcgovern@siren.io>
2023-01-26 13:52:31 +0100

    [INVE-19651] Bumped version (#59)

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -64,7 +64,7 @@ const colorSet = {
 };
 
 export default {
-  version: '0.8.3-siren.0',
+  version: '0.8.3-siren.1',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
--- packages/pc/src/global.ts
+++ packages/pc/src/global.ts
@@ -7,7 +7,7 @@ const textColor = 'rgb(0, 0, 0)';
 const colorSet = getColorsWithSubjectColor(subjectColor, backColor);
 
 export default {
-  version: '0.8.3-siren.0',
+  version: '0.8.3-siren.1',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
