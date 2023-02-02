8761b275d625755acfbc0d81ac5013b1a2d8ae87
Yanyan-Wang <yanyanwang93@gmail.com>
2023-02-02 10:42:38 +0800

    chore: behavior type generic and test; chore: update notes;

--- packages/core/src/runtime/controller/extension.ts
+++ packages/core/src/runtime/controller/extension.ts
@@ -1,5 +1,14 @@
+import { IGraph } from "../../types";
 
 /**
  * Free extensions(plugins) controller
  */
-export class ExtensionController { }
+export class ExtensionController {
+  public extensions = {};
+  public graph: IGraph;
+
+  constructor(graph: IGraph<any>) {
+    this.graph = graph;
+    // this.tap();
+  }
+}
--- packages/core/src/runtime/controller/layout.ts
+++ packages/core/src/runtime/controller/layout.ts
@@ -1,5 +1,14 @@
+import { IGraph } from "../../types";
 
 /**
  * Manages layout extensions and graph layout.
  */
-export class LayoutController { }
+export class LayoutController {
+  public extensions = {};
+  public graph: IGraph;
+
+  constructor(graph: IGraph<any>) {
+    this.graph = graph;
+    // this.tap();
+  }
+}
