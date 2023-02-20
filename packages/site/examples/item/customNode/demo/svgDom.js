ce6ccf1602bc771c4552ec9e601a6afc70f27018
Eve-Sama <948832626@qq.com>
2023-02-21 00:45:20 +0800

    fix: docs error

--- packages/site/examples/item/customNode/demo/svgDom.js
+++ packages/site/examples/item/customNode/demo/svgDom.js
@@ -1,5 +1,4 @@
 import G6 from '@antv/g6';
-import { useEffect } from 'react';
 
 /**
  *  This demo shows how to register a custom node with SVG DOM shape
@@ -24,7 +23,7 @@ G6.registerNode('dom-node', {
           cfg.size[0] - 5
         }px; height: ${cfg.size[1] - 5}px; display: flex;">
           <div style="height: 100%; width: 33%; background-color: #CDDDFD">
-            <img alt="" style="line-height: 100%; margin-left: 7px;" src="https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*Q_FQT6nwEC8AAAAAAAAAAABkARQnAQ" width="20" height="20" />  
+            <img alt="" style="line-height: 100%; margin-left: 7px;" src="https://gw.alipayobjects.com/mdn/rms_f8c6a0/afts/img/A*Q_FQT6nwEC8AAAAAAAAAAABkARQnAQ" width="20" height="20" />
           </div>
           <span style="margin:auto; padding:auto; color: #5B8FF9">${cfg.label}</span>
         </div>
@@ -65,7 +64,7 @@ const width = container.scrollWidth;
 const height = (container.scrollHeight || 500) - 100;
 
 const descriptionDiv = document.createElement('div');
-descriptionDiv.innerHTML = `由于打包问题，本 demo 的 111-113 行被暂时注释。需要您在代码栏中打开 111-113 行的注释以得到自定义 DOM 节点正确的交互。<br /> Due to the packing problem of the site, we have to note the line 111-113 of this demo temporary. Unnote them to see the result of custom DOM node with interactions please.`;
+descriptionDiv.innerHTML = `由于打包问题，本 demo 的 112-114 行被暂时注释。需要您在代码栏中打开 112-114 行的注释以得到自定义 DOM 节点正确的交互。<br /> Due to the packing problem of the site, we have to note the line 112-114 of this demo temporary. Unnote them to see the result of custom DOM node with interactions please.`;
 container.appendChild(descriptionDiv);
 
 const graph = new G6.Graph({
