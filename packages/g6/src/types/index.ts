44970dedb2ada64cbf2767c506afd35ff2559e37
pomelo-nwu <czynwu@gmail.com>
2023-03-22 17:32:55 +0800

    feat: update typing

--- packages/g6/src/types/index.ts
+++ packages/g6/src/types/index.ts
@@ -3,10 +3,9 @@ export type { GraphData } from './data';
 export type { EdgeDisplayModel, EdgeModel, EdgeUserModel, IEdge } from './edge';
 export type { IG6GraphEvent } from './event';
 export type { IGraph } from './graph';
+export { isImmediatelyInvokedLayoutOptions, isLayoutWorkerized } from './layout'; // function 不应该放在 types 文件下面
 export type {
   ImmediatelyInvokedLayoutOptions,
-  isImmediatelyInvokedLayoutOptions,
-  isLayoutWorkerized,
   LayoutOptions,
   StandardLayoutOptions,
 } from './layout';
