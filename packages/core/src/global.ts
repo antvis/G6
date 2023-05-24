f10375de08b268888b957c15dd856cf6eb80da23
Yanyan-Wang <yanyanwang93@gmail.com>
2023-05-24 10:32:39 +0800

    chore: upgrade layout for umd

--- packages/core/src/global.ts
+++ packages/core/src/global.ts
@@ -64,7 +64,7 @@ const colorSet = {
 };
 
 export default {
-  version: '0.8.13',
+  version: '0.8.14',
   rootContainerClassName: 'root-container',
   nodeContainerClassName: 'node-container',
   edgeContainerClassName: 'edge-container',
