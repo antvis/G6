64d863f48c1df8a61e5e998943ffec44c72cffc1
zqqcee <zqqcee@163.com>
2023-08-19 23:11:44 +0800

    test: unit test in a new way

---
+++ packages/g6/tests/integration/node-star.spec.ts
@@ -0,0 +1,70 @@
+import star from "../demo/demo/star";
+import './utils/useSnapshotMatchers';
+import { createContext } from './utils';
+import { triggerEvent } from './utils/event';
+
+describe('node star', () => {
+    it('should be rendered correctly with Canvas2D', (done) => {
+        const dir = `${__dirname}/snapshots/canvas`;
+        const { backgroundCanvas, canvas, transientCanvas, container } =
+            createContext('canvas', 500, 500);
+
+        const graph = star({
+            container,
+            backgroundCanvas,
+            canvas,
+            transientCanvas,
+            width: 500,
+            height: 500,
+        });
+
+        graph.on('afterlayout', async () => {
+            await expect(canvas).toMatchCanvasSnapshot(dir, 'node-star');
+            //seleted state
+            triggerEvent(graph, 'mousedown', 100, 100);
+            triggerEvent(graph, 'mouseup', 100, 100);
+            await expect(canvas).toMatchCanvasSnapshot(
+                dir,
+                'node-ellipse-selected',
+            );
+            //normal state
+            triggerEvent(graph, 'mousedown', 100, 100);
+            triggerEvent(graph, 'mouseup', 100, 100);
+            await expect(canvas).toMatchCanvasSnapshot(
+                dir,
+                'node-star',
+            );
+            graph.destroy();
+            done();
+        });
+    });
+
+    it('should be rendered correctly with SVG', (done) => {
+        const dir = `${__dirname}/snapshots/svg`;
+        const { backgroundCanvas, canvas, transientCanvas, container } =
+            createContext('svg', 500, 500);
+
+        const graph = star({
+            container,
+            backgroundCanvas,
+            canvas,
+            transientCanvas,
+            width: 500,
+            height: 500,
+        });
+
+        graph.on('afterlayout', async () => {
+            await expect(canvas).toMatchSVGSnapshot(dir, 'node-star');
+            const $selected = document.querySelector(
+                'input#selected',
+            ) as HTMLInputElement;
+            $selected.click();
+            await expect(canvas).toMatchSVGSnapshot(
+                dir,
+                'node-star-seleted',
+            );
+            graph.destroy();
+            done();
+        });
+    });
+});
