e73b9c4d699fd8ba94727f32a49d49c2a796f09f
zqqcee <zqqcee@163.com>
2023-08-20 22:45:02 +0800

    test: unit testing of triangle in a new way

--- packages/g6/tests/demo/demo/triangle.ts
+++ packages/g6/tests/demo/demo/triangle.ts
@@ -1,52 +1,90 @@
-import G6 from '../../../src/index';
-import { container, height, width } from '../../datasets/const';
+import { TestCaseContext } from '../interface';
+import { Graph } from '../../../src/index';
 
-export default () => {
-    const data = {
-        nodes: [
-            {
-                id: 1,
-                data: {
-                    x: 100,
-                    y: 100,
-                    type: 'triangle-node',
-                },
-            },
-            {
-                id: 2,
-                data: {
-                    x: 200,
-                    y: 100,
-                    type: 'circle-node',
-                },
-            },
-            {
-                id: 3,
-                data: {
-                    x: 100,
-                    y: 200,
-                    type: 'circle-node',
-                },
-            },
-        ],
-        edges: [
-            {
-                id: 4,
-                source: 1,
-                target: 2,
-                data: {},
-            },
-        ],
-    };
+let graph: any;
 
-    const graph = new G6.Graph({
-        container,
-        width,
-        height,
-        data,
+const createCtrlContainer = (container: HTMLElement) => {
+    const ctrlContainer = document.createElement('div');
+    ctrlContainer.id = 'ctrl-container';
+    ctrlContainer.style.width = '100%';
+    ctrlContainer.style.height = '200px';
+    ctrlContainer.style.backgroundColor = '#eee';
+    container.appendChild(ctrlContainer);
+};
+
+const createCtrl = () => {
+    const conEle = document.querySelector('div#ctrl-container')!;
+    const selectedStyleLabel = document.createElement('span');
+    selectedStyleLabel.textContent = 'custom selected style';
+    // selectedStyleLabel.style.position = 'absolute';
+    selectedStyleLabel.style.top = '124px';
+    selectedStyleLabel.style.left = '16px';
+    selectedStyleLabel.style.zIndex = '100';
+
+    const selectedStyleCb = document.createElement('input');
+    selectedStyleCb.setAttribute('id', 'selected');
+    selectedStyleCb.type = 'checkbox';
+    selectedStyleCb.value = 'selected';
+    // selectedStyleCb.style.position = 'absolute';
+    selectedStyleCb.style.width = '20px';
+    selectedStyleCb.style.height = '20px';
+    selectedStyleCb.style.top = '124px';
+    selectedStyleCb.style.left = '166px';
+    selectedStyleCb.style.zIndex = '100';
+
+    selectedStyleCb.addEventListener('click', (e) => {
+        if (selectedStyleCb.checked) {
+            graph.setItemState('node1', 'selected', true);
+        } else {
+            graph.setItemState('node1', 'selected', false);
+        }
+    });
+    conEle.appendChild(selectedStyleLabel);
+    conEle.appendChild(selectedStyleCb);
+}
+
+const data = {
+    nodes: [
+        {
+            id: 'node1',
+            data: {
+                x: 100,
+                y: 100,
+                type: 'triangle-node',
+            },
+        },
+        {
+            id: 'node2',
+            data: {
+                x: 200,
+                y: 100,
+                type: 'rect-node',
+            },
+        },
+
+    ],
+    edges: [
+        {
+            id: 3,
+            source: 'node1',
+            target: 'node2',
+            data: {},
+        },
+    ],
+};
+
+
+export default (context: TestCaseContext) => {
+    const { width, height, container } = context;
+    createCtrlContainer(container!);
+    createCtrl();
+
+    graph = new Graph({
+        ...context,
         type: 'graph',
+        data,
         modes: {
-            default: ['click-select', 'drag-canvas', 'zoom-canvas', 'drag-node'],
+            default: ['click-select', 'drag-node'],
         },
         node: (nodeInnerModel: any) => {
             const { id, data } = nodeInnerModel;
@@ -54,16 +92,14 @@ export default () => {
                 id,
                 data: {
                     ...data,
-                    keyShape: {
-                        direction: 'left',
-                    },
                     labelShape: {
                         text: 'label',
                         position: 'bottom',
                     },
                     iconShape: {
-                        img: 'https://gw.alipayobjects.com/zos/basement_prod/012bcf4f-423b-4922-8c24-32a89f8c41ce.svg',
-                        // text: 'label',
+                        // img: 'https://gw.alipayobjects.com/zos/basement_prod/012bcf4f-423b-4922-8c24-32a89f8c41ce.svg',
+                        text: 'label',
+                        fontSize: 8,
                     },
                     badgeShapes: [
                         {
