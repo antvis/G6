f4fd5ace59bbd968270e3aaa9cefb329dd542317
pomelo-nwu <czynwu@outlook.com>
2023-08-25 10:35:20 +0800

    feat: update minimap demo

--- packages/site/examples/tool/minimap/demo/minimap.ts
+++ packages/site/examples/tool/minimap/demo/minimap.ts
@@ -1,16 +1,6 @@
 import { Graph, Util } from '@antv/g6';
 import insertCss from 'insert-css';
 
-insertCss(`
-  .g6-minimap-container {
-    border: 1px solid #e2e2e2;
-  }
-  .g6-minimap-viewport {
-    border: 2px solid rgb(25, 128, 255);
-  }
-`);
-
-
 const container = document.getElementById('container');
 const width = container.scrollWidth;
 const height = (container.scrollHeight || 500) - 110;
@@ -18,29 +8,33 @@ const data = Util.mock(20).random();
 /** minimap with string config */
 const minimap1 = 'minimap';
 /** minimap with object onfig and delegate type */
-const minimap2 =  {
+const minimap2 = {
   key: 'minimap2',
   type: 'minimap',
-  size: [200, 200],
+  size: [300, 200],
   mode: 'delegate',
   delegateStyle: {
     fill: 'red',
   },
-}
+  className: 'g6-minimap-2',
+  viewportClassName: 'g6-minimap-viewport-2',
+};
 /** minimap with object onfig and keyShape type */
 const minimap3 = {
   key: 'minimap3',
   type: 'minimap',
   mode: 'keyShape',
-  size: [200, 300],
+  size: [300, 200],
+  className: 'g6-minimap-3',
+  viewportClassName: 'g6-minimap-viewport-3',
 };
 new Graph({
   container,
   width,
   height,
   data,
-  layout:{
-    type:'force',
+  layout: {
+    type: 'force',
   },
   node: {
     labelShape: {
@@ -53,8 +47,29 @@ new Graph({
     },
   },
   modes: {
-    default: ['brush-select',"zoom-canvas","activate-relations","drag-canvas","drag-node"],
+    default: ['brush-select', 'zoom-canvas', 'activate-relations', 'drag-canvas', 'drag-node'],
   },
-  plugins:[minimap1,minimap2,minimap3],
-})
- 
+  plugins: [minimap1, minimap2, minimap3],
+});
+
+/** set the style of minimap  */
+insertCss(`
+  .g6-minimap-2 {
+    position:absolute;
+    bottom:40px;
+    right:40px;
+    box-shadow:0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+  }
+  .g6-minimap-viewport-2 {
+    border: 2px solid rgb(25, 128, 255);
+  }
+  .g6-minimap-3 {
+    position:absolute;
+    bottom:40px;
+    left:40px;
+    box-shadow:0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2);
+  }
+  .g6-minimap-viewport-3 {
+    border: 2px solid rgb(25, 128, 255);
+  }
+`);
