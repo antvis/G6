ceb81003c025f21368774c6ea5ebf17e8b53b00b
liujiangyu <liujiangyu@kuaishou.com>
2023-09-07 21:41:36 +0800

    chore: add scroll-canvas integration

---
+++ packages/g6/tests/integration/behaviors-scroll-canvas.spec.ts
@@ -0,0 +1,63 @@
+import { resetEntityCounter } from '@antv/g';
+import scrollCanvas from '../demo/behaviors/activate-relations';
+import './utils/useSnapshotMatchers';
+import { createContext, triggerEvent } from './utils';
+
+describe('Scroll canvas behavior', () => {
+  beforeEach(() => {
+    /**
+     * SVG Snapshot testing will generate a unique id for each element.
+     * Reset to 0 to keep snapshot consistent.
+     */
+    resetEntityCounter();
+  });
+
+  it('should be rendered correctly with Canvas2D', (done) => {
+    const dir = `${__dirname}/snapshots/canvas`;
+    const { backgroundCanvas, canvas, transientCanvas, container } =
+      createContext('canvas', 500, 500);
+
+    const graph = scrollCanvas({
+      container,
+      backgroundCanvas,
+      canvas,
+      transientCanvas,
+      width: 500,
+      height: 500,
+    });
+
+    graph.on('afterlayout', async () => {
+      await expect(canvas).toMatchCanvasSnapshot(
+        dir,
+        'behaviors-scroll-canvas',
+      );
+
+      graph.destroy();
+      done();
+    });
+  });
+
+  it('should be rendered correctly with SVG', (done) => {
+    const dir = `${__dirname}/snapshots/svg`;
+    const { backgroundCanvas, canvas, transientCanvas, container } =
+      createContext('svg', 500, 500);
+
+    const graph = scrollCanvas({
+      container,
+      backgroundCanvas,
+      canvas,
+      transientCanvas,
+      width: 500,
+      height: 500,
+    });
+
+    graph.on('afterlayout', async () => {
+      await expect(canvas).toMatchSVGSnapshot(
+        dir,
+        'behaviors-scroll-canvas',
+      );
+      graph.destroy();
+      done();
+    });
+  });
+});
