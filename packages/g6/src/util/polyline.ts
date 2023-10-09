b300d3f93e5b4a0c6c97aa40e3685af4f39047ac
yvonneyx <banxuan.zyx@antgroup.com>
2023-10-09 15:43:45 +0800

    fix: lint

--- packages/g6/src/util/polyline.ts
+++ packages/g6/src/util/polyline.ts
@@ -582,7 +582,10 @@ export class QuadTree {
   private southwest?: QuadTree;
   private southeast?: QuadTree;
 
-  constructor(public boundary: AABB, capacity: number) {
+  constructor(
+    public boundary: AABB,
+    capacity: number,
+  ) {
     this.capacity = capacity;
   }
 
