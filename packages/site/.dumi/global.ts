7feab21cd5e124c939cb8a1226bbc5a4f931e8f6
Chushen <yangtao.yangtao@antgroup.com>
2023-10-11 11:49:05 +0800

    chore: config for global types

--- packages/site/.dumi/global.ts
+++ packages/site/.dumi/global.ts
@@ -1,17 +1,18 @@
-if (window) {
+if window {
   // window.g6 = require('@antv/g6/es'); // import the source for debugging
-  (window as any).g6 = require('@antv/g6/lib'); // import the source for debugging
+  window.g6 = require('@antv/g6/lib'); // import the source for debugging
 
-  (window as any).layoutGPU = require('@antv/layout-gpu'); // import the source for debugging
-  (window as any).Algorithm = require('@antv/algorithm');
-  (window as any).layoutWASM = require('@antv/layout-wasm'); // import the source for debugging
-  (window as any).GraphLib = require('@antv/graphlib');
+  window.layoutGPU = require('@antv/layout-gpu'); // import the source for debugging
+  window.Algorithm = require('@antv/algorithm');
+  window.layoutWASM = require('@antv/layout-wasm'); // import the source for debugging
+  window.GraphLib = require('@antv/graphlib');
 
-  (window as any).MapViewPlugin = require('@antv/g6-plugin-map-view');
-  // (window as any).g6 = require('@antv/g6/dist/g6.min.js'); // import the package for webworker
-  (window as any).insertCss = require('insert-css');
-  (window as any).Chart = require('@antv/chart-node-g6');
-  (window as any).AntVUtil = require('@antv/util');
-  (window as any).GraphLayoutPredict = require('@antv/vis-predict-engine');
-  (window as any).stats = require('stats.js');
+  window.MapViewPlugin = require('@antv/g6-plugin-map-view');
+  // window.g6 = require('@antv/g6/diFst/g6.min.js'); // import the package for webworker
+  window.insertCss = require('insert-css');
+  window.Chart = require('@antv/chart-node-g6');
+  window.AntVUtil = require('@antv/util');
+  window.GraphLayoutPredict = require('@antv/vis-predict-engine');
+  window.stats = require('stats.js');
+  window.g2 = require('@antv/g2');
 }
