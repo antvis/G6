38d93bc7d6b23a42cfb1449205cafba2723fdaba
Chushen <yangtao.yangtao@antgroup.com>
2023-10-19 20:37:50 +0800

    chore: remove old react-node package

--- packages/react-node/src/API/ImageStyle.tsx
+++
@@ -1,5 +0,0 @@
-import { ImageStyle } from '../ReactNode/Shape/Image';
-import React from 'react';
-
-const Image: React.FC<ImageStyle> = (props) => <div>{props}</div>;
-export default Image;
--- packages/react-node/src/API/TextStyle.tsx
+++
@@ -1,5 +0,0 @@
-import { TextStyle } from '../ReactNode/Shape/Text';
-import React from 'react';
-
-const Text: React.FC<TextStyle> = (props) => <div>{props}</div>;
-export default Text;
