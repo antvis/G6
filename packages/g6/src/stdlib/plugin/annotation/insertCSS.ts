69876f857c8dfa4b7d76654511250843ffa64968
liujiangyu <liujiangyu@kuaishou.com>
2023-11-20 16:36:10 +0800

    chore: improve card css

--- packages/g6/src/stdlib/plugin/annotation/insertCSS.ts
+++ packages/g6/src/stdlib/plugin/annotation/insertCSS.ts
@@ -11,29 +11,48 @@ export function insertCSS() {
             box-shadow: 0 0 8px rgba(0, 0, 0, 0.85);
             display: flex;
             flex-direction: column;
+            border-radius: 2px;
+            position: absolute;
+            /* font-family reference ant-design: ttps://ant.design/docs/spec/font */
+            font-family: -apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans',sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji';
+        }
+        .g6-annotation-wrapper-hide {
+            display: none;
         }
         .g6-annotation-wrapper-move {
             background-color: rgba(255,255,255, 0.8);
             z-index: 10;
         }
+        .g6-annotation-wrapper-collapsed .g6-annotation-header-wapper {
+            border-radius: inherit;
+        }
+        .g6-annotation-wrapper-collapsed .g6-annotation-content {
+            display: none;
+        }
+        .g6-annotation-wrapper-collapsed .g6-annotation-content-input-wrapper {
+            display: none;
+        }
+        
         .g6-annotation-wrapper-move .g6-annotation-header-wapper {
             background-color: rgb(91,143,249, 0.8);
         }
 
         .g6-annotation-header-wapper {
-            padding: 4px 8px;
+            padding: 4px;
             background-color: rgb(91,143,249);
             display: inline-flex;
             justify-content: space-between;
             align-items: center;
             cursor: move;
             user-select: none;
+            border-radius: 2px 2px 0 0;
         }
         .g6-annotation-title {
             cursor: text;
             user-select: text;
             margin: 0 10px 0 0;
             word-break: break-all;
+            padding: 0 4px;
         }
         .g6-annotation-header-btns {
             display: inline-flex;
@@ -47,35 +66,43 @@ export function insertCSS() {
             text-align: center;
         }
         .g6-annotation-content {
-            margin: 16px 8px;
+            margin: 12px 4px;
+            padding: 0 4px;
             cursor: text;
             word-break: break-all;
             overflow: auto;
             white-space: pre-wrap;
+            font-size: 14px;
         }
         .g6-annotation-title-input-wrapper {
             margin: 0 10px 0 0;
             flex: 1 1 auto;
             display: inline-flex;
         }
+        .g6-annotation-content-input-wrapper {
+            margin: 12px 0;
+            padding: 0 4px;
+            box-sizing: border-box;
+            flex: 1 1 auto;
+            display: flex;
+            flex-direction: column;
+        }
+        .g6-annotation-input {
+            border: 1px solid #d9d9d9;
+            font-size: 14px;
+            box-sizing: border-box;
+            word-break: break-all;
+            font-family: inherit;
+        }
+        .g6-annotation-input:placeholder {
+            color: #95999e;
+        }
         .g6-annotation-title-input, .g6-annotation-content-input {
             min-width: 32px;
             width: 100%;
-            box-sizing: border-box;
-            height: 100%;
-            word-break: break-all;
-            padding: 4px;
-        }
-        .g6-annotation-title-input::placeholder, .g6-annotation-content-input::placeholder {
-            color: #95999e;
-        }
-        .g6-annotation-content-input {
-            min-height: 80px;
-        }
-        .g6-annotation-content-input-wrapper {
-            margin: 16px 0;
-            padding: 0 8px;
-            box-sizing: border-box;
+            padding: 0 3px;
+            flex: 1 1 auto;
+            max-width: 100%;
         }
     `);
 }
