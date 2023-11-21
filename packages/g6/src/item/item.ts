5dff5757d7cbcf5b44ed83d26c3decde6393b61d
yvonneyx <55794321+yvonneyx@users.noreply.github.com>
2023-11-21 17:19:14 +0800

    fix: Improve local update mechanism of encode mapper (#5153)
    * fix: enhance mapper encode logic
    
    * fix: fixed a cache miss when asynchronously updating the show element
    
    * fix: edit data transformer active lifecyle
    
    ---------
    
    Co-authored-by: yvonneyx <banxuan.zyx@antgroup.com>

--- packages/g6/src/item/item.ts
+++ packages/g6/src/item/item.ts
@@ -1,5 +1,5 @@
 import { Group, DisplayObject, AABB, IAnimation } from '@antv/g';
-import { clone, isFunction, isObject, pick, throttle } from '@antv/util';
+import { isFunction, isObject, throttle } from '@antv/util';
 import { OTHER_SHAPES_FIELD_NAME, RESERVED_SHAPE_IDS } from '../constant';
 import { EdgeShapeMap } from '../types/edge';
 import {
@@ -386,7 +386,9 @@ export default abstract class Item implements IItem {
       if (isReservedShapeId) {
         // reserved shapes, fieldName is shapeId
         displayModelData[fieldName] =
-          displayModelData[fieldName] || isObject(innerModel.data[fieldName])
+          displayModelData[fieldName] ||
+          isObject(innerModel.data[fieldName]) ||
+          isObject(this.displayModel?.data[fieldName])
             ? {
                 ...(innerModel.data[fieldName] as object),
                 ...(this.displayModel?.data[fieldName] as object),
@@ -512,7 +514,7 @@ export default abstract class Item implements IItem {
       }
 
       this.visible = true;
-      this.cacheHiddenByItem = {};
+      if (!shapeIds) this.cacheHiddenByItem = {};
       // restore the states
       if (this.states?.length) {
         this.drawWithStates([]);
