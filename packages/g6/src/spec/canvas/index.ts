e181e86ee1fbca84cf5292f1e00b59bbc4249e26
Chushen <yangtao.yangtao@antgroup.com>
2024-01-12 14:26:13 +0800

    refactor: remove background, cursor option

--- packages/g6/src/spec/canvas/index.ts
+++ packages/g6/src/spec/canvas/index.ts
@@ -7,7 +7,7 @@ import type { Layer } from '../../runtime/layered-canvas/types';
  * <en/> Canvas spec
  * @public
  */
-export type CanvasOption = Pick<CanvasConfig, 'background' | 'cursor' | 'devicePixelRatio'> & {
+export type CanvasOption = Pick<CanvasConfig, 'devicePixelRatio'> & {
   /**
    * <zh/> 画布宽度
    *
