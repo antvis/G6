9afec54a43ba2a6784b785af5864a5debff1660f
hustcc <i@hust.cc>
2024-01-19 10:44:36 +0800

    test: add tests for utils/array

---
+++ packages/g6/tests/unit/utils/array.spec.ts
@@ -0,0 +1,39 @@
+import { diffSet, intersectSet, isArrayOverlap, replaceElements, unionSet, uniqBy } from '../../../src/utils/array';
+
+describe('array', () => {
+  it('isArrayOverlap', () => {
+    expect(isArrayOverlap([], [3, 4])).toBe(false);
+    expect(isArrayOverlap([1, 2], [])).toBe(false);
+    expect(isArrayOverlap([1, 2], [3, 4])).toBe(false);
+    expect(isArrayOverlap([1, 2], [2, 3])).toBe(true);
+  });
+
+  it('diffSet', () => {
+    expect(diffSet([1, 2], [3, 4])).toEqual([1, 2]);
+    expect(diffSet([1, 2], [2, 3])).toEqual([1]);
+    expect(diffSet(['1', '2'], ['2', '3'])).toEqual(['1']);
+
+    expect(diffSet([{ a: 1 }, { a: 2 }], [{ a: 3 }, { a: 4 }])).toEqual([{ a: 1 }, { a: 2 }]);
+    expect(diffSet([{ a: 1 }, { a: 2 }], [{ a: 2 }, { a: 3 }])).toEqual([{ a: 1 }]);
+  });
+
+  it('unionSet', () => {
+    expect(unionSet([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+    expect(unionSet([1, 2], [2, 3])).toEqual([1, 2, 3]);
+  });
+
+  it('intersectSet', () => {
+    expect(intersectSet([1, 2], [3, 4])).toEqual([]);
+    expect(intersectSet([1, 2], [2, 3])).toEqual([2]);
+  });
+
+  it('replaceElements', () => {
+    expect(replaceElements([1, 2], 2, 3)).toEqual([1, 3]);
+    expect(replaceElements([1, 2], 3, 4)).toEqual([1, 2]);
+  });
+
+  it('uniqBy', () => {
+    expect(uniqBy([{ a: 1 }, { a: 2 }, { a: 1 }], 'a')).toEqual([{ a: 1 }, { a: 2 }]);
+    expect(uniqBy([{ a: 1 }, { a: 2 }], 'a')).toEqual([{ a: 1 }, { a: 2 }]);
+  });
+});
