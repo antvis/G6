360928b118f955351452168d173302e3a07d45bb
Chushen <yangtao.yangtao@antgroup.com>
2024-01-19 14:23:34 +0800

    refactor: merge controllers into common class

--- packages/g6/src/runtime/controller/index.ts
+++ packages/g6/src/runtime/controller/index.ts
@@ -1,7 +1,36 @@
-export * from './data';
-export * from './interaction';
-export * from './item';
-export * from './layout';
-export * from './plugin';
-export * from './theme';
-export * from './viewport';
+import type { Graph } from '../../types/graph';
+import { DataController } from './data';
+import { InteractionController } from './interaction';
+import { ItemController } from './item';
+import { LayoutController } from './layout';
+import { PluginController } from './plugin';
+import { ThemeController } from './theme';
+import { ViewportController } from './viewport';
+
+export class Controller {
+  public data: DataController;
+
+  public interaction: InteractionController;
+
+  public item: ItemController;
+
+  public layout: LayoutController;
+
+  public widget: PluginController;
+
+  public theme: ThemeController;
+
+  public viewport: ViewportController;
+
+  constructor(graph: Graph) {
+    this.data = new DataController(graph);
+    this.interaction = new InteractionController(graph);
+    this.item = new ItemController(graph);
+    this.layout = new LayoutController(graph);
+    this.widget = new PluginController(graph);
+    this.theme = new ThemeController(graph);
+    this.viewport = new ViewportController(graph);
+  }
+
+  public destroy() {}
+}
