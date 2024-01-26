440a8da81a4d8bcf970ed9ec8a123cfb96e68e26
Aaron <yangtao.yangtao@antgroup.com>
2024-01-26 10:18:48 +0800

    refactor(test): update snapshot naming rule

--- packages/g6/__tests__/integration/utils/get-cases.ts
+++ packages/g6/__tests__/integration/utils/get-cases.ts
@@ -4,5 +4,9 @@ export function getCases(module: Record<string, TestCase>) {
   const cases = Object.entries(module);
   const only = cases.filter(([, { only = false }]) => only);
   if (only.length !== 0 && process.env.CI === 'true') throw new Error('Cannot run only tests in CI');
-  return (only.length !== 0 ? only : cases).filter(([, { skip = false }]) => !skip);
+  return (only.length !== 0 ? only : cases)
+    .filter(([, { skip = false }]) => !skip)
+    .map(([name, testCase]) => [kebabCase(name), testCase] as [string, TestCase]);
 }
+
+const kebabCase = (str: string) => str.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
