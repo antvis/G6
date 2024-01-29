e1a6baf51d28eca5dbeeddf031c6d6f4a13dee48
Aaron <yangtao.yangtao@antgroup.com>
2024-01-29 10:32:13 +0800

    feat(themes): add theme plugin type definition

--- packages/g6/src/registry/types.ts
+++ packages/g6/src/registry/types.ts
@@ -1,12 +1,12 @@
 import type { DisplayObject } from '@antv/g';
 import type { STDAnimation } from '../animations/types';
 import type { BaseNode, BaseNodeStyleProps } from '../elements/nodes';
+import type { Theme } from '../themes/types';
 
 // TODO 待使用正式类型定义 / To be used formal type definition
 declare type Node = BaseNode<BaseNodeStyleProps<any>, DisplayObject>;
 declare type Edge = unknown;
 declare type Combo = unknown;
-declare type Theme = unknown;
 declare type Layout = unknown;
 declare type Behavior = unknown;
 declare type Widget = unknown;
--- packages/g6/src/themes/types.ts
+++ packages/g6/src/themes/types.ts
@@ -1,1 +1,11 @@
+import type { StaticComboOptions } from '../spec/element/combo';
+import type { StaticEdgeOptions } from '../spec/element/edge';
+import type { StaticNodeOptions } from '../spec/element/node';
+
 export type BuiltInTheme = 'light' | 'dark';
+
+export type Theme = {
+  node?: StaticNodeOptions;
+  edge?: StaticEdgeOptions;
+  combo?: StaticComboOptions;
+};
