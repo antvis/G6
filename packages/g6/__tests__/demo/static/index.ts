0d403d518e16a6afe61fa9563c6abefc1257e04a
yvonneyx <banxuan.zyx@antgroup.com>
2024-01-30 17:06:38 +0800

    fix: export circle demo

--- packages/g6/__tests__/demo/static/index.ts
+++ packages/g6/__tests__/demo/static/index.ts
@@ -1,5 +1,6 @@
 export * from './edge-line';
 export * from './layered-canvas';
+export * from './node-circle.ts';
 export * from './shape-badge';
 export * from './shape-icon';
 export * from './shape-label';
