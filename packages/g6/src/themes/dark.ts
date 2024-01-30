77b47176a174f1a59dec1e2389da041f8dcb531b
Aaron <yangtao.yangtao@antgroup.com>
2024-01-30 20:51:06 +0800

    feat(theme): add built in theme

---
+++ packages/g6/src/themes/dark.ts
@@ -0,0 +1,53 @@
+import { Theme } from './types';
+
+export const DARK_THEME: Theme = {
+  node: {
+    style: {
+      fill: '#f8f8f8',
+      stroke: '#8b9baf',
+    },
+    state: {
+      selected: {
+        lineWidth: 2,
+        stroke: '#34d058',
+      },
+      active: {
+        lineWidth: 2,
+      },
+    },
+    animation: {
+      enter: 'fade',
+      exit: 'fade',
+    },
+  },
+  edge: {
+    style: {
+      fill: '#ffea7f',
+    },
+    state: {
+      selected: {
+        lineWidth: 2,
+        stroke: '##79b8ff',
+      },
+      active: {
+        lineWidth: 2,
+      },
+    },
+  },
+  combo: {
+    style: {
+      fill: 'rgba(170, 174, 178, 0.2)',
+      stroke: '#aaaeb2',
+      lineWidth: 1,
+    },
+    state: {
+      selected: {
+        lineWidth: 2,
+        stroke: '#34d058',
+      },
+      active: {
+        stroke: '#7b464e',
+      },
+    },
+  },
+};
--- packages/g6/src/themes/index.ts
+++ packages/g6/src/themes/index.ts
@@ -1,7 +1,12 @@
+import { DARK_THEME } from './dark';
+import { LIGHT_THEME } from './light';
+
 /**
  * <zh/> 内置主题
  *
  * <en/> Built-in themes
  */
-
-export {};
+export const BUILT_IN_THEMES = {
+  light: LIGHT_THEME,
+  dark: DARK_THEME,
+};
