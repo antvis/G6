368075cac8107083045f4ed7053a7d02c2bfb30f
yvonneyx <banxuan.zyx@antgroup.com>
2024-01-31 23:08:45 +0800

    feat(edge): add cubic vertical edge element

---
+++ packages/g6/src/elements/edges/cubic-vertical.ts
@@ -0,0 +1,57 @@
+import type { DisplayObjectConfig, PathStyleProps } from '@antv/g';
+import { deepMix } from '@antv/util';
+import type { Point } from '../../types';
+import { type BaseEdgeStyleProps } from './base-edge';
+import { Cubic } from './cubic';
+
+type CubicVerticalKeyStyleProps = PathStyleProps & {
+  /**
+   * <zh/> 边的起点
+   * <en/> The source point. Represents the start of the edge
+   */
+  sourcePoint: Point;
+  /**
+   * <zh/> 边的终点
+   * <en/> The target point. Represents the end of the edge
+   */
+  targetPoint: Point;
+  /**
+   * <zh/> 控制点在两端点连线上的相对位置，范围为`0-1`
+   * <en/> The relative position of the control point on the line, ranging from `0-1`
+   */
+  curvePosition?: number | [number, number];
+  /**
+   * <zh/> 控制点距离两端点连线的距离，可理解为控制边的弯曲程度
+   * <en/> The distance of the control point from the line
+   */
+  curveOffset?: number | [number, number];
+};
+
+export type CubicVerticalStyleProps = BaseEdgeStyleProps<CubicVerticalKeyStyleProps>;
+
+type CubicVerticalOptions = DisplayObjectConfig<CubicVerticalStyleProps>;
+
+export class CubicVertical extends Cubic {
+  static defaultStyleProps: Partial<CubicVerticalStyleProps> = {
+    curvePosition: [0.5, 0.5],
+    curveOffset: [0, 0],
+    isBillboard: true,
+  };
+
+  constructor(options: CubicVerticalOptions) {
+    super(deepMix({}, { style: CubicVertical.defaultStyleProps }, options));
+  }
+
+  protected getControlPoints(
+    sourcePoint: Point,
+    targetPoint: Point,
+    curvePosition: [number, number],
+    curveOffset: [number, number],
+  ): [Point, Point] {
+    const yDist = targetPoint[1] - sourcePoint[1];
+    return [
+      [sourcePoint[0], sourcePoint[1] + yDist * curvePosition[0] + curveOffset[0]],
+      [targetPoint[0], targetPoint[1] - yDist * curvePosition[1] + curveOffset[1]],
+    ];
+  }
+}
--- packages/g6/src/elements/edges/index.ts
+++ packages/g6/src/elements/edges/index.ts
@@ -1,9 +1,11 @@
 export { Cubic } from './cubic';
 export { CubicHorizontal } from './cubic-horizontal';
+export { CubicVertical } from './cubic-vertical';
 export { Line } from './line';
 export { Quadratic } from './quadratic';
 
 export type { CubicStyleProps } from './cubic';
 export type { CubicHorizontalStyleProps } from './cubic-horizontal';
+export type { CubicVerticalStyleProps } from './cubic-vertical';
 export type { LineStyleProps } from './line';
 export type { QuadraticStyleProps } from './quadratic';
