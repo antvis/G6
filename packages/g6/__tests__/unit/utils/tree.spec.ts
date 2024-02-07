7f5814ada8da36cb3ff16924269292b75cf1d57d
Aaron <yangtao.yangtao@antgroup.com>
2024-02-07 17:14:17 +0800

    refactor(utils): remove transformGraphDataToTreeData

--- packages/g6/__tests__/unit/utils/tree.spec.ts
+++ packages/g6/__tests__/unit/utils/tree.spec.ts
@@ -1,4 +1,4 @@
-import { transformGraphDataToTreeData, transformTreeDataToGraphData } from '../../../src/utils/tree';
+import { transformTreeDataToGraphData } from '../../../src/utils/tree';
 
 describe('tree', () => {
   it('transformTreeDataToGraphData', () => {
@@ -66,54 +66,5 @@ describe('tree', () => {
       ],
       edges: [{ source: 'root', target: 'child', data: { weight: 11 } }],
     });
-  });
-
-  it('transformGraphDataToTreeData', () => {
-    // tree
-    expect(
-      transformGraphDataToTreeData({
-        nodes: [
-          { id: 'root', style: { fill: 'red' }, data: { value: 10 } },
-          { id: 'child', style: { fill: 'green' }, data: { value: 1 } },
-        ],
-        edges: [{ source: 'root', target: 'child', data: { weight: 11 } }],
-      }),
-    ).toEqual([
-      {
-        id: 'root',
-        style: { fill: 'red' },
-        data: { value: 10 },
-        children: [{ id: 'child', style: { fill: 'green' }, data: { value: 1 } }],
-      },
-    ]);
-
-    // forest
-    expect(
-      transformGraphDataToTreeData({
-        nodes: [
-          { id: 'root', style: { fill: 'red' }, data: { value: 10 } },
-          { id: 'child', style: { fill: 'green' }, data: { value: 1 } },
-          { id: 'root2', style: { fill: 'blue' }, data: { value: 10 } },
-          { id: 'child2', style: { fill: 'yellow' }, data: { value: 1 } },
-        ],
-        edges: [
-          { source: 'root', target: 'child', data: { weight: 11 } },
-          { source: 'root2', target: 'child2', data: { weight: 11 } },
-        ],
-      }),
-    ).toEqual([
-      {
-        id: 'root',
-        style: { fill: 'red' },
-        data: { value: 10 },
-        children: [{ id: 'child', style: { fill: 'green' }, data: { value: 1 } }],
-      },
-      {
-        id: 'root2',
-        style: { fill: 'blue' },
-        data: { value: 10 },
-        children: [{ id: 'child2', style: { fill: 'yellow' }, data: { value: 1 } }],
-      },
-    ]);
   });
 });
--- packages/g6/src/utils/tree.ts
+++ packages/g6/src/utils/tree.ts
@@ -1,58 +1,6 @@
-import type { ID } from '@antv/graphlib';
 import type { EdgeData, GraphData, NodeData } from '../spec';
 import type { TreeData } from '../types';
 import { dfs } from './traverse';
-
-type GraphDataGetter = {
-  getID?: (datum: NodeData) => ID;
-  getTreeNodeData?: (datum: NodeData) => TreeData;
-};
-
-/**
- * <zh/> 将图数据转换为树数据
- *
- * <en/> Convert graph data to tree data
- * @param graphData - <zh/> 图数据 | <en/> Graph data
- * @param getter - <zh/> 获取 ID 和树节点数据的方法 | <en/> Methods to get ID and tree node data
- * @returns <zh/> 树数据 | <en/> Tree data
- */
-export function transformGraphDataToTreeData(graphData: GraphData, getter?: GraphDataGetter): TreeData[] {
-  const { nodes = [], edges = [] } = graphData;
-  const { getID = (datum: NodeData) => datum.id, getTreeNodeData = (datum: NodeData) => datum as TreeData } =
-    getter || {};
-
-  const nodeIdToChildren: Record<ID, NodeData[]> = {};
-
-  for (const edge of edges) {
-    const { source, target } = edge;
-    if (!(source in nodeIdToChildren)) {
-      nodeIdToChildren[source] = [];
-    }
-    nodeIdToChildren[source].push(nodes.find((node) => node.id === target)!);
-  }
-
-  const treeData: TreeData[] = [];
-  const visited: Set<ID> = new Set();
-
-  for (const node of nodes) {
-    if (!visited.has(getID(node))) {
-      const tree: TreeData = { ...getTreeNodeData(node), children: [] };
-      dfs(
-        node,
-        (n) => {
-          if (node === n) return;
-          visited.add(getID(n));
-          tree.children!.push(getTreeNodeData(n));
-        },
-        (n: NodeData) => nodeIdToChildren[getID(n)],
-        'TB',
-      );
-      treeData.push(tree);
-    }
-  }
-
-  return treeData;
-}
 
 type TreeDataGetter = {
   getNodeData?: (datum: TreeData) => NodeData;
