625a6cd21f1d02cf6c8359c6a63e77b913d52f9e
Aaron <yangtao.yangtao@antgroup.com>
2024-02-08 10:37:48 +0800

    fix: fix issue occured in rebase

--- packages/g6/src/index.ts
+++ packages/g6/src/index.ts
@@ -5,4 +5,4 @@ export const version = '5.0.0';
 export { getPlugin, getPlugins, register } from './registry';
 export * from './spec';
 
-export { transformGraphDataToTreeData, transformTreeDataToGraphData } from './utils/tree';
+export { transformTreeDataToGraphData } from './utils/tree';
