e6268a0d4ca863ad0d33c2903febc6a19905bb8a
gantang <lh375460@digital-engine.com>
2024-02-20 15:08:24 +0800

    fix(react-node): export type 另起一行

--- packages/react-node/src/ReactNode/index.ts
+++ packages/react-node/src/ReactNode/index.ts
@@ -1,12 +1,21 @@
-export { default as Group, type GroupProps } from './Group';
-export { default as Circle, type CircleProps } from './Shape/Circle';
-export { default as Ellipse, type EllipseProps } from './Shape/Ellipse';
-export { default as Image, type ImageProps } from './Shape/Image';
-export { default as Marker, type MarkerProps } from './Shape/Marker';
-export { default as Polygon, type PolygonProps } from './Shape/Polygon';
-export { default as Rect, type RectProps } from './Shape/Rect';
-export { default as Path, type PathProps } from './Shape/Path';
-export { default as Text, type TextProps } from './Shape/Text';
+export { default as Group } from './Group';
+export type { GroupProps } from './Group';
+export { default as Circle } from './Shape/Circle';
+export type { CircleProps } from './Shape/Circle';
+export { default as Ellipse } from './Shape/Ellipse';
+export type { EllipseProps } from './Shape/Ellipse';
+export { default as Image } from './Shape/Image';
+export type { ImageProps } from './Shape/Image';
+export { default as Marker } from './Shape/Marker';
+export type { MarkerProps } from './Shape/Marker';
+export { default as Polygon } from './Shape/Polygon';
+export type { PolygonProps } from './Shape/Polygon';
+export { default as Rect } from './Shape/Rect';
+export type { RectProps } from './Shape/Rect';
+export { default as Path } from './Shape/Path';
+export type { PathProps } from './Shape/Path';
+export { default as Text } from './Shape/Text';
+export type { TextProps } from './Shape/Text';
 export {
   createNodeFromReact,
   renderTarget,
