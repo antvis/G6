06d71a16d32d3d8435cac7903de8a4e350521c3b
hustcc <i@hust.cc>
2024-02-22 10:31:35 +0800

    fix: rect anchor draw error

--- packages/g6/src/elements/nodes/rect.ts
+++ packages/g6/src/elements/nodes/rect.ts
@@ -1,4 +1,4 @@
-import type { DisplayObjectConfig, Group } from '@antv/g';
+import type { DisplayObjectConfig, RectStyleProps as GRectStyleProps, Group } from '@antv/g';
 import { Rect as GRect } from '@antv/g';
 import { deepMix } from '@antv/util';
 import type { BaseNodeProps } from '../../types';
@@ -16,11 +16,17 @@ export class Rect extends BaseNode<BaseNodeProps, GRect> {
   static defaultStyleProps: Partial<RectStyleProps> = {
     width: 100,
     height: 30,
-    anchor: [0.5, 0.5],
   };
 
   constructor(options: RectOptions) {
     super(deepMix({}, { style: Rect.defaultStyleProps }, options));
+  }
+
+  protected getKeyStyle(attributes: ParsedRectStyleProps): GRectStyleProps {
+    return {
+      ...(super.getKeyStyle(attributes) as GRectStyleProps),
+      anchor: [0.5, 0.5], // !!! It cannot be set to default values because G.CustomElement cannot handle it properly.
+    };
   }
 
   protected drawKeyShape(attributes: ParsedRectStyleProps, container: Group): GRect | undefined {
