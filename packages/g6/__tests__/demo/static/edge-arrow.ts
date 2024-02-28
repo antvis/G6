e802349d81bf973e38b269447e2e0394b5b41f00
Yuxin <55794321+yvonneyx@users.noreply.github.com>
2024-02-28 12:34:24 +0800

    docs: add build-in and custom arrows demos (#5477)
    * refactor: simplify arrow size setting
    
    * test: add buildin and custom arrows tests
    
    * docs: add arrows site demos
    
    * test: add arrow snapshots

---
+++ packages/g6/__tests__/demo/static/edge-arrow.ts
@@ -0,0 +1,46 @@
+import { Graph } from '@/src';
+import type { StaticTestCase } from '../types';
+
+export const edgeArrow: StaticTestCase = async (context) => {
+  const { canvas, animation } = context;
+
+  const edgeIds = [
+    'default-arrow',
+    'triangle-arrow',
+    'simple-arrow',
+    'vee-arrow',
+    'circle-arrow',
+    'rect-arrow',
+    'diamond-arrow',
+    'triangleRect-arrow',
+  ];
+
+  const data = {
+    nodes: new Array(16).fill(0).map((_, i) => ({ id: `node${i + 1}` })),
+    edges: edgeIds.map((id, i) => ({
+      id,
+      source: `node${i * 2 + 1}`,
+      target: `node${i * 2 + 2}`,
+    })),
+  };
+
+  const graph = new Graph({
+    container: canvas,
+    data,
+    edge: {
+      style: {
+        type: 'line', // 👈🏻 Edge shape type.
+        labelText: (d: any) => d.id,
+        endArrow: true,
+        endArrowType: (d: any) => d.id.split('-')[0],
+      },
+    },
+    layout: {
+      type: 'grid',
+      cols: 2,
+    },
+    animation,
+  });
+
+  await graph.render();
+};
---
+++ packages/site/examples/item/arrows/demo/built-in-arrows.ts
@@ -0,0 +1,39 @@
+import { Graph } from '@antv/g6';
+
+const edgeIds = [
+  'default-arrow',
+  'triangle-arrow',
+  'simple-arrow',
+  'vee-arrow',
+  'circle-arrow',
+  'rect-arrow',
+  'diamond-arrow',
+  'triangleRect-arrow',
+];
+
+const data = {
+  nodes: new Array(16).fill(0).map((_, i) => ({ id: `node${i + 1}` })),
+  edges: edgeIds.map((id, i) => ({
+    id,
+    source: `node${i * 2 + 1}`,
+    target: `node${i * 2 + 2}`,
+  })),
+};
+
+const graph = new Graph({
+  data,
+  edge: {
+    style: {
+      type: 'line', // 👈🏻 Edge shape type.
+      labelText: (d: any) => d.id,
+      endArrow: true,
+      endArrowType: (d: any) => d.id.split('-')[0],
+    },
+  },
+  layout: {
+    type: 'grid',
+    cols: 2,
+  },
+});
+
+graph.render();
