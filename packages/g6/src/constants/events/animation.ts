3ae6260c7f8d9f5123aafa0d2704f55452d414c9
Aaron <yangtao.yangtao@antgroup.com>
2024-03-04 16:09:37 +0800

    refactor(spec): adjust animation, add extra viewport options

--- packages/g6/src/constants/events/animation.ts
+++ packages/g6/src/constants/events/animation.ts
@@ -1,8 +1,6 @@
 export const enum AnimationType {
   DRAW = 'draw',
-  ZOOM = 'zoom',
-  ROTATE = 'rotate',
-  TRANSLATE = 'translate',
+  TRANSFORM = 'transform',
   // LAYOUT = 'layout', // 布局没有统一的动画对象，因此不抛出动画事件 | There is no unified animation object for layout, so no animation event is thrown
   ELEMENT_VISIBILITY_CHANGE = 'elementvisibilitychange',
   ELEMENT_STATE_CHANGE = 'elementstatechange',
