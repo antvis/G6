faeadca6ccd2d7e857386310d74da638badee379
yvonneyx <zhuyuxin0627@gmail.com>
2024-03-05 16:46:52 +0800

    test: add unit tests

--- packages/g6/__tests__/unit/utils/combo.spec.ts
+++ packages/g6/__tests__/unit/utils/combo.spec.ts
@@ -1,5 +1,6 @@
 import { Circle, CircleCombo } from '@/src/elements';
 import {
+  getCircleCollapsedOrigin,
   getCollapsedMarkerText,
   getDescendantCount,
   getRectCollapsedOrigin,
@@ -18,6 +19,15 @@ describe('combo', () => {
     expect(getRectCollapsedOrigin('bottom-left', [100, 100], [200, 200])).toEqual([0.25, 0.75]);
     expect(getRectCollapsedOrigin('bottom-right', [100, 100], [200, 200])).toEqual([0.75, 0.75]);
     expect(getRectCollapsedOrigin([0.5, 0.5], [100, 100], [200, 200])).toEqual([0.5, 0.5]);
+  });
+
+  it('getCircleCollapsedOrigin', () => {
+    expect(getCircleCollapsedOrigin('top', [100, 100], [200, 200])).toEqual([0.5, 0.25]);
+    expect(getCircleCollapsedOrigin('bottom', [100, 100], [200, 200])).toEqual([0.5, 0.75]);
+    expect(getCircleCollapsedOrigin('left', [100, 100], [200, 200])).toEqual([0.25, 0.5]);
+    expect(getCircleCollapsedOrigin('right', [100, 100], [200, 200])).toEqual([0.75, 0.5]);
+    expect(getCircleCollapsedOrigin('center', [100, 100], [200, 200])).toEqual([0.5, 0.5]);
+    expect(getCircleCollapsedOrigin([0.5, 0.5], [100, 100], [200, 200])).toEqual([0.5, 0.5]);
   });
 
   it('getXYByCollapsedOrigin', () => {
