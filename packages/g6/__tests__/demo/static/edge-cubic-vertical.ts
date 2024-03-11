07eb7432d16eb907db5c11dd17cbb002e1bc4dbe
yvonneyx <zhuyuxin0627@gmail.com>
2024-03-11 21:51:18 +0800

    docs: add cubic vertical and cubic horizontal demos

--- packages/g6/__tests__/demo/static/edge-cubic-vertical.ts
+++ packages/g6/__tests__/demo/static/edge-cubic-vertical.ts
@@ -1,20 +1,71 @@
-import { CubicVertical } from '@/src/elements/edges';
-import { createEdgeNode } from '@@/utils';
+import { Graph } from '@/src';
 import type { StaticTestCase } from '../types';
 
 export const edgeCubicVertical: StaticTestCase = async (context) => {
-  const { container } = context;
-  [100, 150, 200, 250, 300].forEach((v) => {
-    container.appendChild(
-      new CubicVertical({
-        style: {
-          sourceNode: createEdgeNode([200, 100]),
-          targetNode: createEdgeNode([v, 300]),
-          stroke: '#1890FF',
-          lineWidth: 2,
-          endArrow: true,
-        },
-      }),
-    );
+  const { container, animation, theme } = context;
+
+  const data = {
+    nodes: [{ id: 'node1' }, { id: 'node2' }, { id: 'node3' }, { id: 'node4' }, { id: 'node5' }, { id: 'node6' }],
+    edges: [
+      {
+        id: 'line-default',
+        source: 'node1',
+        target: 'node2',
+      },
+      {
+        id: 'line-active',
+        source: 'node1',
+        target: 'node3',
+      },
+      {
+        id: 'line-selected',
+        source: 'node1',
+        target: 'node4',
+      },
+      {
+        id: 'line-highlight',
+        source: 'node1',
+        target: 'node5',
+      },
+      {
+        id: 'line-inactive',
+        source: 'node1',
+        target: 'node6',
+      },
+    ],
+  };
+
+  const graph = new Graph({
+    container: container,
+    theme,
+    data,
+    node: {
+      style: {
+        port: true,
+        ports: [{ placement: 'top' }, { placement: 'bottom' }],
+      },
+    },
+    edge: {
+      style: {
+        type: 'cubic-vertical', // 👈🏻 Edge shape type.
+        labelText: (d: any) => d.id,
+        endArrow: true,
+      },
+    },
+    layout: {
+      type: 'dagre',
+      begin: [50, 50],
+      rankdir: 'TB',
+      nodesep: 25,
+      ranksep: 150,
+    },
+    animation,
   });
+
+  await graph.render();
+
+  graph.setElementState('line-active', 'active');
+  graph.setElementState('line-selected', 'selected');
+  graph.setElementState('line-highlight', 'highlight');
+  graph.setElementState('line-inactive', 'inactive');
 };
