f129f5206926f0eced806bfa5c4fa452f0f4f70b
hustcc <i@hust.cc>
2024-03-14 11:28:34 +0800

    docs: add fruchterman demo

--- packages/site/examples/net/fruchtermanLayout/demo/fruchtermanWebWorker.ts
+++ packages/site/examples/net/fruchtermanLayout/demo/fruchtermanWebWorker.ts
@@ -1,15 +1,6 @@
-import { Graph, Extensions, extend } from '@antv/g6';
-
-const ExtGraph = extend(Graph, {
-  layouts: {
-    fruchterman: Extensions.FruchtermanLayout,
-  },
-});
+import { Graph } from '@antv/g6';
 
 const container = document.getElementById('container');
-const width = container.scrollWidth;
-const height = (container.scrollHeight || 500) - 20;
-
 const descriptionDiv = document.createElement('div');
 descriptionDiv.innerHTML = 'Doing layout... web-worker is enabled in this demo, so the layout will not block the page.';
 container.appendChild(descriptionDiv);
@@ -17,38 +8,32 @@ container.appendChild(descriptionDiv);
 fetch('https://gw.alipayobjects.com/os/basement_prod/7bacd7d1-4119-4ac1-8be3-4c4b9bcbc25f.json')
   .then((res) => res.json())
   .then((data) => {
-    const graph = new ExtGraph({
+    const graph = new Graph({
       container: 'container',
-      width,
-      height,
-      transforms: [
-        {
-          type: 'transform-v4-data',
-          activeLifecycle: ['read'],
-        },
-      ],
-      modes: {
-        default: ['drag-canvas', 'drag-node', 'zoom-canvas', 'click-select'],
-      },
+      data,
       layout: {
         type: 'fruchterman',
         speed: 20,
         gravity: 1,
         maxIteration: 10000,
-        gravity: 1,
         workerEnabled: true,
       },
       node: {
-        keyShape: {
-          r: 6,
+        style: {
+          size: 6,
         },
       },
-      data,
+      edge: {
+        style: {
+          stroke: '#ddd',
+        },
+      },
+      behaviors: ['drag-canvas', 'drag-node'],
     });
+
+    graph.render();
 
     graph.on('afterlayout', () => {
-      descriptionDiv.innerHTML = 'Done!';
+      descriptionDiv.innerHTML = 'Layout in Worker, Done!';
     });
-
-    window.graph = graph;
   });
