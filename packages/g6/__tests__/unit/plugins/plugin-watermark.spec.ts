01bc0cb2fe6aff7ce3b83fbb540849a1427db5a6
hustcc <i@hust.cc>
2024-03-20 10:55:20 +0800

    test: add tc for watermark, but should skip it

--- packages/g6/__tests__/unit/plugins/plugin-watermark.spec.ts
+++ packages/g6/__tests__/unit/plugins/plugin-watermark.spec.ts
@@ -1,9 +1,30 @@
-import { pluginWatermark } from '@@/demo/case';
+import { pluginWatermark, pluginWatermarkImage } from '@@/demo/case';
 import { createDemoGraph } from '@@/utils';
 
-describe('plugin watermark', () => {
-  it('watermark css', async () => {
+// Skip it because it's not working in the test environment.
+describe.skip('plugin watermark', () => {
+  it('watermark text', async () => {
     const graph = await createDemoGraph(pluginWatermark);
-    expect(graph.getPlugins()).toEqual([{ type: 'watermark', text: 'hello, \na watermark.', textFontSize: 24 }]);
+    expect(graph.getPlugins()).toEqual([{ type: 'watermark', text: 'hello, \na watermark.', textFontSize: 12 }]);
+    expect(graph.getCanvas().getContainer()?.style.backgroundImage).toContain('data:image/png;base64');
+
+    graph.destroy();
+    expect(graph.getCanvas().getContainer()?.style.backgroundImage).toBeFalsy();
+  });
+
+  it('watermark image', async () => {
+    const graph = await createDemoGraph(pluginWatermarkImage);
+    expect(graph.getPlugins()).toEqual([
+      {
+        type: 'watermark',
+        width: 100,
+        height: 100,
+        imageURL: 'https://mdn.alipayobjects.com/huamei_qa8qxu/afts/img/A*7svFR6wkPMoAAAAAAAAAAAAADmJ7AQ/original',
+      },
+    ]);
+    expect(graph.getCanvas().getContainer()?.style.backgroundImage).toContain('data:image/png;base64');
+
+    graph.destroy();
+    expect(graph.getCanvas().getContainer()?.style.backgroundImage).toBeFalsy();
   });
 });
