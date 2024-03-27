9f373eee5de077022bf41ff177583300f7cbc84a
Aaron <yangtao.yangtao@antgroup.com>
2024-03-27 19:48:38 +0800

    feat(plugins): add 3d light

---
+++ packages/g6-extension-3d/src/plugins/light.ts
@@ -0,0 +1,78 @@
+import type { AmbientLightProps, DirectionalLightProps } from '@antv/g-plugin-3d';
+import { AmbientLight, DirectionalLight } from '@antv/g-plugin-3d';
+import type { BasePluginOptions, RuntimeContext } from '@antv/g6';
+import { BasePlugin, GraphEvent } from '@antv/g6';
+import { deepMix } from '@antv/util';
+
+export interface LightOptions extends BasePluginOptions {
+  /**
+   * <zh/> 环境光
+   *
+   * <en/> Ambient light
+   */
+  ambient?: AmbientLightProps;
+  /**
+   * <zh/> 平行光
+   *
+   * <en/> Directional light
+   */
+  directional?: DirectionalLightProps;
+}
+
+export class Light extends BasePlugin<LightOptions> {
+  static defaultOptions: Partial<LightOptions> = {
+    ambient: {
+      fill: '#fff',
+      intensity: Math.PI * 2,
+    },
+    directional: {
+      fill: '#fff',
+      direction: [-1, 0, 1],
+      intensity: Math.PI * 0.7,
+    },
+  };
+
+  private ambient?: AmbientLight;
+
+  private directional?: DirectionalLight;
+
+  constructor(context: RuntimeContext, options: LightOptions) {
+    super(context, deepMix({}, Light.defaultOptions, options));
+    this.bindEvents();
+  }
+
+  private bindEvents() {
+    this.context.graph.on(GraphEvent.BEFORE_DRAW, this.setLight);
+  }
+
+  private unbindEvents() {
+    this.context.graph.off(GraphEvent.BEFORE_DRAW, this.setLight);
+  }
+
+  private setLight = () => {
+    const { ambient, directional } = this.options;
+
+    this.upsertLight('directional', directional);
+    this.upsertLight('ambient', ambient);
+  };
+
+  private upsertLight(type: 'ambient', options?: AmbientLightProps): void;
+  private upsertLight(type: 'directional', options?: DirectionalLightProps): void;
+  private upsertLight(type: 'ambient' | 'directional', options?: AmbientLightProps | DirectionalLightProps) {
+    if (options) {
+      const light = this[type];
+      if (light) light.attr(options);
+      else {
+        const Ctor = type === 'ambient' ? AmbientLight : DirectionalLight;
+        const light = new Ctor({ style: options });
+        this[type] = light as any;
+        this.context.canvas.appendChild(light);
+      }
+    } else this[type]?.remove();
+  }
+
+  public destroy() {
+    this.unbindEvents();
+    super.destroy();
+  }
+}
