2843de52334a5720a0c1820bd0ab0e28955ecf3d
Aaron <yangtao.yangtao@antgroup.com>
2024-03-27 19:49:09 +0800

    refactor: adjust exports

--- packages/g6-extension-3d/src/index.ts
+++ packages/g6-extension-3d/src/index.ts
@@ -1,4 +1,1 @@
-export { BaseNode3D, Sphere } from './elements';
-export { renderer } from './renderer';
-
-export type { BaseNode3DStyleProps, SphereStyleProps } from './elements';
+export * from './exports';
