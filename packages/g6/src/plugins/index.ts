48cfc9053e85569f8910e698c509351febf2f2b9
lxfu1 <954055752@qq.com>
2024-03-28 20:40:39 +0800

    chore: rebase v5

--- packages/g6/src/plugins/index.ts
+++ packages/g6/src/plugins/index.ts
@@ -1,6 +1,7 @@
 export { BasePlugin } from './base-plugin';
 export { Contextmenu } from './contextmenu';
 export { GridLine } from './grid-line';
+export { Legend } from './legend';
 export { Toolbar } from './toolbar';
 export { Tooltip } from './tooltip';
 export { Watermark } from './watermark';
@@ -8,6 +9,7 @@ export { Watermark } from './watermark';
 export type { BasePluginOptions } from './base-plugin';
 export type { ContextmenuOptions } from './contextmenu';
 export type { GridLineOptions } from './grid-line';
+export type { LegendOptions } from './legend';
 export type { ToolbarOptions } from './toolbar';
 export type { TooltipOptions } from './tooltip';
 export type { WatermarkOptions } from './watermark';
