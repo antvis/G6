b7b1c70ff32b2183ac120031c3733b3b135ff3a9
lxfu1 <954055752@qq.com>
2024-03-28 20:40:39 +0800

    chore: rebase v5

--- packages/g6/__tests__/demos/index.ts
+++ packages/g6/__tests__/demos/index.ts
@@ -80,6 +80,7 @@ export * from './layout-radial-prevent-overlap-unstrict';
 export * from './layout-radial-sort';
 export * from './plugin-contextmenu';
 export * from './plugin-grid-line';
+export * from './plugin-legend';
 export * from './plugin-toolbar-build-in';
 export * from './plugin-toolbar-iconfont';
 export * from './plugin-tooltip';
