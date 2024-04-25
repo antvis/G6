0c1cfd1c97e09cd8855b42199a37c53c8281a136
Aaron <yangtao.yangtao@antgroup.com>
2024-04-25 17:43:47 +0800

    feat(site): add script that clear generated docs

---
+++ packages/site/scripts/clear-doc.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+
+/**
+ *
+ */
+function clear() {
+  const baseDir = path.resolve(__dirname, '../docs/api');
+
+  // read gitignore
+  const gitignore = fs.readFileSync(path.resolve(baseDir, '.gitignore'), 'utf-8');
+
+  // clear all files list in .gitignore
+  const lines = gitignore.split('\n');
+
+  lines.forEach((line) => {
+    if (line.startsWith('#')) return;
+
+    const file = line.trim();
+    if (file) {
+      const filepath = path.resolve(baseDir, file);
+
+      if (fs.existsSync(filepath)) {
+        if (fs.lstatSync(filepath).isDirectory()) {
+          console.log('Clearing folder: ', filepath);
+          fs.rmdirSync(filepath, { recursive: true });
+        } else {
+          console.log('Clearing file: ', filepath);
+          fs.unlinkSync(filepath);
+        }
+      }
+    }
+  });
+
+  // remove .gitignore
+  fs.unlinkSync('docs/api/.gitignore');
+}
+
+clear();
