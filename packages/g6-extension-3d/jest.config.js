007aa4ea63ceef796c9a4aa9452ad0ec89345aaf
Aaron <yangtao.yangtao@antgroup.com>
2024-05-29 20:44:02 +0800

    fix: fix jest config

--- packages/g6-extension-3d/jest.config.js
+++ packages/g6-extension-3d/jest.config.js
@@ -5,4 +5,5 @@ module.exports = {
   testRegex: '(/__tests__/.*\\.(test|spec))\\.(ts|tsx|js)$',
   collectCoverageFrom: ['src/**/*.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
+  transformIgnorePatterns: [`<rootDir>/node_modules/.pnpm/(?!(d3-*))`],
 };
