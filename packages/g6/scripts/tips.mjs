34eab260a6fcd1c05317de20f756cf043ccbc2be
Aaron <yangtao.yangtao@antgroup.com>
2024-06-06 21:45:27 +0800

    chore: fix postinstall error (#5833)

--- packages/g6/scripts/tips.mjs
+++
@@ -1,6 +0,0 @@
-console.log('-'.repeat(40));
-console.log('\x1b[42m\x1b[97m\x1b[1mNotice\x1b[0m');
-console.log('You are using the latest', '\x1b[1m\x1b[32mAntV G6 5.0\x1b[0m', '!');
-console.log('For more information, please visit:');
-console.log('👉', '\x1b[32m\x1b[4mhttps://g6.antv.antgroup.com\x1b[0m');
-console.log('-'.repeat(40));
