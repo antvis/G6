6f3e00c5dc5dbb0b284a53ccb072d12dede82f2c
SharkMK <wangjianxu0808@163.com>
2024-06-18 09:26:03 +0800

    修复Demo中Fruchterman图布局"使用GPU加速"layout的type配置错误

--- packages/site/examples/layout/fruchterman/demo/run-in-gpu.js
+++ packages/site/examples/layout/fruchterman/demo/run-in-gpu.js
@@ -20,7 +20,7 @@ fetch('https://gw.alipayobjects.com/os/basement_prod/7bacd7d1-4119-4ac1-8be3-4c4
         },
       },
       layout: {
-        type: 'fruchterman',
+        type: 'fruchterman-gpu',
         speed: 20,
         gravity: 1,
         maxIteration: 10000,
