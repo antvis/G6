911d6720837aa98b36ccd3bd3f36e39baafb57e1
Aaron <yangtao.yangtao@antgroup.com>
2024-06-18 16:45:18 +0800

    refactor(behaviors): refactor click-select in abnormal environment

---
+++ packages/g6/__tests__/unit/behaviors/click-select-catch.spec.ts
@@ -0,0 +1,36 @@
+import { behaviorClickSelect } from '@/__tests__/demos';
+import type { Graph } from '@/src';
+import { CanvasEvent, NodeEvent } from '@/src';
+import { createDemoGraph } from '@@/utils';
+
+describe('behavior click-select element', () => {
+  let graph: Graph;
+
+  beforeAll(async () => {
+    graph = await createDemoGraph(behaviorClickSelect, { animation: false });
+  });
+
+  afterAll(() => {
+    graph.destroy();
+  });
+
+  it('delete selected element', async () => {
+    graph.setBehaviors([
+      { type: 'click-select', state: 'active', unselectedState: 'inactive', multiple: true, trigger: [] },
+    ]);
+
+    graph.emit(NodeEvent.CLICK, { target: { id: '0' }, targetType: 'node' });
+    graph.emit(NodeEvent.CLICK, { target: { id: '1' }, targetType: 'node' });
+
+    await expect(graph).toMatchSnapshot(__filename, 'selected');
+
+    graph.removeNodeData(['1']);
+    graph.draw();
+
+    await expect(graph).toMatchSnapshot(__filename, 'deleted');
+
+    graph.emit(CanvasEvent.CLICK, { target: {} });
+
+    await expect(graph).toMatchSnapshot(__filename, 'clear');
+  });
+});
