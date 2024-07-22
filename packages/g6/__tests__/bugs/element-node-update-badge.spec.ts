b573febe3a9940b40c0421da1decf7d889e601dd
王璨 <can.wang@eoitek.com>
2024-07-22 18:51:37 +0800

    fix: the initial values of some nodes are not set by default

---
+++ packages/g6/__tests__/bugs/element-node-update-badge.spec.ts
@@ -0,0 +1,30 @@
+import { createGraph } from '../utils';
+
+describe('bug: element-node-update-badge', () => {
+  it('should update node badge', async () => {
+    const graph = createGraph({
+      animation: false,
+      node: {
+        style: {
+          badge: true,
+          badges: [{ text: '1' }],
+          badgeFill: 'white',
+          badgeBackgroundFill: 'red',
+        },
+      },
+      data: {
+        nodes: [{ id: 'node-0', style: { x: 100, y: 100 }, states: ['inactive'] }],
+      },
+    });
+
+    await graph.render();
+
+    await expect(graph).toMatchSnapshot(__filename);
+
+    graph.setElementState('node-0', []);
+
+    await graph.render();
+
+    await expect(graph).toMatchSnapshot(__filename, 'update-node-badge');
+  });
+});
