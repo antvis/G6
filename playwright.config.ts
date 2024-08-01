5a577971bc602514a45e20af6e5e7e6e8ad67e8d
antv <antv@antfin.com>
2024-08-01 16:38:53 +0800

    chore: update test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 5 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -73,6 +73,6 @@ export default defineConfig({
   webServer: {
     command: 'npm run dev:g6',
     url: 'http://localhost:8080',
-    // reuseExistingServer: !process.env.CI,
+    reuseExistingServer: !process.env.CI,
   },
 });
--- tests/g6/plugins/plugins-minimap.spec.ts
+++ tests/g6/plugins/plugins-minimap.spec.ts
@@ -13,33 +13,37 @@ test.describe('plugin minimap', () => {
     // wheel on to zoom
     await page.mouse.move(250, 250);
 
-    await page.mouse.wheel(0, 10);
+    // 在 github action 中执行时，滚动的距离需要更大
+    // In github action, the scroll distance needs to be larger
+    await page.mouse.wheel(0, process.env.CI ? 20 : 10);
 
-    await expect(page).toHaveScreenshot({ clip });
+    await expect(page).toHaveScreenshot({ clip, maxDiffPixels: 100 });
 
-    await page.mouse.wheel(0, -20);
+    await page.mouse.wheel(0, process.env.CI ? -40 : -20);
 
-    await expect(page).toHaveScreenshot({ clip });
+    await expect(page).toHaveScreenshot({ clip, maxDiffPixels: 100 });
 
     // drag minimap mask
     await page.mouse.move(400, 425);
     await page.mouse.down();
     await page.mouse.move(425, 450, { steps: 10 });
     await page.mouse.up();
-    await expect(page).toHaveScreenshot({ clip });
+    await expect(page).toHaveScreenshot({ clip, maxDiffPixels: 100 });
 
     // drag mask overflow
     await page.mouse.move(425, 450);
     await page.mouse.down();
     await page.mouse.move(550, 550, { steps: 10 });
     await page.mouse.up();
-    await expect(page).toHaveScreenshot({ clip });
+    await expect(page).toHaveScreenshot({ clip, maxDiffPixels: 100 });
 
     // drag canvas
     // playwright mouse 模拟操作无法正常触发 g canvas 的 drag 事件
     // 因此这里直接调用 graph 实例的方法
+    // playwright mouse simulation operation cannot trigger the drag event of g canvas normally
+    // So here directly call the method of the graph instance
     await page.evaluate(() => (window as any).graph.translateTo([100, 100]));
 
-    await expect(page).toHaveScreenshot({ clip });
+    await expect(page).toHaveScreenshot({ clip, maxDiffPixels: 100 });
   });
 });
