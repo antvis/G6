51d4d7f5fd2c35a066126f3d530b383ee9c3fc1d
Aaron <yangtao.yangtao@antgroup.com>
2024-08-02 10:06:28 +0800

    fix(elements): fix incorrect ports position when rotate node (#6113)
    Co-authored-by: antv <antv@antfin.com>

---
+++ packages/g6/__tests__/bugs/element-port-rotate.spec.ts
@@ -0,0 +1,29 @@
+import { createGraph } from '@@/utils';
+
+describe('element port rotate', () => {
+  it('default', async () => {
+    const graph = createGraph({
+      data: {
+        nodes: [{ id: 'node-1', style: { x: 100, y: 100 } }],
+      },
+      node: {
+        type: 'rect',
+        style: {
+          size: [50, 150],
+          port: true,
+          portR: 3,
+          ports: [
+            { key: 'port-1', placement: [0, 0.15] },
+            { key: 'port-2', placement: 'left' },
+            { key: 'port-3', placement: [0, 0.85] },
+          ],
+          transform: 'rotate(45deg)',
+        },
+      },
+    });
+
+    await graph.draw();
+
+    await expect(graph).toMatchSnapshot(__filename);
+  });
+});
