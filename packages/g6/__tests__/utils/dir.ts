9b3570baf9f63b783efd9e4bbafd751b8a75c246
antv <antv@antfin.com>
2024-08-02 16:07:08 +0800

    chore: adjust bugs dir, and snapshot path rule

--- packages/g6/__tests__/utils/dir.ts
+++ packages/g6/__tests__/utils/dir.ts
@@ -10,7 +10,11 @@ import path from 'path';
  */
 export function getSnapshotDir(dir: string, detail: string = 'default'): [string, string] {
   const root = process.cwd();
-  const subDir = dir.replace(root, '').replace('__tests__/unit/', '').replace('.spec.ts', '');
+  const subDir = dir
+    .replace(root, '')
+    .replace('__tests__/unit/', '')
+    .replace('__tests__/bugs/', 'bugs/')
+    .replace('.spec.ts', '');
   const outputDir = path.join(root, '__tests__', 'snapshots', subDir);
   return [outputDir, detail];
 }
