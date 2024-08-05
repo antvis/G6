9c5b855816461b85f179968d8fc6e93b870239f4
antv <antv@antfin.com>
2024-08-05 21:16:44 +0800

    feat(utils): add getAncestorShapes util

--- packages/g6/src/utils/shape.ts
+++ packages/g6/src/utils/shape.ts
@@ -4,7 +4,7 @@ import type { DisplayObject } from '@antv/g';
  * <zh/> 获取图形的所有子元素
  *
  * <en/> Get all the child elements of the shape
- * @param shape <zh/> 图形元素 | <en/> shape
+ * @param shape - <zh/> 图形元素 | <en/> shape
  * @returns <zh/> 子元素数组 | <en/> child elements array
  */
 export function getDescendantShapes<T extends DisplayObject>(shape: T) {
@@ -23,4 +23,21 @@ export function getDescendantShapes<T extends DisplayObject>(shape: T) {
   traverse(shape);
 
   return succeeds;
+}
+
+/**
+ * <zh/> 获取图形的所有祖先元素
+ *
+ * <en/> Get all the ancestor elements of the shape
+ * @param shape - <zh/> 图形元素 | <en/> shape
+ * @returns <zh/> 祖先元素数组 | <en/> ancestor elements array
+ */
+export function getAncestorShapes<T extends DisplayObject>(shape: T) {
+  const ancestors: DisplayObject[] = [];
+  let currentNode = shape.parentNode as DisplayObject;
+  while (currentNode) {
+    ancestors.push(currentNode);
+    currentNode = currentNode.parentNode as DisplayObject;
+  }
+  return ancestors;
 }
