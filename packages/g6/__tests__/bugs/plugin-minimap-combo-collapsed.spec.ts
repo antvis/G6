7ca88d0dc4b4375a907ab75ea728796a218a3df4
MusicTen <37668403+MusicTen@users.noreply.github.com>
2024-08-05 10:04:22 +0800

    fix(minimap): fix collapsed combo childrenNode getShape not found (#6120)
    Co-authored-by: 王璨 <can.wang@eoitek.com>

---
+++ packages/g6/__tests__/bugs/plugin-minimap-combo-collapsed.spec.ts
@@ -0,0 +1,36 @@
+import { createGraph, sleep } from '@@/utils';
+
+describe('bug: plugin-minimap-combo-collapsed', () => {
+  it('should be collapsed', async () => {
+    const graph = createGraph({
+      animation: false,
+      data: {
+        nodes: [{ id: 'node1', combo: 'combo1' }, { id: 'node2' }],
+        edges: [{ source: 'node1', target: 'node2' }],
+        combos: [{ id: 'combo1' }],
+      },
+      layout: {
+        type: 'grid',
+      },
+      plugins: [
+        {
+          key: 'minimap',
+          type: 'minimap',
+          size: [240, 160],
+        },
+      ],
+    });
+
+    await graph.render();
+
+    await expect(graph).toMatchSnapshot(__filename);
+
+    await sleep(1000);
+    graph.collapseElement('combo1');
+    graph.translateElementBy('combo1', [100, 100]);
+
+    await graph.render();
+
+    await expect(graph).toMatchSnapshot(__filename, 'update collapsed combo');
+  });
+});
