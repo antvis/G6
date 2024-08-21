985e6062d0385bf3f087cd83e1e8bc2d84434513
antv <antv@antfin.com>
2024-08-21 11:29:46 +0800

    test: update test case

--- packages/g6/__tests__/unit/import.spec.ts
+++ packages/g6/__tests__/unit/import.spec.ts
@@ -3,7 +3,7 @@ import * as G6 from '@/src';
 describe('import', () => {
   it('default', () => {
     expect(G6).not.toBe(undefined);
-    const keys = Object.keys(G6);
-    expect(keys.length).toBeGreaterThan(0);
+    const entries = Object.entries(G6);
+    expect(entries.length).toBeGreaterThan(0);
   });
 });
