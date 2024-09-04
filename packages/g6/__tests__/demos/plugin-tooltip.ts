cd756af67867642a599d5c88e9c1c79fc6a353c0
Orangii <me@orangii.cn>
2024-09-04 11:46:11 +0800

    fix: correctly hide tooltip when point out (#6270)
    Signed-off-by: orangii <me@orangii.cn>

--- packages/g6/__tests__/demos/plugin-tooltip.ts
+++ packages/g6/__tests__/demos/plugin-tooltip.ts
@@ -1,4 +1,5 @@
 import data from '@@/dataset/combo.json';
+import type { IElementEvent } from '@antv/g6';
 import { Graph } from '@antv/g6';
 
 export const pluginTooltip: TestCase = async (context) => {
@@ -33,6 +34,7 @@ export const pluginTooltip: TestCase = async (context) => {
   pluginTooltip.form = (panel) => {
     const config = {
       trigger: 'click',
+      enable: 'all',
     };
     return [
       panel
@@ -42,6 +44,20 @@ export const pluginTooltip: TestCase = async (context) => {
           graph.setPlugins((plugins) =>
             plugins.map((plugin) => {
               if (typeof plugin === 'object' && plugin.type === 'tooltip') return { ...plugin, trigger };
+              return plugin;
+            }),
+          );
+        }),
+      panel
+        .add(config, 'enable', ['all', 'node', 'edge', 'combo'])
+        .name('Change Enable Target')
+        .onChange((enable: string) => {
+          graph.setPlugins((plugins) =>
+            plugins.map((plugin) => {
+              if (typeof plugin === 'object' && plugin.type === 'tooltip') {
+                if (enable === 'all') return { ...plugin, enable: true };
+                else return { ...plugin, enable: (event: IElementEvent) => event.targetType === enable };
+              }
               return plugin;
             }),
           );
