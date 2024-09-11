730fe3255ad2c46c41e89dfbe7dc3e1d5ac87e11
antv <antv@antfin.com>
2024-09-11 16:33:04 +0800

    fix(elements): fix element cannot translate to origin point

---
+++ packages/g6/__tests__/bugs/element-set-position-to-origin.spec.ts
@@ -0,0 +1,30 @@
+import type { ID } from '@/src';
+import { createGraph } from '@@/utils';
+
+describe('element set position to origin', () => {
+  it('suite 1', async () => {
+    const graph = createGraph({
+      data: {
+        nodes: [{ id: 'node-1' }],
+      },
+    });
+
+    await graph.draw();
+
+    // @ts-expect-error Property 'context' is protected
+    const getElementOf = (id: ID) => graph.context.element!.getElement(id)!;
+
+    expect(graph.getNodeData('node-1').style).toEqual({});
+    expect(getElementOf('node-1').style.transform).toBe('translate(0, 0)');
+
+    graph.translateElementTo('node-1', [100, 100]);
+
+    expect(graph.getNodeData('node-1').style).toEqual({ x: 100, y: 100, z: 0 });
+    expect(getElementOf('node-1').style.transform).toBe('translate(100, 100)');
+
+    graph.translateElementTo('node-1', [0, 0]);
+
+    expect(graph.getNodeData('node-1').style).toEqual({ x: 0, y: 0, z: 0 });
+    expect(getElementOf('node-1').style.transform).toBe('translate(0, 0)');
+  });
+});
