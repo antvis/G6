ba69902b63d0612fa6ee450af7c8214ef0e969cf
zenghawtin <1448593512@qq.com>
2024-09-13 21:43:29 +0800

    fix: change zIndex in DataController
    * fix: deduplicate multiple translations to the same node
    * test: update zIndex related snapshots
    * fix: deal with path seperator in Windows

--- packages/g6/__tests__/utils/dir.ts
+++ packages/g6/__tests__/utils/dir.ts
@@ -10,11 +10,16 @@ import path from 'path';
  */
 export function getSnapshotDir(dir: string, detail: string = 'default'): [string, string] {
   const root = process.cwd();
-  const subDir = dir
-    .replace(root, '')
-    .replace('__tests__/unit/', '')
-    .replace('__tests__/bugs/', 'bugs/')
-    .replace('.spec.ts', '');
-  const outputDir = path.join(root, '__tests__', 'snapshots', subDir);
+
+  const relativeDir = dir.replace(root, '');
+
+  const relativeDirSlices = relativeDir.split(path.sep);
+
+  const testsPartIdx = relativeDirSlices.findIndex((slice) => slice === '__tests__');
+  if (testsPartIdx !== -1) relativeDirSlices[testsPartIdx] = '';
+  const unitPartIdx = relativeDirSlices.findIndex((slice) => slice === 'unit');
+  if (unitPartIdx !== -1) relativeDirSlices[unitPartIdx] = '';
+
+  const outputDir = path.join(root, '__tests__', 'snapshots', ...relativeDirSlices).replace('.spec.ts', '');
   return [outputDir, detail];
 }
