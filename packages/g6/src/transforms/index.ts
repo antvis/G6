335bb5d45ea58968ca316f06b0ff3df908a37484
yvonneyx <zhuyuxin0627@gmail.com>
2024-09-16 16:14:52 +0800

    feat: add map-node-size transform

--- packages/g6/src/transforms/index.ts
+++ packages/g6/src/transforms/index.ts
@@ -3,10 +3,12 @@ export { BaseTransform } from './base-transform';
 export { CollapseExpandCombo } from './collapse-expand-combo';
 export { CollapseExpandNode } from './collapse-expand-node';
 export { GetEdgeActualEnds } from './get-edge-actual-ends';
+export { MapNodeSize } from './map-node-size';
 export { PlaceRadialLabels } from './place-radial-labels';
 export { ProcessParallelEdges } from './process-parallel-edges';
 export { UpdateRelatedEdge } from './update-related-edge';
 
 export type { BaseTransformOptions } from './base-transform';
+export type { MapNodeSizeOptions } from './map-node-size';
 export type { PlaceRadialLabelsOptions } from './place-radial-labels';
 export type { ProcessParallelEdgesOptions } from './process-parallel-edges';
