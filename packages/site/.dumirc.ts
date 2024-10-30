1baa161d9ff5db9f6a5f9916c161e6abe050e4a2
Aaron <yangtao.yangtao@antgroup.com>
2024-10-30 21:19:16 +0800

    chore: remote site news, update site deploy (#6457)

--- packages/site/.dumirc.ts
+++ packages/site/.dumirc.ts
@@ -473,33 +473,6 @@ export default defineConfig({
         },
       ],
     },
-    /** 新闻公告，优先选择配置的，如果没有配置则使用远程的！ */
-    news: [
-      {
-        type: {
-          zh: '推荐',
-          en: 'News',
-        },
-        title: {
-          zh: 'G6 5.0 正式版本发布',
-          en: 'G6 5.0 is released!',
-        },
-        date: '2024.06.06',
-        link: 'https://g6.antv.antgroup.com/',
-      },
-      {
-        type: {
-          zh: '推荐',
-          en: 'News',
-        },
-        title: {
-          zh: '图可视分析与搭建平台 GraphInsight 开源',
-          en: 'GraphInsight is opened source!',
-        },
-        date: '2022.11.22',
-        link: 'https://www.yuque.com/antv/blog/nyl5bkhdkicgm7v8',
-      },
-    ],
     /** 首页特性介绍 */
     features: [
       {
