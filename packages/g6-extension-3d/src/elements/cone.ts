511d565214b920861f62e2d53f95d28cd0c06161
antv <antv@antfin.com>
2024-11-14 17:51:22 +0800

    fix: fix 3d element rendering exceptions

--- packages/g6-extension-3d/src/elements/cone.ts
+++ packages/g6-extension-3d/src/elements/cone.ts
@@ -1,6 +1,7 @@
 import type { DisplayObjectConfig } from '@antv/g';
 import type { ConeGeometryProps, ProceduralGeometry as GGeometry } from '@antv/g-plugin-3d';
 import { ConeGeometry } from '@antv/g-plugin-3d';
+import type { Vector3 } from '@antv/g6';
 import { deepMix } from '@antv/util';
 import { createGeometry } from '../utils/geometry';
 import type { BaseNode3DStyleProps } from './base-node-3d';
@@ -30,15 +31,15 @@ export class Cone extends BaseNode3D<ConeStyleProps> {
     super(deepMix({}, { style: Cone.defaultStyleProps }, options));
   }
 
+  protected getSize(attributes: ConeStyleProps = this.attributes): Vector3 {
+    const { size } = attributes;
+    if (typeof size === 'number') return [size / 2, 0, size];
+    return super.getSize();
+  }
+
   protected getGeometry(attributes: Required<ConeStyleProps>): GGeometry<any> {
     const size = this.getSize();
-    const {
-      baseRadius = size[0] / 2,
-      peakRadius = size[1] / 2,
-      height = size[2],
-      heightSegments,
-      capSegments,
-    } = attributes;
+    const { baseRadius = size[0], peakRadius = size[1], height = size[2], heightSegments, capSegments } = attributes;
     return createGeometry('cone', this.device, ConeGeometry, {
       baseRadius,
       peakRadius,
