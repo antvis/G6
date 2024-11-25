c42a2ee35eb0cdf991b912ac800ef1a5157dd9ca
Aaron <yangtao.yangtao@antgroup.com>
2024-11-25 22:23:40 +0800

    chore: update deps version

--- packages/g6/src/preset.ts
+++ packages/g6/src/preset.ts
@@ -1,6 +1,3 @@
-import { runtime } from '@antv/g';
 import { registerBuiltInExtensions } from './registry/build-in';
-
-runtime.enableMassiveParsedStyleAssignOptimization = true;
 
 registerBuiltInExtensions();
