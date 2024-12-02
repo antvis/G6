279c409089a9b4d17a18d304c8175b412cb55dd6
yvonneyx <zhuyuxin0627@gmail.com>
2024-12-02 15:43:11 +0800

    fix: update remarks

--- packages/g6/src/layouts/snake.ts
+++ packages/g6/src/layouts/snake.ts
@@ -62,11 +62,11 @@ export interface SnakeLayoutOptions extends BaseLayoutOptions {
  *
  * <en/> Snake layout
  * @remarks
- * <zh/> 蛇形布局（Snake Layout）是一种特殊的图形布局方式，能够在较小的空间内更有效地展示长链结构。需要注意的是，其图数据需要满足线性排列的约束。
+ * <zh/> 蛇形布局（Snake Layout）是一种特殊的图形布局方式，能够在较小的空间内更有效地展示长链结构。需要注意的是，其图数据需要确保节点按照从源节点到汇节点的顺序进行线性排列，形成一条明确的路径。
  *
  * <zh/> 节点按 S 字型排列，第一个节点位于第一行的起始位置，接下来的节点在第一行向右排列，直到行末尾。到达行末尾后，下一行的节点从右向左反向排列。这个过程重复进行，直到所有节点排列完毕。
  *
- * <en/> The Snake layout is a special way of graph layout that can more effectively display long chain structures in a smaller space. It should be noted that the graph data needs to meet the constraints of linear arrangement.
+ * <en/> The Snake layout is a special way of graph layout that can more effectively display long chain structures in a smaller space. Note that the graph data needs to ensure that the nodes are linearly arranged in the order from the source node to the sink node to form a clear path.
  *
  * <en/> The nodes are arranged in an S-shaped pattern, with the first node at the beginning of the first row, and the following nodes arranged to the right until the end of the row. After reaching the end of the row, the nodes in the next row are arranged in reverse from right to left. This process is repeated until all nodes are arranged.
  */
