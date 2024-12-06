4cec7b034c306e0a869ec389cd6fa56858b66ad7
Aaron <yangtao.yangtao@antgroup.com>
2024-12-06 10:14:00 +0800

    refactor: ssr support export jpeg

--- packages/g6-ssr/src/types.ts
+++ packages/g6-ssr/src/types.ts
@@ -1,6 +1,6 @@
 import type { GraphOptions } from '@antv/g6';
 import { Graph as G6Graph } from '@antv/g6';
-import type { PdfConfig, PngConfig } from 'canvas';
+import type { JpegConfig, PdfConfig, PngConfig } from 'canvas';
 
 export interface Options extends Omit<GraphOptions, 'renderer' | 'container'> {
   width: number;
@@ -19,9 +19,15 @@ export interface Options extends Omit<GraphOptions, 'renderer' | 'container'> {
    * @defaultValue 'image'
    */
   outputType?: 'image' | 'pdf' | 'svg';
+  /**
+   * <zh/> 图片类型，默认为 png
+   *
+   * <en/> Image type, default is png
+   */
+  imageType?: 'png' | 'jpeg';
 }
 
-export type MetaData = PdfConfig | PngConfig;
+export type MetaData = PdfConfig | PngConfig | JpegConfig;
 
 export interface Graph extends G6Graph {
   exportToFile: (file: string, meta?: MetaData) => void;
