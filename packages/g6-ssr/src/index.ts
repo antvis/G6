ae044338e9a1b0ad6b114ea6fc4e7d7db1c45cf6
Aaron <yangtao.yangtao@antgroup.com>
2024-12-23 15:43:16 +0800

    chore: export g6 getExtension, getExtensions and register from ssr

--- packages/g6-ssr/src/index.ts
+++ packages/g6-ssr/src/index.ts
@@ -1,3 +1,4 @@
+export { getExtension, getExtensions, register } from '@antv/g6';
 export { createCanvas } from './canvas';
 export { createGraph } from './graph';
 export type { Graph, MetaData, Options } from './types';
