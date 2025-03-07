740b57db03559d687a199b39bfa1ce2c52e674f5
whf01206399 <whf01206399@antgroup.com>
2025-03-07 16:47:15 +0800

    docs: change data request from remote to local in demo

--- packages/site/examples/element/node/demo/donut.js
+++ packages/site/examples/element/node/demo/donut.js
@@ -4,50 +4,99 @@ const style = document.createElement('style');
 style.innerHTML = `@import url('${iconfont.css}');`;
 document.head.appendChild(style);
 
-fetch('https://assets.antv.antgroup.com/g6/element-nodes.json')
-  .then((res) => res.json())
-  .then((data) => {
-    const graph = new Graph({
-      container: 'container',
-      animation: false,
-      data,
-      node: {
-        type: 'donut',
-        style: {
-          size: 80,
-          fill: '#DB9D0D',
-          innerR: 0.5,
-          donuts: (_, index) => {
-            if (index === 0) return [1, 2, 3];
+const data = {
+  nodes: [
+    { id: 'default' },
+    { id: 'halo' },
+    { id: 'badges' },
+    { id: 'ports' },
+    {
+      id: 'active',
+      states: ['active'],
+    },
+    {
+      id: 'selected',
+      states: ['selected'],
+    },
+    {
+      id: 'highlight',
+      states: ['highlight'],
+    },
+    {
+      id: 'inactive',
+      states: ['inactive'],
+    },
+    {
+      id: 'disabled',
+      states: ['disabled'],
+    },
+  ],
+};
 
-            if (index === 1) {
-              return [
-                { value: 50, color: 'red' },
-                { value: 150, color: 'green' },
-                { value: 100, color: 'blue' },
-              ];
-            }
+const graph = new Graph({
+  container: 'container',
+  animation: false,
+  data,
+  node: {
+    type: 'donut',
+    style: {
+      size: 80,
+      fill: '#DB9D0D',
+      innerR: 0.5,
+      donuts: (_, index) => {
+        if (index === 0) return [1, 2, 3];
 
-            if (index === 4) {
-              return [
-                { value: 150, fill: 'pink', stroke: '#fff', lineWidth: 1 },
-                { value: 250, stroke: '#fff', lineWidth: 1 },
-                { value: 200, stroke: '#fff', lineWidth: 1 },
-              ];
-            }
+        if (index === 1) {
+          return [
+            { value: 50, color: 'red' },
+            { value: 150, color: 'green' },
+            { value: 100, color: 'blue' },
+          ];
+        }
 
-            return [100, 200, 100, 200];
-          },
-          labelText: (d) => d.id,
+        if (index === 4) {
+          return [
+            { value: 150, fill: 'pink', stroke: '#fff', lineWidth: 1 },
+            { value: 250, stroke: '#fff', lineWidth: 1 },
+            { value: 200, stroke: '#fff', lineWidth: 1 },
+          ];
+        }
 
-          iconFontFamily: 'iconfont',
-          iconText: '\ue602',
-        },
+        return [100, 200, 100, 200];
       },
-      layout: {
-        type: 'grid',
-      },
-    });
+      labelText: (d) => d.id,
+      iconFontFamily: 'iconfont',
+      iconText: '\ue602',
+      halo: (d) => (d.id === 'halo' ? true : false),
+      badges: (d) =>
+        d.id === 'badges'
+          ? [
+              {
+                text: 'A',
+                placement: 'right-top',
+              },
+              {
+                text: 'Important',
+                placement: 'right',
+              },
+              {
+                text: 'Notice',
+                placement: 'right-bottom',
+              },
+            ]
+          : [],
+      badgeFontSize: 8,
+      badgePadding: [1, 4],
+      portR: 3,
+      ports: (d) =>
+        d.id === 'ports'
+          ? [{ placement: 'left' }, { placement: 'right' }, { placement: 'top' }, { placement: 'bottom' }]
+          : [],
+    },
+  },
+  layout: {
+    type: 'grid',
+  },
+});
 
-    graph.render();
-  });
+graph.render();
