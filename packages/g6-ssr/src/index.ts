7d02ca8936198a4acecb27da309c484910e39c26
Aaron <yangtao.yangtao@antgroup.com>
2025-04-08 20:57:35 +0800

    fix: fix g6-ssr

--- packages/g6-ssr/src/index.ts
+++ packages/g6-ssr/src/index.ts
@@ -1,4 +1,7 @@
+import * as G6 from '@antv/g6';
+
 export { getExtension, getExtensions, register } from '@antv/g6';
 export { createCanvas } from './canvas';
 export { createGraph } from './graph';
 export type { Graph, MetaData, Options } from './types';
+export { G6 };
