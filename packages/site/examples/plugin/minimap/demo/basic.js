959619d51eded88a2139fac8f470be35e57cabf6
markleo <liaodalin19903@126.com>
2025-04-15 11:59:54 +0800

    fix: docs/update-fullscreen

--- packages/site/examples/plugin/minimap/demo/basic.js
+++ packages/site/examples/plugin/minimap/demo/basic.js
@@ -1,22 +1,39 @@
 import { Graph } from '@antv/g6';
 
 const graph = new Graph({
-  container: 'container',
   data: { nodes: Array.from({ length: 20 }).map((_, i) => ({ id: `node${i}` })) },
-  behaviors: ['drag-canvas', 'zoom-canvas', 'drag-element'],
+  autoFit: 'center',
+  background: '#fff',
   plugins: [
     {
-      type: 'minimap',
-      size: [240, 160],
+      type: 'fullscreen',
+      key: 'fullscreen',
+    },
+    function () {
+      const graph = this;
+      return {
+        type: 'toolbar',
+        key: 'toolbar',
+        position: 'top-left',
+        onClick: (item) => {
+          const fullscreenPlugin = graph.getPluginInstance('fullscreen');
+          if (item === 'request-fullscreen') {
+            fullscreenPlugin.request();
+          }
+          if (item === 'exit-fullscreen') {
+            fullscreenPlugin.exit();
+          }
+        },
+        getItems: () => {
+          return [
+            { id: 'request-fullscreen', value: 'request-fullscreen' },
+            { id: 'exit-fullscreen', value: 'exit-fullscreen' },
+          ];
+        },
+      };
     },
   ],
-  node: {
-    palette: 'spectral',
-  },
-  layout: {
-    type: 'circular',
-  },
-  autoFit: 'view',
 });
 
 graph.render();
+
