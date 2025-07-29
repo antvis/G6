d6d14a6a5ae742ec56876e0b8f5cc810032597e0
JK <940941188@qq.com>
2025-07-29 14:17:11 +0800

    fix: correct drag node position at rotated canvas

--- packages/g6/__tests__/bugs/behaviors-drag-rotated-canvas.spec.ts
+++ packages/g6/__tests__/bugs/behaviors-drag-rotated-canvas.spec.ts
@@ -1,5 +1,5 @@
 import type { Graph } from '@/src';
-import { CommonEvent } from '@/src';
+import { CommonEvent, NodeEvent } from '@/src';
 import { bugDragRotatedCanvas } from '@@/demos';
 import { createDemoGraph, dispatchCanvasEvent } from '@@/utils';
 
@@ -60,5 +60,41 @@ describe('behavior drag rotated canvas', () => {
 
     expect(graph.getRotation()).toBe(270);
     expect(graph.getPosition()).toBeCloseTo([x + 20, y - 10]);
+  });
+
+  it('drag element when 30 rotated canvas', async () => {
+    await graph.rotateTo(30);
+
+    const id = 'node1';
+    const targetType = 'node';
+
+    const [x, y] = graph.getElementPosition(id);
+
+    graph.emit(NodeEvent.DRAG_START, { target: { id: id }, targetType });
+    graph.emit(NodeEvent.DRAG, { dx: 10, dy: 10 });
+    graph.emit(NodeEvent.DRAG_END, { target: { id: id }, targetType });
+
+    expect(graph.getRotation()).toBe(30);
+    const [nextX, nextY] = graph.getElementPosition(id);
+    expect(nextX.toFixed(2)).toBeCloseTo((x + 3.66).toFixed(2));
+    expect(nextY.toFixed(2)).toBeCloseTo((y + 13.66).toFixed(2));
+  });
+
+  it('drag combo when 30 rotated canvas', async () => {
+    await graph.rotateTo(30);
+
+    const id = 'comboA';
+    const targetType = 'combo';
+
+    const [x, y] = graph.getElementPosition(id);
+
+    graph.emit(NodeEvent.DRAG_START, { target: { id: id }, targetType });
+    graph.emit(NodeEvent.DRAG, { dx: 10, dy: 10 });
+    graph.emit(NodeEvent.DRAG_END, { target: { id: id }, targetType });
+
+    expect(graph.getRotation()).toBe(30);
+    const [nextX, nextY] = graph.getElementPosition(id);
+    expect(nextX.toFixed(2)).toBeCloseTo((x + 3.66).toFixed(2));
+    expect(nextY.toFixed(2)).toBeCloseTo((y + 13.66).toFixed(2));
   });
 });
