5c11704b1519e7292d2740199049aff0d9b5cbd4
Aaron <yangtao.yangtao@antgroup.com>
2025-12-01 11:49:39 +0800

    fix(behaviors): fix create edge get event position, skip assist node style override

--- packages/g6/src/behaviors/create-edge.ts
+++ packages/g6/src/behaviors/create-edge.ts
@@ -4,6 +4,7 @@ import type { RuntimeContext } from '../runtime/types';
 import type { EdgeData } from '../spec';
 import type { EdgeStyle } from '../spec/element/edge';
 import type { ID, IElementEvent, IPointerEvent, NodeLikeData } from '../types';
+import { OVERRIDE_KEY } from '../utils/data';
 import type { BaseBehaviorOptions } from './base-behavior';
 import { BaseBehavior } from './base-behavior';
 
@@ -133,7 +134,10 @@ export class CreateEdge extends BaseBehavior<CreateEdgeOptions> {
     graph.addNodeData([
       {
         id: ASSIST_NODE_ID,
+        type: 'circle',
+        [OVERRIDE_KEY]: false,
         style: {
+          size: 1,
           visibility: 'hidden',
           ports: [{ key: 'port-1', placement: [0.5, 0.5] }],
           x: sourceNode.style?.x,
@@ -159,12 +163,8 @@ export class CreateEdge extends BaseBehavior<CreateEdgeOptions> {
   private updateAssistEdge = async (event: IPointerEvent) => {
     if (!this.source) return;
     const { model, element } = this.context;
-
-    model.translateNodeTo(ASSIST_NODE_ID, [event.canvas.x, event.canvas.y]);
-    //  仅在事件目标阶段才进行绘制 | Draw only during the event target phase.
-    if (event.eventPhase === 2) {
-      await element!.draw({ animation: false, silence: true })?.finished;
-    }
+    model.translateNodeTo(ASSIST_NODE_ID, [event.client.x, event.client.y]);
+    await element!.draw({ animation: false, silence: true })?.finished;
   };
 
   private createEdge = (event: IElementEvent) => {
